21f2c3c81d1dd50cac7de3f69be084ec1afc6715
John Stultz <johnstul@us.ibm.com>
2009-07-03 08:29:58 -0500

    posix-timers: Thread posix-cpu-timers on -rt
    posix-cpu-timer code takes non -rt safe locks in hard irq
    context. Move it to a thread.
    
    [ 3.0 fixes from Peter Zijlstra <peterz@infradead.org> ]
    
    Signed-off-by: John Stultz <johnstul@us.ibm.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1675,6 +1675,9 @@ struct task_struct {
 
 	struct task_cputime cputime_expires;
 	struct list_head cpu_timers[3];
+#ifdef CONFIG_PREEMPT_RT_BASE
+	struct task_struct *posix_timer_list;
+#endif
 
 /* process credentials */
 	const struct cred __rcu *ptracer_cred; /* Tracer's credentials at attach */
