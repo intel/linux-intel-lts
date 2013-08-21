d0645c4063a55e65dcd376c8652c51d58a222501
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2013-08-21 17:48:46 +0200

    genirq: Do not invoke the affinity callback via a workqueue on RT
    [ Upstream commit 2122adbe011cdc0eb62ad62494e181005b23c76a ]
    
    Joe Korty reported, that __irq_set_affinity_locked() schedules a
    workqueue while holding a rawlock which results in a might_sleep()
    warning.
    This patch uses swork_queue() instead.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Tom Zanussi <zanussi@kernel.org>
    
     Conflicts:
    	include/linux/interrupt.h
    	kernel/irq/manage.c

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -15,7 +15,7 @@
 #include <linux/hrtimer.h>
 #include <linux/kref.h>
 #include <linux/workqueue.h>
-#include <linux/swork.h>
+#include <linux/kthread.h>
 
 #include <linux/atomic.h>
 #include <asm/ptrace.h>
@@ -230,7 +230,6 @@ extern void resume_device_irqs(void);
  * struct irq_affinity_notify - context for notification of IRQ affinity changes
  * @irq:		Interrupt to which notification applies
  * @kref:		Reference count, for internal use
- * @swork:		Swork item, for internal use
  * @work:		Work item, for internal use
  * @notify:		Function to be called on change.  This will be
  *			called in process context.
@@ -243,7 +242,7 @@ struct irq_affinity_notify {
 	unsigned int irq;
 	struct kref kref;
 #ifdef CONFIG_PREEMPT_RT_BASE
-	struct swork_event swork;
+	struct kthread_work work;
 #else
 	struct work_struct work;
 #endif
