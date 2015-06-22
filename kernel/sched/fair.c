31dcf6738f3f549e9338ab61d87db85604daa0f8
Patrick Bellasi <patrick.bellasi@arm.com>
2015-06-22 13:51:07 +0100

    ANDROID: DEBUG: schedtune: add tracepoint for CPU boost signal
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Signed-off-by: Andres Oportus <andresoportus@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5977,6 +5977,8 @@ boosted_cpu_util(int cpu)
 	unsigned long util = cpu_util(cpu);
 	unsigned long margin = schedtune_cpu_margin(util, cpu);
 
+	trace_sched_boost_cpu(cpu, util, margin);
+
 	return util + margin;
 }
 
