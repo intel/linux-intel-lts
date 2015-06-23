1bf169fd2dd777f16eaed1dbe6ed87a234a7e56e
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2015-06-23 15:32:51 +0200

    irqwork: push most work into softirq context
    Initially we defered all irqwork into softirq because we didn't want the
    latency spikes if perf or another user was busy and delayed the RT task.
    The NOHZ trigger (nohz_full_kick_work) was the first user that did not work
    as expected if it did not run in the original irqwork context so we had to
    bring it back somehow for it. push_irq_work_func is the second one that
    requires this.
    
    This patch adds the IRQ_WORK_HARD_IRQ which makes sure the callback runs
    in raw-irq context. Everything else is defered into softirq context. Without
    -RT we have the orignal behavior.
    
    This patch incorporates tglx orignal work which revoked a little bringing back
    the arch_irq_work_raise() if possible and a few fixes from Steven Rostedt and
    Mike Galbraith,
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -1663,7 +1663,7 @@ void update_process_times(int user_tick)
 	scheduler_tick();
 	run_local_timers();
 	rcu_check_callbacks(user_tick);
-#ifdef CONFIG_IRQ_WORK
+#if defined(CONFIG_IRQ_WORK) && !defined(CONFIG_PREEMPT_RT_FULL)
 	if (in_irq())
 		irq_work_tick();
 #endif
@@ -1715,6 +1715,10 @@ static __latent_entropy void run_timer_softirq(struct softirq_action *h)
 	 * long periods due to idle.
 	 */
 	base->must_forward_clk = false;
+
+#if defined(CONFIG_IRQ_WORK) && defined(CONFIG_PREEMPT_RT_FULL)
+	irq_work_tick();
+#endif
 
 	__run_timers(base);
 	if (IS_ENABLED(CONFIG_NO_HZ_COMMON) && base->nohz_active)
