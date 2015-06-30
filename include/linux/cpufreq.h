bcb1ad0f2f060440c4152a8992d242c238dea78c
Michael Turquette <mturquette@baylibre.com>
2015-06-30 12:45:27 +0100

    ANDROID: cpufreq: introduce cpufreq_driver_is_slow
    Some architectures and platforms perform CPU frequency transitions
    through a non-blocking method, while some might block or sleep. Even
    when frequency transitions do not block or sleep they may be very slow.
    This distinction is important when trying to change frequency from
    a non-interruptible context in a scheduler hot path.
    
    Describe this distinction with a cpufreq driver flag,
    CPUFREQ_DRIVER_FAST. The default is to not have this flag set,
    thus erring on the side of caution.
    
    cpufreq_driver_is_slow() is also introduced in this patch. Setting
    the above flag will allow this function to return false.
    
    [smuckle@linaro.org: change flag/API to include drivers that are too
     slow for scheduler hot paths, in addition to those that block/sleep]
    
    Cc: Rafael J. Wysocki <rafael@kernel.org>
    Cc: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Michael Turquette <mturquette@baylibre.com>
    Signed-off-by: Steve Muckle <smuckle@linaro.org>
    Signed-off-by: Andres Oportus <andresoportus@google.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -193,6 +193,7 @@ u64 get_cpu_idle_time(unsigned int cpu, u64 *wall, int io_busy);
 int cpufreq_get_policy(struct cpufreq_policy *policy, unsigned int cpu);
 void cpufreq_update_policy(unsigned int cpu);
 bool have_governor_per_policy(void);
+bool cpufreq_driver_is_slow(void);
 struct kobject *get_governor_parent_kobj(struct cpufreq_policy *policy);
 void cpufreq_enable_fast_switch(struct cpufreq_policy *policy);
 void cpufreq_disable_fast_switch(struct cpufreq_policy *policy);
@@ -384,6 +385,14 @@ struct cpufreq_driver {
  * set.
  */
 #define CPUFREQ_NO_AUTO_DYNAMIC_SWITCHING (1 << 6)
+
+/*
+ * Indicates that it is safe to call cpufreq_driver_target from
+ * non-interruptable context in scheduler hot paths.  Drivers must
+ * opt-in to this flag, as the safe default is that they might sleep
+ * or be too slow for hot path use.
+ */
+#define CPUFREQ_DRIVER_FAST		(1 << 7)
 
 int cpufreq_register_driver(struct cpufreq_driver *driver_data);
 int cpufreq_unregister_driver(struct cpufreq_driver *driver_data);
