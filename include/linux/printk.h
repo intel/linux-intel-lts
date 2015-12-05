4a2af41b40b65fc3218e0a96bdd1fdba4e731a03
Philippe Gerum <rpm@xenomai.org>
2015-12-05 18:36:25 +0100

    printk: introduce raw console channel
    A raw output handler (.write_raw) is added to the console descriptor
    for writing (short) text output unmodified, without any logging,
    header or preparation whatsoever, usable from any interrupt stage.
    
    The dedicated raw_printk() variant formats the output message then
    passes it on to the handler with hard pipeline lock held, irqs off.
    
    This is a very basic debug channel for situations when resorting to
    the fairly complex printk() handling is not an option. Unlike early
    consoles, regular consoles can provide a raw output service past the
    boot sequence. Raw output handlers are typically provided by serial
    console devices.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/linux/printk.h
+++ include/linux/printk.h
@@ -161,7 +161,19 @@ static inline void printk_nmi_direct_enter(void) { }
 static inline void printk_nmi_direct_exit(void) { }
 #endif /* PRINTK_NMI */
 
+
 struct dev_printk_info;
+
+#ifdef CONFIG_RAW_PRINTK
+void raw_vprintk(const char *fmt, va_list ap);
+asmlinkage __printf(1, 2)
+void raw_printk(const char *fmt, ...);
+#else
+static inline __cold
+void raw_vprintk(const char *s, va_list ap) { }
+static inline __printf(1, 2) __cold
+void raw_printk(const char *s, ...) { }
+#endif
 
 #ifdef CONFIG_PRINTK
 asmlinkage __printf(4, 0)
