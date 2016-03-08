85f5f5eb184f72271ac95bc770693bf2fb2ad445
Hardik T Shah <hardik.t.shah@intel.com>
2016-03-08 16:05:59 +0530

    SoundWire: Add #defines to the bus driver
    This patch adds the #defines to the SoundWire bus
    driver.
    
    Change-Id: Ic4dfa9582aa45c3685aee021c156cfbc3e23d817
    Signed-off-by: Hardik T Shah <hardik.t.shah@intel.com>

--- include/linux/sdw_bus.h
+++ include/linux/sdw_bus.h
@@ -26,6 +26,39 @@
 #ifndef _LINUX_SDW_BUS_H
 #define _LINUX_SDW_BUS_H
 
+#include <linux/device.h>	/* for struct device */
+#include <linux/mod_devicetable.h> /* For Name size */
+#include <linux/rtmutex.h> /* For rt mutex */
+
+
+#define SOUNDWIRE_MAX_DEVICES 11
+
+#define SDW_NUM_DEV_ID_REGISTERS 6
+
+/* Port flow mode, used to indicate what port flow mode
+ * slave supports
+ */
+#define SDW_PORT_FLOW_MODE_ISOCHRONOUS		0x1
+#define SDW_PORT_FLOW_MODE_TX_CONTROLLED	0x2
+#define SDW_PORT_FLOW_MODE_RX_CONTROLLED	0x4
+#define SDW_PORT_FLOW_MODE_ASYNCHRONOUS		0x8
+
+/* Bit-mask used to indicate Port capability, OR both bits if
+ * Port is bidirectional capable
+ */
+#define SDW_PORT_SOURCE				0x1
+#define SDW_PORT_SINK				0x2
+
+/* Mask to specify what type of sample packaging mode
+ * is supported by port
+ */
+#define	SDW_PORT_BLK_PKG_MODE_BLK_PER_PORT_MASK	0x1
+#define	SDW_PORT_BLK_PKG_MODE_BLK_PER_CH_MASK 0x2
+
+/* Mask to specify data encoding supported by port */
+#define SDW_PORT_ENCODING_TYPE_TWOS_CMPLMNT	0x1
+#define SDW_PORT_ENCODING_TYPE_SIGN_MAGNITUDE	0x2
+#define SDW_PORT_ENCODING_TYPE_IEEE_32_FLOAT	0x4
 
 
 #endif /*  _LINUX_SDW_BUS_H */
