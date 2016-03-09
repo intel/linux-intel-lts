6e9d28ef645ff02847f89af05cc49514f4fee30e
Hardik T Shah <hardik.t.shah@intel.com>
2016-03-09 09:42:34 +0530

    SoundWire: Add #defines to the bus driver private header.
    This patch adds the #defines to the bus driver private
    header file. This needs to be exposed only between
    bus driver source files. These #defines are not
    publicly exposed.
    
    Change-Id: Ib6393566cd95648bfdc790d656f71a3ebe8aaff0
    Signed-off-by: Hardik T Shah <hardik.t.shah@intel.com>

--- drivers/sdw/sdw_priv.h
+++ drivers/sdw/sdw_priv.h
@@ -25,4 +25,33 @@
 #include <linux/kthread.h>	/* For kthread */
 #include <linux/spinlock.h>
 
+#define SDW_MAX_STREAM_TAG_KEY_SIZE	80
+#define SDW_NUM_STREAM_TAGS		100
+#define MAX_NUM_ROWS			23 /* As per SDW Spec */
+#define MAX_NUM_COLS			8/* As per SDW Spec */
+#define MAX_NUM_ROW_COLS		(MAX_NUM_ROWS * MAX_NUM_COLS)
+
+#define SDW_STATE_INIT_STREAM_TAG	    0x1
+#define SDW_STATE_ALLOC_STREAM              0x2
+#define SDW_STATE_CONFIG_STREAM             0x3
+#define SDW_STATE_COMPUTE_STREAM	    0x4
+#define SDW_STATE_PREPARE_STREAM            0x5
+#define SDW_STATE_ENABLE_STREAM             0x6
+#define SDW_STATE_DISABLE_STREAM            0x7
+#define SDW_STATE_UNPREPARE_STREAM          0x8
+#define SDW_STATE_UNCOMPUTE_STREAM	    0x9
+#define SDW_STATE_RELEASE_STREAM            0xa
+#define SDW_STATE_FREE_STREAM               0xb
+#define SDW_STATE_FREE_STREAM_TAG           0xc
+#define SDW_STATE_ONLY_XPORT_STREAM	    0xd
+
+#define SDW_STATE_INIT_RT		0x1
+#define SDW_STATE_CONFIG_RT		0x2
+#define SDW_STATE_PREPARE_RT		0x3
+#define SDW_STATE_ENABLE_RT		0x4
+#define SDW_STATE_DISABLE_RT		0x5
+#define SDW_STATE_UNPREPARE_RT		0x6
+#define SDW_STATE_RELEASE_RT		0x7
+
+
 #endif /* _LINUX_SDW_PRIV_H */
