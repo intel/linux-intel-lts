b33f96bec389e99b5472fea70e385580818d1bcb
Hardik T Shah <hardik.t.shah@intel.com>
2016-03-09 11:36:44 +0530

    SoundWire: Add "sdw" bus type registration with kernel core
    This patch adds the bus type registration to kernel. It
    also initializes the core data structure as a part of
    bus initialization.
    
    Change-Id: I1cc301a7d7dfe4564cacdd6c5ff37db63060d488
    Signed-off-by: Hardik T Shah <hardik.t.shah@intel.com>

--- drivers/sdw/sdw.c
+++ drivers/sdw/sdw.c
@@ -37,3 +37,49 @@
 
 /* Global instance handling all the SoundWire buses */
 struct sdw_core sdw_core;
+
+
+struct bus_type sdw_bus_type = {
+	.name		= "soundwire",
+};
+EXPORT_SYMBOL_GPL(sdw_bus_type);
+
+struct device sdw_slv = {
+	.init_name = "soundwire",
+};
+
+static void sdw_exit(void)
+{
+	device_unregister(&sdw_slv);
+	bus_unregister(&sdw_bus_type);
+}
+
+static int sdw_init(void)
+{
+	int retval;
+	int i;
+
+	for (i = 0; i < SDW_NUM_STREAM_TAGS; i++)
+		sdw_core.stream_tags[i].stream_tag = i;
+	mutex_init(&sdw_core.core_lock);
+	INIT_LIST_HEAD(&sdw_core.bus_list);
+	idr_init(&sdw_core.idr);
+	retval = bus_register(&sdw_bus_type);
+
+	if (!retval)
+		retval = device_register(&sdw_slv);
+
+
+	if (retval)
+		bus_unregister(&sdw_bus_type);
+
+	return retval;
+}
+postcore_initcall(sdw_init);
+module_exit(sdw_exit);
+
+MODULE_AUTHOR("Hardik Shah <hardik.t.shah@intel.com>");
+MODULE_LICENSE("GPL v2");
+MODULE_VERSION("0.1");
+MODULE_DESCRIPTION("SoundWire bus driver");
+MODULE_ALIAS("platform:soundwire");
