3d2dbb1150ec4e45d9f9ac3d56c3033a722f5e26
Hardik T Shah <hardik.t.shah@intel.com>
2016-03-09 09:26:54 +0530

    SoundWire: Add placeholder for bus driver private file
    Change-Id: Ib284d063fdc9a653a18114f95feb6c8e73bf46fd
    Signed-off-by: Hardik T Shah <hardik.t.shah@intel.com>

---
+++ drivers/sdw/sdw_priv.h
@@ -0,0 +1,28 @@
+/*
+ *  sdw_priv.h - Private definition for sdw bus interface.
+ *
+ *  Copyright (C) 2014-2015 Intel Corp
+ *  Author:  Hardik Shah  <hardik.t.shah@intel.com>
+ *
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ */
+
+#ifndef _LINUX_SDW_PRIV_H
+#define _LINUX_SDW_PRIV_H
+
+#include <linux/kthread.h>	/* For kthread */
+#include <linux/spinlock.h>
+
+#endif /* _LINUX_SDW_PRIV_H */
