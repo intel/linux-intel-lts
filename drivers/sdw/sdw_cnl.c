471cc4608aacf3b85f18f5006fc9a55c59f49688
Hardik T Shah <hardik.t.shah@intel.com>
2016-03-10 19:21:29 +0530

    SoundWire: Intel: Add the register read/write functions.
    This patch adds the register read/write helper functions
    used by driver.
    
    Change-Id: I7fc1c07eb885ecd44a9834b09ec78c200aabe5d5
    Signed-off-by: Hardik T Shah <hardik.t.shah@intel.com>

--- drivers/sdw/sdw_cnl.c
+++ drivers/sdw/sdw_cnl.c
@@ -41,6 +41,70 @@
 #include <linux/sdw/sdw_cnl.h>
 #include "sdw_cnl_priv.h"
 
+static inline int cnl_sdw_reg_readl(void __iomem *base, int offset)
+{
+	int value;
+
+	value = readl(base + offset);
+	return value;
+}
+
+static inline void cnl_sdw_reg_writel(void __iomem *base, int offset, int value)
+{
+	writel(value, base + offset);
+}
+
+static inline u16 cnl_sdw_reg_readw(void __iomem *base, int offset)
+{
+	int value;
+
+	value = readw(base + offset);
+	return value;
+}
+
+static inline void cnl_sdw_reg_writew(void __iomem *base, int offset, u16 value)
+{
+	writew(value, base + offset);
+}
+
+static inline int cnl_sdw_port_reg_readl(void __iomem *base, int offset,
+						int port_num)
+{
+	return cnl_sdw_reg_readl(base, offset + port_num * 128);
+}
+
+static inline void cnl_sdw_port_reg_writel(u32 __iomem *base, int offset,
+						int port_num, int value)
+{
+	return cnl_sdw_reg_writel(base, offset + port_num * 128, value);
+}
+
+struct cnl_sdw {
+	struct cnl_sdw_data data;
+	struct sdw_master *mstr;
+	irqreturn_t (*thread)(int irq, void *context);
+	void *thread_context;
+	struct completion tx_complete;
+	struct cnl_sdw_port port[CNL_SDW_MAX_PORTS];
+	int num_pcm_streams;
+	struct cnl_sdw_pdi_stream *pcm_streams;
+	int num_in_pcm_streams;
+	struct cnl_sdw_pdi_stream *in_pcm_streams;
+	int num_out_pcm_streams;
+	struct cnl_sdw_pdi_stream *out_pcm_streams;
+	int num_pdm_streams;
+	struct cnl_sdw_pdi_stream *pdm_streams;
+	int num_in_pdm_streams;
+	struct cnl_sdw_pdi_stream *in_pdm_streams;
+	int num_out_pdm_streams;
+	struct cnl_sdw_pdi_stream *out_pdm_streams;
+	struct mutex	stream_lock;
+	spinlock_t ctrl_lock;
+	u32 response_buf[0x80];
+	bool sdw_link_status;
+
+};
+
 
 static struct sdw_mstr_driver cnl_sdw_mstr_driver = {
 	.driver_type = SDW_DRIVER_TYPE_MASTER,
