f21a1495062612e5a81a094da628dab629f2fb11
Divya Prakash <divya1.prakash@intel.com>
2016-04-20 10:08:03 +0530

    ASoC: Intel: Add Probe compress APIs
    Using the compress framework, introduce probe
    compress dai APIs to open, set parameters, start,
    stop, and close a probe stream.
    
    Change-Id: Id869e23c5a59602f171aadff89bf5acb0419abab
    Signed-off-by: Divya Prakash
    Reviewed-on:
    Reviewed-by: Babu, Ramesh <ramesh.babu@intel.com>
    Tested-by: Babu, Ramesh <ramesh.babu@intel.com>
    
    ASoC: Intel: Skylake: Add probe CPU dai and DAI ops
    
    Add 2 CPU dais, one each for extractor and injector
    and register the corresponding DAI ops
    
    Change-Id: I0df64b2b6a1e242f8e10bec833c3f5ab28aa2d84
    Signed-off-by: Divya Prakash
    Reviewed-on:
    Reviewed-by: Babu, Ramesh <ramesh.babu@intel.com>
    Tested-by: Babu, Ramesh <ramesh.babu@intel.com>
    
    ASoC: Intel: Maintain the platform compress APIs as common code.
    
    Keep the platform driver compress APIs in a seperate
    file for common use across different compress DAI ops
    
    Change-Id: I62f2396f5e04007ba1454a2187c807c196547c4a
    Signed-off-by: Divya Prakash
    Reviewed-on:
    Reviewed-by: Babu, Ramesh <ramesh.babu@intel.com>
    Tested-by: Babu, Ramesh <ramesh.babu@intel.com>
    
    ASoC: Intel: Implement probe TLV handler
    
    Implement probe TLV handler to set and get
    probe points. The controls parameters contains
    following information:
    - whether it is a connection or disconnection
    - whether the probe type is injector, extractor
    or injector-reextract
    - Probe point to probe, uniquely represented by
    Module ID, Instance ID, queue type and index
    
    This information is parsed by the driver in the
    handler to send the corrensponding IPC and IPC
    payload.Therefore a custom TLV handler implementation
    is required for probe.
    
    Change-Id: I54d6dc656b0629d85d64a793d54b29f02cc43c35
    Signed-off-by: Divya Prakash
    Signed-off-by: Mousumi Jana <mousumix.jana@intel.com>
    Reviewed-on:
    Reviewed-by: Babu, Ramesh <ramesh.babu@intel.com>
    Tested-by: Babu, Ramesh <ramesh.babu@intel.com>
    
    ASoC: Intel: Probe stream operations in alignment with new firmware interface
    
    Based on the new firmware interface and IPCs, following
    are implemented:
    - Probe as a standalone module and not as a pipeline
    - Probe module initialization, gateway configuration
      and deletion
    - Single extractor and multiple injector stream setup
    - Caching stream info such as DMA channel, buffer size,
      DMA type for all streams
    - Single probe instance running for all extractors and
      injectors
    - IPCs for connection, disconnection and DMA attach
    
    Change-Id: Icad5108227ff12a7f6cb23b3ae47358f1c8f2cf4
    Signed-off-by: Divya Prakash
    Reviewed-on:
    Reviewed-by: Babu, Ramesh <ramesh.babu@intel.com>
    Tested-by: Babu, Ramesh <ramesh.babu@intel.com>
    
    ALSA: hda: ext: KW fixes for probe feature in hdac_ext stream
    
    This patch will add fixes for below klocwork errors,
    
    1. Null pointer 'res' that comes from line 528 may be
       dereferenced at line 560.
    2. Pointer 'res' checked for NULL at line 552 may be
       dereferenced at line 560.
    
    Change-Id: I083c779466589db36d38ccfdda2483004deaeb43
    Signed-off-by: G Kranthi <gudishax.kranthikumar@intel.com>
    Reviewed-on:
    Reviewed-by: B, Jayachandran <jayachandran.b@intel.com>
    Reviewed-by: Babu, Ramesh <ramesh.babu@intel.com>
    Tested-by: Babu, Ramesh <ramesh.babu@intel.com>
    
    ALSA: hda: KW fixes for probe feature
    
    This patch will add fixes for below klocwork errors,
    
    1. Null pointer 'dma_buffer_p' that comes from line 390
       may be passed to function and can be dereferenced
       there by passing argument 2 to function 'setup_bdle'
       at line 443.
    2. Null pointer 'runtime' that comes from line 472 may
       be dereferenced at line 490.
    
    Change-Id: Iee59594e8571c06568980204f7352583554c5bf1
    Signed-off-by: G Kranthi <gudishax.kranthikumar@intel.com>
    Reviewed-on:
    Reviewed-by: B, Jayachandran <jayachandran.b@intel.com>
    Reviewed-by: Babu, Ramesh <ramesh.babu@intel.com>
    Tested-by: Babu, Ramesh <ramesh.babu@intel.com>

--- sound/soc/intel/skylake/skl-pcm.c
+++ sound/soc/intel/skylake/skl-pcm.c
@@ -30,6 +30,7 @@
 #include "skl-sst-ipc.h"
 #include "skl-sdw-pcm.h"
 #include "skl-fwlog.h"
+#include "skl-probe.h"
 
 #define HDA_MONO 1
 #define HDA_STEREO 2
@@ -826,13 +827,15 @@ static int skl_trace_compr_copy(struct snd_compr_stream *stream,
 				char __user *dest, size_t count)
 {
 	struct skl_sst *skl_sst = skl_get_sst_compr(stream);
+	struct snd_soc_pcm_runtime *rtd = stream->private_data;
+	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
 	struct sst_dsp *sst = skl_sst->dsp;
 	int core = skl_get_compr_core(stream);
 
 	if (skl_is_logging_core(core))
 		return skl_dsp_copy_log_user(sst, core, dest, count);
 	else
-		return 0;
+		return skl_probe_compr_copy(stream, dest, count, cpu_dai);
 }
 
 static int skl_trace_compr_free(struct snd_compr_stream *stream,
@@ -857,6 +860,15 @@ static int skl_trace_compr_free(struct snd_compr_stream *stream,
 
 static struct snd_compr_ops skl_platform_compr_ops = {
 	.copy = skl_trace_compr_copy,
+};
+
+static struct snd_soc_cdai_ops skl_probe_compr_ops = {
+	.startup = skl_probe_compr_open,
+	.shutdown = skl_probe_compr_close,
+	.trigger = skl_probe_compr_trigger,
+	.ack = skl_probe_compr_ack,
+	.pointer = skl_probe_compr_tstamp,
+	.set_params = skl_probe_compr_set_params,
 };
 
 static struct snd_soc_cdai_ops skl_trace_compr_ops = {
@@ -1008,6 +1020,24 @@ static struct snd_soc_dai_driver skl_fe_dai[] = {
 		.rates = SNDRV_PCM_RATE_48000,
 		.formats = SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_LE,
 		.sig_bits = 32,
+	},
+},
+{
+	.name = "Compress Probe0 Pin",
+	.compress_new = snd_soc_new_compress,
+	.cops = &skl_probe_compr_ops,
+	.playback = {
+		.stream_name = "Probe Playback",
+		.channels_min = HDA_MONO,
+	},
+},
+{
+	.name = "Compress Probe1 Pin",
+	.compress_new = snd_soc_new_compress,
+	.cops = &skl_probe_compr_ops,
+	.capture = {
+			.stream_name = "Probe Capture",
+			.channels_min = HDA_MONO,
 	},
 },
 {
@@ -1692,6 +1722,29 @@ static int skl_populate_modules(struct skl *skl)
 
 	return ret;
 }
+static int skl_get_probe_widget(struct snd_soc_component *component,
+							struct skl *skl)
+{
+	struct skl_probe_config *pconfig = &skl->skl_sst->probe_config;
+	struct snd_soc_dapm_widget *w;
+
+	list_for_each_entry(w, &component->card->widgets, list) {
+		if (is_skl_dsp_widget_type(w, skl->skl_sst->dev) &&
+				(strstr(w->name, "probe") != NULL)) {
+			pconfig->w = w;
+
+			dev_dbg(component->dev, "widget type=%d name=%s\n",
+							w->id, w->name);
+			break;
+		}
+	}
+
+	pconfig->probe_count = 0;
+	pconfig->no_injector = 6;
+	pconfig->no_extractor = 8;
+
+	return 0;
+}
 
 static int skl_platform_soc_probe(struct snd_soc_component *component)
 {
@@ -1746,6 +1799,8 @@ static int skl_platform_soc_probe(struct snd_soc_component *component)
 					skl->cfg.astate_cfg->count,
 					skl->cfg.astate_cfg);
 		}
+
+		skl_get_probe_widget(component, skl);
 	}
 	pm_runtime_mark_last_busy(component->dev);
 	pm_runtime_put_autosuspend(component->dev);
