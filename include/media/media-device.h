c0d23c9973fb7bc5dc87bf0a2ea496f826ad2d41
Sakari Ailus <sakari.ailus@linux.intel.com>
2016-04-26 15:41:37 +0300

    media: Add MEDIA_IOC_DQEVENT
    Events on a media device tell about completion of requests. Blocking
    and non-blocking operation is supported.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- include/media/media-device.h
+++ include/media/media-device.h
@@ -32,8 +32,12 @@
 #include <media/media-entity.h>
 
 struct ida;
+#include <uapi/linux/media.h>
+
 struct device;
+struct file;
 struct media_device;
+struct media_device_fh;
 
 enum media_device_request_state {
 	MEDIA_DEVICE_REQUEST_STATE_IDLE,
@@ -56,6 +60,7 @@ struct media_device_request {
 	u32 id;
 	struct media_device *mdev;
 	struct file *filp;
+	struct media_kevent *kev;
 	struct kref kref;
 	struct list_head list;
 	struct list_head fh_list;
@@ -97,6 +102,11 @@ struct media_device_ops {
 			 struct media_device_request *req);
 	int (*req_queue)(struct media_device *mdev,
 			 struct media_device_request *req);
+};
+
+struct media_kevent {
+	struct list_head list;
+	struct media_event ev;
 };
 
 /**
