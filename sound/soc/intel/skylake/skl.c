1de98b1e67152e21164402805c5adb5ff741fe9d
Guneshwor Singh <guneshwor.o.singh@intel.com>
2016-10-24 16:24:22 +0530

    ASoC: Intel: CNL: Enable runtime PM
    This was a part of commit 59ff9c2 [CNL FPGA] ASoC: Intel: CNL: Add CNL FPGA ifdefs
    
    Change-Id: Ibfef4af5f788a321dd6eb3f1190f484f312f5236
    Signed-off-by: Guneshwor Singh <guneshwor.o.singh@intel.com>

--- sound/soc/intel/skylake/skl.c
+++ sound/soc/intel/skylake/skl.c
@@ -1078,9 +1078,7 @@ static struct pci_driver skl_driver = {
 	.remove = skl_remove,
 	.shutdown = skl_shutdown,
 	.driver = {
-#ifndef CONFIG_SDW
 		.pm = &skl_pm,
-#endif
 	},
 };
 module_pci_driver(skl_driver);
