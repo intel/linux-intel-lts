06899e35d6a47bf863117d12f2d0b8dca127884c
Imre Deak <imre.deak@intel.com>
2016-11-21 21:15:06 +0200

    drm/i915/lspcon: Wait for expected LSPCON mode to settle
    Some LSPCON adaptors may return an incorrect LSPCON mode right after
    waking from DP Sleep state. This is the case at least for the ParadTech
    PS175 adaptor, both when waking because of exiting the DP Sleep to
    active state, or due to any other AUX CH transfer. We can determine the
    current expected mode based on whether the DPCD area is accessible,
    since according to the LSPCON spec this area is only accesible
    in PCON mode.
    
    This wait will avoid us trying to change the mode, while the current
    expected mode hasn't settled yet and start link training before the
    adaptor thinks it's in PCON mode after waking from DP Sleep state.
    
    Cc: Shashank Sharma <shashank.sharma@intel.com>
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Shashank Sharma <shashank.sharma@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1479755707-29596-4-git-send-email-imre.deak@intel.com

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -2402,6 +2402,8 @@ void intel_dp_sink_dpms(struct intel_dp *intel_dp, int mode)
 		ret = drm_dp_dpcd_writeb(&intel_dp->aux, DP_SET_POWER,
 					 DP_SET_POWER_D3);
 	} else {
+		struct intel_lspcon *lspcon = dp_to_lspcon(intel_dp);
+
 		/*
 		 * When turning on, we need to retry for 1ms to give the sink
 		 * time to wake up.
@@ -2413,6 +2415,9 @@ void intel_dp_sink_dpms(struct intel_dp *intel_dp, int mode)
 				break;
 			msleep(1);
 		}
+
+		if (ret == 1 && lspcon->active)
+			lspcon_wait_pcon_mode(lspcon);
 	}
 
 	if (ret != 1)
