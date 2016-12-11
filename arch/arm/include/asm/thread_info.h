467304f95e6486fbefafe7492b21a329ad8d0a0d
Philippe Gerum <rpm@xenomai.org>
2016-12-11 16:32:28 +0100

    ARM/thread: add thread-local (synchronous) flags
    Provide per-thread storage for private flags (i.e. lockless access is
    safe).
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- arch/arm/include/asm/thread_info.h
+++ arch/arm/include/asm/thread_info.h
@@ -45,6 +45,7 @@ struct cpu_context_save {
  */
 struct thread_info {
 	unsigned long		flags;		/* low level flags */
+	__u32			local_flags;	/* local (synchronous) flags */
 	int			preempt_count;	/* 0 => preemptable, <0 => bug */
 	mm_segment_t		addr_limit;	/* address limit */
 	struct task_struct	*task;		/* main task structure */
@@ -71,9 +72,12 @@ struct thread_info {
 {									\
 	.task		= &tsk,						\
 	.flags		= 0,						\
+	.local_flags	= 0,						\
 	.preempt_count	= INIT_PREEMPT_COUNT,				\
 	.addr_limit	= KERNEL_DS,					\
 }
+
+#define ti_local_flags(__ti)	((__ti)->local_flags)
 
 /*
  * how to get the thread information struct from C
