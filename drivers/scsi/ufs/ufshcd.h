b0833af208aebf5357c17bf18a3cedf8ec8652f1
Tomas Winkler <tomas.winkler@intel.com>
2017-01-04 14:14:21 +0200

    scsi: ufs: revamp string descriptor reading
    Define new a type: uc_string_id for easier string
    handling and less casting. Reduce number or string
    copies in price of a dynamic allocation.
    In addition drop usage of variable length array (VLA)
    as it's not considered to be safe.
    
    V9: Fix memory corruption.
    
    Change-Id: Ieda6a4b68e60b8a2d8a2d93a371ff5396dec989b
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -875,14 +875,17 @@ int ufshcd_read_desc_param(struct ufs_hba *hba,
 			   enum desc_idn desc_id,
 			   int desc_index,
 			   u8 param_offset,
-			   u8 *param_read_buf,
+			   void *param_read_buf,
 			   u8 param_size);
 int ufshcd_query_attr(struct ufs_hba *hba, enum query_opcode opcode,
 		      enum attr_idn idn, u8 index, u8 selector, u32 *attr_val);
 int ufshcd_query_flag(struct ufs_hba *hba, enum query_opcode opcode,
 	enum flag_idn idn, bool *flag_res);
-int ufshcd_read_string_desc(struct ufs_hba *hba, int desc_index,
-			    u8 *buf, u32 size, bool ascii);
+
+#define SD_ASCII_STD true
+#define SD_RAW false
+int ufshcd_read_string_desc(struct ufs_hba *hba, u8 desc_index,
+			    char **buf, bool ascii);
 
 int ufshcd_hold(struct ufs_hba *hba, bool async);
 void ufshcd_release(struct ufs_hba *hba);
