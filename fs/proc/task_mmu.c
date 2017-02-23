7882b930813cbdf5d48368477d230e28182aaf63
Amit Pundir <amit.pundir@linaro.org>
2017-02-23 09:24:31 -0800

    ANDROID: task_mmu: fix get_user_pages_remote() call
    Fix get_user_pages_remote() call to align with upstream commit
    5b56d49fc31d ("mm: add locked parameter to get_user_pages_remote()") changes.
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -155,7 +155,7 @@ static void seq_print_vma_name(struct seq_file *m, struct vm_area_struct *vma)
 		struct page *page;
 
 		pages_pinned = get_user_pages_remote(current, mm,
-				page_start_vaddr, 1, 0, &page, NULL);
+				page_start_vaddr, 1, 0, &page, NULL, NULL);
 		if (pages_pinned < 1) {
 			seq_puts(m, "<fault>]");
 			return;
