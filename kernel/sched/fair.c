2720e678b6161ab6742cfafd7c71e4efa3133807
Dietmar Eggemann <dietmar.eggemann@arm.com>
2017-03-20 17:26:47 +0000

    ANDROID: sched/events: Introduce sched_entity load tracking trace event
    The trace event keys load and util (utilization) are mapped to:
    
     (1) load : se->avg.load_avg
    
     (2) util : se->avg.util_avg
    
    To let this trace event work for configurations w/ and w/o group
    scheduling support for cfs (CONFIG_FAIR_GROUP_SCHED) the following
    special handling is necessary for non-existent key=value pairs:
    
     path = "(null)" : In case of !CONFIG_FAIR_GROUP_SCHED or the
                       sched_entity represents a task.
    
     comm = "(null)" : In case sched_entity represents a task_group.
    
     pid = -1        : In case sched_entity represents a task_group.
    
    The following list shows examples of the key=value pairs in different
    configurations for:
    
     (1) a task:
    
         cpu=0 path=(null) comm=sshd pid=2206 load=102 util=102
    
     (2) a taskgroup:
    
         cpu=1 path=/tg1/tg11/tg111 comm=(null) pid=-1 load=882 util=510
    
     (3) an autogroup:
    
         cpu=0 path=/autogroup-13 comm=(null) pid=-1 load=49 util=48
    
     (4) w/o CONFIG_FAIR_GROUP_SCHED:
    
         cpu=0 path=(null) comm=sshd pid=2211 load=301 util=265
    
    The trace event is only defined for CONFIG_SMP.
    
    The helper functions __trace_sched_cpu(), __trace_sched_path() and
    __trace_sched_id() are extended to deal with sched_entities as well.
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Change-Id: Ia628795d7f0fd54e22a1daeb6e3d37b30ab85583
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3026,6 +3026,8 @@ ___update_load_avg(u64 now, int cpu, struct sched_avg *sa,
 
 	if (cfs_rq)
 		trace_sched_load_cfs_rq(cfs_rq);
+	else
+		trace_sched_load_se(container_of(sa, struct sched_entity, avg));
 
 	return 1;
 }
@@ -3271,6 +3273,7 @@ static inline int propagate_entity_load_avg(struct sched_entity *se)
 	update_tg_cfs_load(cfs_rq, se);
 
 	trace_sched_load_cfs_rq(cfs_rq);
+	trace_sched_load_se(se);
 
 	return 1;
 }
