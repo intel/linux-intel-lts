9eaa906afcfd3aeae18e0216a5e9096ddae54d58
Leoni Prodduvaka <leoni.prodduvaka@intel.com>
2017-04-18 22:57:51 +0530

    ASoC: Intel: CNL: Add support for ICL FPGA SDW Aggregation
    Added Master 1 as the skl_platform_dai id for icl fpga sdw aggregation
    
    Change-Id: Ia21d008dddad0ca0928b7903d6760e0da0433b04
    Signed-off-by: Leoni Prodduvaka <leoni.prodduvaka@intel.com>
    Reviewed-on: https://git-gar-1.devtools.intel.com/gerrit/14880
    Reviewed-by: audio_build <audio_build@intel.com>
    Reviewed-by: Kale, Sanyog R <sanyog.r.kale@intel.com>
    Reviewed-by: Kp, Jeeja <jeeja.kp@intel.com>
    Reviewed-by: R, Dharageswari <dharageswari.r@intel.com>
    Reviewed-by: Diwakar, Praveen <praveen.diwakar@intel.com>
    Reviewed-by: Jayanti, Satya Charitardha <satya.charitardha.jayanti@intel.com>
    Tested-by: Avati, Santosh Kumar <santosh.kumar.avati@intel.com>

--- sound/soc/intel/skylake/skl-pcm.c
+++ sound/soc/intel/skylake/skl-pcm.c
@@ -1260,7 +1260,11 @@ static struct snd_soc_dai_driver skl_platform_dai[] = {
 	 */
 	.name = "SDW Pin",
 #if IS_ENABLED(CONFIG_SND_SOC_INTEL_CNL_FPGA)
+#ifndef CONFIG_SND_SOC_SDW_AGGM1M2
 	.id = SDW_BE_DAI_ID_MSTR0,
+#else
+	.id = SDW_BE_DAI_ID_MSTR1,
+#endif
 #else
 	.id = SDW_BE_DAI_ID_MSTR1,
 #endif
@@ -1286,7 +1290,11 @@ static struct snd_soc_dai_driver skl_platform_dai[] = {
 	 */
 	.name = "SDW10 Pin",
 #if IS_ENABLED(CONFIG_SND_SOC_INTEL_CNL_FPGA)
+#ifndef CONFIG_SND_SOC_SDW_AGGM1M2
 	.id = SDW_BE_DAI_ID_MSTR0,
+#else
+	.id = SDW_BE_DAI_ID_MSTR1,
+#endif
 #else
 	.id = SDW_BE_DAI_ID_MSTR1,
 #endif
