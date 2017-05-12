fe96326f36890b63878afa0663d427580e445409
Weight, Russell H <russell.h.weight@intel.com>
2017-05-12 14:20:36 -0700

    Revert "gpu: in RT mode, don't check for the lock owner."
    RT related patches are being reverted in preparation for including
    the newly released 4.9.27 RT Patch.
    
    This reverts commit 847b27d4831aa7271ec0ffc4c41cfca46491a305.

--- drivers/gpu/drm/msm/msm_gem_shrinker.c
+++ drivers/gpu/drm/msm/msm_gem_shrinker.c
@@ -18,18 +18,6 @@
 #include "msm_drv.h"
 #include "msm_gem.h"
 
-#ifdef CONFIG_PREEMPT_RT_FULL
-static bool msm_gem_shrinker_lock(struct drm_device *dev, bool *unlock)
-{
-	if (!mutex_trylock(&dev->struct_mutex)) {
-		*unlock = false;
-		return false;
-	} else {
-		*unlock = true;
-		return true;
-	}
-}
-#else
 static bool msm_gem_shrinker_lock(struct drm_device *dev, bool *unlock)
 {
 	switch (mutex_trylock_recursive(&dev->struct_mutex)) {
@@ -47,7 +35,6 @@ static bool msm_gem_shrinker_lock(struct drm_device *dev, bool *unlock)
 
 	BUG();
 }
-#endif
 
 static unsigned long
 msm_gem_shrinker_count(struct shrinker *shrinker, struct shrink_control *sc)
