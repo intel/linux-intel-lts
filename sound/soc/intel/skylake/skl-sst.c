4a5fa3f2a1693e6f1290ad864e9cf5796e41c6c2
Sanyog Kale <sanyog.r.kale@intel.com>
2017-07-02 11:51:00 +0530

    ASoC: Intel: CNL: Register BRA ops in init.
    Signed-off-by: Sanyog Kale <sanyog.r.kale@intel.com>

--- sound/soc/intel/skylake/skl-sst.c
+++ sound/soc/intel/skylake/skl-sst.c
@@ -528,7 +528,8 @@ static struct sst_dsp_device skl_dev = {
 };
 
 int skl_sst_dsp_init(struct device *dev, void __iomem *mmio_base, int irq,
-		const char *fw_name, struct skl_dsp_loader_ops dsp_ops, struct skl_sst **dsp)
+			const char *fw_name, struct skl_dsp_loader_ops dsp_ops,
+			struct skl_sst **dsp, void *ptr)
 {
 	struct skl_sst *skl;
 	struct sst_dsp *sst;
