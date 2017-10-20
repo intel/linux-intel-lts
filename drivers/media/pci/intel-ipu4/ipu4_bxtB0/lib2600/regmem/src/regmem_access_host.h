596420e83fcd2f9445f04cba5fbb1a9595e0911e
robot007 <robot007@intel.com>
2017-10-19 18:42:00 -0700

    isys & psys package lib2600b0 for commit-id 69d341d
    Releasing on commit-id 8048490
    
    FW Bundle patch number of this release : 276651
    
    FW tag :scci_ipu4_master_20171019_1101
    
    b4c80c6 [DSS] PG test for VHDR_PREGDC_HP 2017-10-11
    18f431b [IPU4P] Re-organize DMEM partition according to FAS-VTIO
    spec 2017-10-18
    e10b180 [IPU4P-SDK] New SDK ICL_AO 20171010_1818_WW41 2017-10-16
    a905172 [IPU4P] Temporarily disable unstable ISYS FPGA test
    2017-10-12
    a2e3c7e [DSS] Creating vhdr_pregdc_hp program group (front end)
    stub 2017-10-06
    26265b3 [DRC] Added Output Merge Routines 2017-10-16
    23e66d5 [DRC] Added Bayer Delay Queue 2017-10-16
    d7ceaa8 [FWQ] IPU4P : Add 2 dpcm tests 2017-10-16
    
    FW OTM: commit-id 69d341d
    
    Change-Id: I01034217bbdbc8a1aa28dcdbc8af703a2a061f05
    Signed-off-by: robot007 <robot007@intel.com>

--- drivers/media/pci/intel-ipu4/ipu4_bxtB0/lib2600/regmem/src/regmem_access_host.h
+++ drivers/media/pci/intel-ipu4/ipu4_bxtB0/lib2600/regmem/src/regmem_access_host.h
@@ -25,14 +25,16 @@
 STORAGE_CLASS_INLINE unsigned int
 regmem_load_32(unsigned int mem_addr, unsigned int reg, unsigned int ssid)
 {
-	return ia_css_cmem_load_32(ssid, mem_addr + REGMEM_OFFSET + (4*reg));
+	/* No need to add REGMEM_OFFSET, it is already included in mem_addr. */
+	return ia_css_cmem_load_32(ssid, mem_addr + (REGMEM_WORD_BYTES*reg));
 }
 
 STORAGE_CLASS_INLINE void
 regmem_store_32(unsigned int mem_addr, unsigned int reg,
 	unsigned int value, unsigned int ssid)
 {
-	ia_css_cmem_store_32(ssid, mem_addr + REGMEM_OFFSET + (4*reg),
+	/* No need to add REGMEM_OFFSET, it is already included in mem_addr. */
+	ia_css_cmem_store_32(ssid, mem_addr + (REGMEM_WORD_BYTES*reg),
 		value);
 }
 
