b9b2ad664af8136a6fc851709563efb3a4c1026f
Michael Byrne <michaelx.byrne@intel.com>
2017-10-25 13:12:34 +0200

    drm/i915: i915 changes to allow DomU to support 3 HDMI displays.
    If vgpu is active;
    a) and we are requesting port A, and a monitor is present on port A,
    force HDMI initialization (Port A is normally reserved for DP/eDP).
    b) ignore check for lspcon as this disables HDMI initialiisation if
    lspcon is supported.
    c) ignore check for intel_encoder->type != INTEL_OUTPUT_EDP so we can
    initialise HDMI (HDMI initialization will change the encode type anyway).
    d) allow HDMI to specify pins for port A to allow initialisation.
    
    Change-Id: I99f9636dc6403d67a0de0422eff9b504190fc5f1
    Reviewed-by: Min He <min.he@intel.com>
    Signed-off-by: Michael Byrne <michaelx.byrne@intel.com>
    Reviewed-on:
    Reviewed-by: He, Min <min.he@intel.com>
    Reviewed-by: Jiang, Fei <fei.jiang@intel.com>
    Reviewed-by: Dong, Eddie <eddie.dong@intel.com>
    Tested-by: Dong, Eddie <eddie.dong@intel.com>

--- drivers/gpu/drm/i915/intel_hdmi.c
+++ drivers/gpu/drm/i915/intel_hdmi.c
@@ -2182,6 +2182,14 @@ static u8 bxt_port_to_ddc_pin(struct drm_i915_private *dev_priv, enum port port)
 	u8 ddc_pin;
 
 	switch (port) {
+	case PORT_A:
+		if ((IS_GEN9_LP(dev_priv)) && (intel_vgpu_active(dev_priv)))
+			ddc_pin = GMBUS_PIN_3_BXT;
+		else {
+			MISSING_CASE(port);
+			ddc_pin = GMBUS_PIN_DPB;
+		}
+		break;
 	case PORT_B:
 		ddc_pin = GMBUS_PIN_1_BXT;
 		break;
@@ -2365,7 +2373,8 @@ void intel_hdmi_init_connector(struct intel_digital_port *intel_dig_port,
 
 	intel_hdmi->ddc_bus = intel_hdmi_ddc_pin(dev_priv, port);
 
-	if (WARN_ON(port == PORT_A))
+	if (!intel_vgpu_active(dev_priv) &&
+		WARN_ON(port == PORT_A))
 		return;
 	intel_encoder->hpd_pin = intel_hpd_pin_default(dev_priv, port);
 
