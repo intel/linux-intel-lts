0f71808f18175876e3602c00f8938236debcbb70
Pardha Saradhi K <pardha.saradhi.kesapragada@intel.com>
2018-01-04 19:40:48 +0530

    ASoC: Intel: Skylake: validate the downloaded firmware
    Print the firmware version downloaded and verify if it is
    equal or greater than the minimum supported version for
    the existing driver. Report error and exit driver
    initialization if downloaded firmware is not compatible
    with current driver.
    
    Change-Id: I6e0f046d88123c9c216ac9a1c9054f37661e1bfe
    Signed-off-by: Prakash, Divya1
    Signed-off-by: Mohit Sinha <mohit.sinha@intel.com>
    Signed-off-by: Pardha Saradhi K <pardha.saradhi.kesapragada@intel.com>
    Signed-off-by: Amadeusz Sławiński <amadeuszx.slawinski@intel.com>
    Reviewed-on:
    Reviewed-by: Rojewski, Cezary <cezary.rojewski@intel.com>
    Tested-by: Rojewski, Cezary <cezary.rojewski@intel.com>

--- sound/soc/intel/skylake/skl-messages.c
+++ sound/soc/intel/skylake/skl-messages.c
@@ -277,7 +277,8 @@ static const struct skl_dsp_ops dsp_ops[] = {
 		.loader_ops = skl_get_loader_ops,
 		.init = skl_sst_dsp_init,
 		.init_fw = skl_sst_init_fw,
-		.cleanup = skl_sst_dsp_cleanup
+		.cleanup = skl_sst_dsp_cleanup,
+		.min_fw_ver = {9, 21, 0, 3173}
 	},
 	{
 		.id = 0x9d71,
@@ -285,7 +286,8 @@ static const struct skl_dsp_ops dsp_ops[] = {
 		.loader_ops = skl_get_loader_ops,
 		.init = skl_sst_dsp_init,
 		.init_fw = skl_sst_init_fw,
-		.cleanup = skl_sst_dsp_cleanup
+		.cleanup = skl_sst_dsp_cleanup,
+		.min_fw_ver = {9, 21, 0, 3173}
 	},
 	{
 		.id = 0x5a98,
@@ -294,7 +296,8 @@ static const struct skl_dsp_ops dsp_ops[] = {
 		.init = bxt_sst_dsp_init,
 		.init_fw = bxt_sst_init_fw,
 		.cleanup = bxt_sst_dsp_cleanup,
-		.do_recovery = skl_do_recovery
+		.do_recovery = skl_do_recovery,
+		.min_fw_ver = {9, 22, 1, 3132}
 	},
 	{
 		.id = 0x3198,
@@ -303,7 +306,8 @@ static const struct skl_dsp_ops dsp_ops[] = {
 		.init = bxt_sst_dsp_init,
 		.init_fw = bxt_sst_init_fw,
 		.cleanup = bxt_sst_dsp_cleanup,
-		.do_recovery = skl_do_recovery
+		.do_recovery = skl_do_recovery,
+		.min_fw_ver = {9, 22, 1, 3366}
 	},
 	{
 		.id = 0x9dc8,
@@ -312,7 +316,8 @@ static const struct skl_dsp_ops dsp_ops[] = {
 		.init = cnl_sst_dsp_init,
 		.init_fw = cnl_sst_init_fw,
 		.cleanup = cnl_sst_dsp_cleanup,
-		.do_recovery = skl_do_recovery
+		.do_recovery = skl_do_recovery,
+		.min_fw_ver = {10, 23, 0, 1233}
 	},
 	{
 		.id = 0x34c8,
@@ -321,7 +326,8 @@ static const struct skl_dsp_ops dsp_ops[] = {
 		.init = cnl_sst_dsp_init,
 		.init_fw = cnl_sst_init_fw,
 		.cleanup = cnl_sst_dsp_cleanup,
-		.do_recovery = skl_do_recovery
+		.do_recovery = skl_do_recovery,
+		.min_fw_ver = {10, 23, 0, 1233}
 	},
 };
 
@@ -1283,14 +1289,12 @@ int skl_init_dsp(struct skl *skl)
 		goto unmap_mmio;
 	}
 
-	loader_ops = ops->loader_ops();
-	ret = ops->init(bus->dev, mmio_base, irq, skl->fw_name, loader_ops,
+	ret = ops->init(bus->dev, mmio_base, irq, skl->fw_name, ops,
 					&skl->skl_sst, &cnl_sdw_bra_ops);
 
 	if (ret < 0)
 		goto unmap_mmio;
 
-	skl->skl_sst->dsp_ops = ops;
 	cores = &skl->skl_sst->cores;
 	cores->count = ops->num_cores;
 
