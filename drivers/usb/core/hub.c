86726735f6e24de09add2952b6427893161ce4d1
Mathias Nyman <mathias.nyman@linux.intel.com>
2018-02-09 17:28:36 +0200

    usb: remove Link Powermanagement (LPM) disable before port reset.
    Trying to disable Link Powermanagement (LPM) before port reset is
    unnecessary and causes issues if host can't communicate with the device,
    which is often the reason why device is reset in the first place.
    
    This was seen when xHC host controller was reset at resume from suspend
    or hibernate, causing all devices to be reset before the hub driver
    resets the ports at resume. As disabling LPM failed the device was
    logically disconnected and re-enumerated.
    
    usb_disable_lpm() will
    - zero usb U1/U2 timeouts for the hub downstream port
    - send ENABLE U1/U2 clear feature requests to the connected device.
    - increase internal reference count for udev->lpm_disable_count
    
    There is no need to zero U1/U2 hub port timeouts, or clearing the
    U1/U2 enable for the connected device before reset. These are the
    default valus after reset.
    
    USB 3.1 section 10.2.2 "HUB Downstream port U1/U2 timers" states that:
    "the U1 and U2 timeout values for a downstream port reset to the default
    values when the port receives a SetPortFeature request for a port reset"
    
    Set the udev->lpm_disable_count to "1" after port reset, which is the
    default lpm_disable_count value when allocating udev, representing
    disabled LPM.
    
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4876,6 +4876,7 @@ hub_port_init(struct usb_hub *hub, struct usb_device *udev, int port1,
 		retval = usb_get_bos_descriptor(udev);
 		if (!retval) {
 			udev->lpm_capable = usb_device_supports_lpm(udev);
+			udev->lpm_disable_count = 1;
 			usb_set_lpm_parameters(udev);
 		}
 	}
@@ -5771,16 +5772,6 @@ static int usb_reset_and_verify_device(struct usb_device *udev)
 	 */
 	usb_disable_usb2_hardware_lpm(udev);
 
-	/* Disable LPM while we reset the device and reinstall the alt settings.
-	 * Device-initiated LPM, and system exit latency settings are cleared
-	 * when the device is reset, so we have to set them up again.
-	 */
-	ret = usb_unlocked_disable_lpm(udev);
-	if (ret) {
-		dev_err(&udev->dev, "%s Failed to disable LPM\n", __func__);
-		goto re_enumerate_no_bos;
-	}
-
 	bos = udev->bos;
 	udev->bos = NULL;
 
@@ -5882,8 +5873,6 @@ done:
 re_enumerate:
 	usb_release_bos_descriptor(udev);
 	udev->bos = bos;
-re_enumerate_no_bos:
-	/* LPM state doesn't matter when we're about to destroy the device. */
 	hub_port_logical_disconnect(parent_hub, port1);
 	return -ENODEV;
 }
