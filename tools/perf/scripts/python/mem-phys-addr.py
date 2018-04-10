b5aea37fbf598809d35e0880f1ba5e07d0f05977
Unazed Spectaculum <leethax0r.coder@gmail.com>
2018-04-10 22:19:27 +0000

    Update mem-phys-addr.py

--- tools/perf/scripts/python/mem-phys-addr.py
+++ tools/perf/scripts/python/mem-phys-addr.py
@@ -27,24 +27,24 @@ event_name = None
 
 def parse_iomem():
 	global f
-	f = open('/proc/iomem', 'r')
+	f = open('/proc/iomem')
 	for i, j in enumerate(f):
-		m = re.split('-|:',j,2)
-		if m[2].strip() == 'System RAM':
-			system_ram.append(long(m[0], 16))
-			system_ram.append(long(m[1], 16))
-		if m[2].strip() == 'Persistent Memory':
-			pmem.append(long(m[0], 16))
-			pmem.append(long(m[1], 16))
+		m = re.split('-|:', j, 2)
+		m[2] = m[2].strip()
+		if m[2] == 'System RAM':
+			system_ram.extend([long(m[0], 16), long(m[1], 16)])
+		elif m[2] == 'Persistent Memory':
+			pmem.extend([long(m[0], 16), long(m[1], 16)])
 
 def print_memory_type():
-	print "Event: %s" % (event_name)
+	print "Event: %s" % event_name
 	print "%-40s  %10s  %10s\n" % ("Memory type", "count", "percentage"),
 	print "%-40s  %10s  %10s\n" % ("----------------------------------------", \
 					"-----------", "-----------"),
 	total = sum(load_mem_type_cnt.values())
-	for mem_type, count in sorted(load_mem_type_cnt.most_common(), \
-					key = lambda(k, v): (v, k), reverse = True):
+	for mem_type, count in sorted(load_mem_type_cnt.most_common(),
+				      key = lambda k, v: v, k,
+				      reverse = True):
 		print "%-40s  %10d  %10.1f%%\n" % (mem_type, count, 100 * count / total),
 
 def trace_begin():
@@ -57,29 +57,24 @@ def trace_end():
 def is_system_ram(phys_addr):
 	#/proc/iomem is sorted
 	position = bisect.bisect(system_ram, phys_addr)
-	if position % 2 == 0:
-		return False
-	return True
+	return position & 1
 
 def is_persistent_mem(phys_addr):
 	position = bisect.bisect(pmem, phys_addr)
-	if position % 2 == 0:
-		return False
-	return True
+	return position & 1
 
 def find_memory_type(phys_addr):
-	if phys_addr == 0:
+	if not phys_addr:
 		return "N/A"
-	if is_system_ram(phys_addr):
+	elif is_system_ram(phys_addr):
 		return "System RAM"
-
-	if is_persistent_mem(phys_addr):
+	elif is_persistent_mem(phys_addr):
 		return "Persistent Memory"
 
 	#slow path, search all
 	f.seek(0, 0)
 	for j in f:
-		m = re.split('-|:',j,2)
+		m = re.split('-|:', j, 2)
 		if long(m[0], 16) <= phys_addr <= long(m[1], 16):
 			return m[2]
 	return "N/A"
@@ -90,6 +85,6 @@ def process_event(param_dict):
 	phys_addr  = sample["phys_addr"]
 
 	global event_name
-	if event_name == None:
+	if event_name is None:
 		event_name = name
 	load_mem_type_cnt[find_memory_type(phys_addr)] += 1
