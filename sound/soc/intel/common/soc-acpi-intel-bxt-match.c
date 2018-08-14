c37f623d61bc8b919e0524e35d400b9d1e366a51
Cezary Rojewski <cezary.rojewski@intel.com>
2018-08-14 11:38:49 +0200

    ASoC: Intel: Move bxt machine drv tables to common directory.
    Align with machine-drv-tables declaration model present on upstream.
    Tables are moved from skl.c into separate file within common directory
    dedicated to bxt-specific data only.
    
    Change-Id: I6780bcd1506709757e673130ffc0ce6d0b26709f
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>
    Reviewed-on:
    Reviewed-by: Uzieblo, Olgierd <olgierd.uzieblo@intel.com>

--- sound/soc/intel/common/soc-acpi-intel-bxt-match.c
+++ sound/soc/intel/common/soc-acpi-intel-bxt-match.c
@@ -8,17 +8,39 @@
 
 #include <sound/soc-acpi.h>
 #include <sound/soc-acpi-intel-match.h>
+#include "../skylake/skl.h"
 
 static struct snd_soc_acpi_codecs bxt_codecs = {
 	.num_codecs = 1,
 	.codecs = {"MX98357A"}
 };
 
+static struct skl_machine_pdata bxt_pdata = {
+	.use_tplg_pcm = true,
+};
+
 struct snd_soc_acpi_mach snd_soc_acpi_intel_bxt_machines[] = {
 	{
 		.id = "INT343A",
+#if IS_ENABLED(CONFIG_SND_SOC_INTEL_BXTP_IVI_RSE_MACH)
+		.drv_name = "bxt_ivi_rse_i2s",
+#elif IS_ENABLED(CONFIG_SND_SOC_INTEL_BXTP_IVI_HU_MACH)
+		.drv_name = "bxt_ivi_hu_i2s",
+#elif IS_ENABLED(CONFIG_SND_SOC_INTEL_BXTP_IVI_M3_MACH)
+		.drv_name = "bxt_ivi_m3_i2s",
+#elif IS_ENABLED(CONFIG_SND_SOC_INTEL_BXTP_IVI_GENERIC_MACH)
+		.drv_name = "bxt_ivi_generic_i2s",
+#else
 		.drv_name = "bxt_alc298s_i2s",
+#endif
+#if IS_ENABLED(CONFIG_SND_SOC_INTEL_BXTP_IVI_RSE_MACH) || \
+	IS_ENABLED(CONFIG_SND_SOC_INTEL_BXTP_IVI_HU_MACH) || \
+	IS_ENABLED(CONFIG_SND_SOC_INTEL_BXTP_IVI_M3_MACH) || \
+	IS_ENABLED(CONFIG_SND_SOC_INTEL_BXTP_IVI_GENERIC_MACH)
+		.fw_filename = "intel/ADSPFW.bin"
+#else
 		.fw_filename = "intel/dsp_fw_bxtn.bin",
+#endif
 	},
 	{
 		.id = "DLGS7219",
@@ -44,13 +66,24 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_bxt_machines[] = {
 		.sof_tplg_filename = "intel/sof-apl-wm8804.tplg",
 		.asoc_plat_name = "0000:00:0e.0",
 	},
+#if IS_ENABLED(CONFIG_SND_SOC_INTEL_BXT_ULL_MACH)
+	{
+		.id = "INT34C3",
+		.drv_name = "bxt_ivi_ull",
+		.fw_filename = "intel/dsp_fw_ull_bxtn.bin",
+		.pdata = &bxt_pdata,
+	},
+#else
 	{
 		.id = "INT34C3",
 		.drv_name = "bxt_tdf8532",
+		.fw_filename = "intel/dsp_fw_bxtn.bin",
 		.sof_fw_filename = "intel/sof-apl.ri",
 		.sof_tplg_filename = "intel/sof-apl-tdf8532.tplg",
 		.asoc_plat_name = "0000:00:0e.0",
+		.pdata = &bxt_pdata,
 	},
+#endif
 	{},
 };
 EXPORT_SYMBOL_GPL(snd_soc_acpi_intel_bxt_machines);
