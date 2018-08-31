31699a496d4099172489cc87e5078020e077e30d
Mingqiang Chi <mingqiang.chi@intel.com>
2018-08-31 10:59:01 +0800

    vhm: add trusty init/de-init support
    vhm will allocate trusty memory from cma then do ept map for a VM with trusty.
    vhm will de-init trusty for a VM during its destroying.
    
    Signed-off-by: Mingqiang Chi <mingqiang.chi@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    Reviewed-by: Zhao Yakui <yakui.zhao@intel.com>
    Acked-by: Eddie Dong <eddie.dong@intel.com>

--- include/linux/vhm/vhm_vm_mngt.h
+++ include/linux/vhm/vhm_vm_mngt.h
@@ -96,6 +96,7 @@ struct vhm_vm {
 	struct device *dev;
 	struct list_head list;
 	unsigned long vmid;
+	unsigned long trusty_host_gpa;
 	int ioreq_fallback_client;
 	long refcnt;
 	struct mutex seg_lock;
