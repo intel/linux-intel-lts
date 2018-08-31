2649c98efeaa7086ebcbaae3735190a38bbc025d
Mingqiang Chi <mingqiang.chi@intel.com>
2018-08-31 10:58:56 +0800

    sos: cleanup hypercall API
    Put all hypercall APIs into vhm_hypercall.c
    other modules need to call hypercall API from
    this file.
    
    Rmove the unused IC_VM_PCI_MSIX_REMAP for PCI MSIX device.
    
    Change-Id: Id896a8300cf54279151a9d5674ed27a352df3f3f
    Tracked-On:218445
    Signed-off-by: Mingqiang Chi <mingqiang.chi@intel.com>
    Reviewed-on: 
    Reviewed-by: Dong, Eddie <eddie.dong@intel.com>
    Tested-by: Dong, Eddie <eddie.dong@intel.com>

--- drivers/char/vhm/vhm_dev.c
+++ drivers/char/vhm/vhm_dev.c
@@ -153,25 +153,66 @@ static long vhm_dev_ioctl(struct file *filep,
 	}
 
 	switch (ioctl_num) {
-	case IC_CREATE_VM:
-		ret = vhm_create_vm(vm, ioctl_param);
-		break;
+	case IC_CREATE_VM: {
+		struct acrn_create_vm created_vm;
 
-	case IC_RESUME_VM:
-		ret = vhm_resume_vm(vm);
-		break;
+		if (copy_from_user(&created_vm, (void *)ioctl_param,
+			sizeof(struct acrn_create_vm)))
+			return -EFAULT;
 
-	case IC_PAUSE_VM:
-		ret = vhm_pause_vm(vm);
-		break;
+		ret = hcall_create_vm(virt_to_phys(&created_vm));
+		if ((ret < 0) ||
+			(created_vm.vmid == ACRN_INVALID_VMID)) {
+			pr_err("vhm: failed to create VM from Hypervisor !\n");
+			return -EFAULT;
+		}
 
-	case IC_DESTROY_VM:
-		ret = vhm_destroy_vm(vm);
-		break;
+		if (copy_to_user((void *)ioctl_param, &created_vm,
+			sizeof(struct acrn_create_vm)))
+			return -EFAULT;
 
-	case IC_QUERY_VMSTATE:
-		ret = vhm_query_vm_state(vm);
+		vm->vmid = created_vm.vmid;
+
+		pr_info("vhm: VM %ld created\n", created_vm.vmid);
 		break;
+	}
+
+	case IC_RESUME_VM: {
+		ret = hcall_resume_vm(vm->vmid);
+		if (ret < 0) {
+			pr_err("vhm: failed to start VM %ld!\n", vm->vmid);
+			return -EFAULT;
+		}
+		break;
+	}
+
+	case IC_PAUSE_VM: {
+		ret = hcall_pause_vm(vm->vmid);
+		if (ret < 0) {
+			pr_err("vhm: failed to pause VM %ld!\n", vm->vmid);
+			return -EFAULT;
+		}
+		break;
+	}
+
+	case IC_DESTROY_VM: {
+		ret = hcall_destroy_vm(vm->vmid);
+		if (ret < 0) {
+			pr_err("failed to destroy VM %ld\n", vm->vmid);
+			return -EFAULT;
+		}
+		vm->vmid = ACRN_INVALID_VMID;
+		break;
+	}
+
+	case IC_QUERY_VMSTATE: {
+		ret = hcall_query_vm_state(vm->vmid);
+		if (ret < 0) {
+			pr_err("vhm: failed to query VM State%ld!\n", vm->vmid);
+			return -EFAULT;
+		}
+		return ret;
+	}
 
 	case IC_ALLOC_MEMSEG: {
 		struct vm_memseg memseg;
@@ -239,17 +280,43 @@ static long vhm_dev_ioctl(struct file *filep,
 	}
 
 	case IC_ASSERT_IRQLINE: {
-		ret = vhm_assert_irqline(vm, ioctl_param);
+		struct acrn_irqline irq;
+
+		if (copy_from_user(&irq, (void *)ioctl_param, sizeof(irq)))
+			return -EFAULT;
+
+		ret = hcall_assert_irqline(vm->vmid, virt_to_phys(&irq));
+		if (ret < 0) {
+			pr_err("vhm: failed to assert irq!\n");
+			return -EFAULT;
+		}
 		break;
 	}
-
 	case IC_DEASSERT_IRQLINE: {
-		ret = vhm_deassert_irqline(vm, ioctl_param);
+		struct acrn_irqline irq;
+
+		if (copy_from_user(&irq, (void *)ioctl_param, sizeof(irq)))
+			return -EFAULT;
+
+		ret = hcall_deassert_irqline(vm->vmid, virt_to_phys(&irq));
+		if (ret < 0) {
+			pr_err("vhm: failed to deassert irq!\n");
+			return -EFAULT;
+		}
 		break;
 	}
-
 	case IC_PULSE_IRQLINE: {
-		ret = vhm_pulse_irqline(vm, ioctl_param);
+		struct acrn_irqline irq;
+
+		if (copy_from_user(&irq, (void *)ioctl_param, sizeof(irq)))
+			return -EFAULT;
+
+		ret = hcall_pulse_irqline(vm->vmid,
+					virt_to_phys(&irq));
+		if (ret < 0) {
+			pr_err("vhm: failed to assert irq!\n");
+			return -EFAULT;
+		}
 		break;
 	}
 
@@ -268,27 +335,72 @@ static long vhm_dev_ioctl(struct file *filep,
 	}
 
 	case IC_ASSIGN_PTDEV: {
-		ret = vhm_assign_ptdev(vm, ioctl_param);
+		uint16_t bdf;
+
+		if (copy_from_user(&bdf,
+				(void *)ioctl_param, sizeof(uint16_t)))
+			return -EFAULT;
+
+		ret = hcall_assign_ptdev(vm->vmid, virt_to_phys(&bdf));
+		if (ret < 0) {
+			pr_err("vhm: failed to assign ptdev!\n");
+			return -EFAULT;
+		}
 		break;
 	}
-
 	case IC_DEASSIGN_PTDEV: {
-		ret = vhm_deassign_ptdev(vm, ioctl_param);
+		uint16_t bdf;
+
+		if (copy_from_user(&bdf,
+				(void *)ioctl_param, sizeof(uint16_t)))
+			return -EFAULT;
+
+		ret = hcall_deassign_ptdev(vm->vmid, virt_to_phys(&bdf));
+		if (ret < 0) {
+			pr_err("vhm: failed to deassign ptdev!\n");
+			return -EFAULT;
+		}
 		break;
 	}
 
 	case IC_SET_PTDEV_INTR_INFO: {
-		ret = vhm_set_ptdev_intr_info(vm, ioctl_param);
+		struct acrn_ptdev_irq pt_irq;
+		int i;
+
+		if (copy_from_user(&pt_irq,
+				(void *)ioctl_param, sizeof(pt_irq)))
+			return -EFAULT;
+
+		ret = hcall_set_ptdev_intr_info(vm->vmid,
+				virt_to_phys(&pt_irq));
+		if (ret < 0) {
+			pr_err("vhm: failed to set intr info for ptdev!\n");
+			return -EFAULT;
+		}
+
 		break;
 	}
-
 	case IC_RESET_PTDEV_INTR_INFO: {
-		ret = vhm_reset_ptdev_intr_info(vm, ioctl_param);
+		struct acrn_ptdev_irq pt_irq;
+		int i;
+
+		if (copy_from_user(&pt_irq,
+				(void *)ioctl_param, sizeof(pt_irq)))
+			return -EFAULT;
+
+		ret = hcall_reset_ptdev_intr_info(vm->vmid,
+				virt_to_phys(&pt_irq));
+		if (ret < 0) {
+			pr_err("vhm: failed to reset intr info for ptdev!\n");
+			return -EFAULT;
+		}
+
 		break;
 	}
 
 	case IC_VM_PCI_MSIX_REMAP: {
-		ret = vhm_remap_pci_msix(vm, ioctl_param);
+		/* This is not used any more */
+		ret = -EFAULT;
 		break;
 	}
 
