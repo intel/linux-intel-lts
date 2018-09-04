8dff23660841d70a761970e7ecda8286c4686388
Min He <min.he@intel.com>
2018-09-04 01:00:01 +0000

    drm/i915/gvt: clean up the cfg space and MMIO spaces
    When vgpu instance is destroyed, we need to clean up the MMIO and
    aperture regions, which is missing in current code.
    
    Signed-off-by: Min He <min.he@intel.com>
    Signed-off-by: Yin Fengwei <fengwei.yin@intel.com>
    Reviewed-by: Zhao Yakui <yakui.zhao@intel.com>
    Tracked-on: https://github.com/projectacrn/acrn-hypervisor/issues/1146

--- drivers/gpu/drm/i915/gvt/vgpu.c
+++ drivers/gpu/drm/i915/gvt/vgpu.c
@@ -300,6 +300,7 @@ void intel_gvt_destroy_vgpu(struct intel_vgpu *vgpu)
 	intel_vgpu_clean_gtt(vgpu);
 	intel_gvt_hypervisor_detach_vgpu(vgpu);
 	intel_vgpu_free_resource(vgpu);
+	intel_vgpu_reset_cfg_space(vgpu);
 	intel_vgpu_clean_mmio(vgpu);
 	intel_vgpu_dmabuf_cleanup(vgpu);
 	mutex_unlock(&vgpu->vgpu_lock);
