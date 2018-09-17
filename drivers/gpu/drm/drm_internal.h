10ec55bafa1b73ded0adcb4d10c3146517be4748
Fan, Yugang <yugang.fan@intel.com>
2018-09-17 09:50:43 +0800

    drm/i915: Sysfs interface to get GFX shmem usage stats per process
    There is a requirement of a new interface for achieving below
    functionalities:
    1) Need to provide Client based detailed information about the
    distribution of Graphics memory
    2) Need to provide an interface which can provide info about the
    sharing of Graphics buffers between the clients.
    
    The client based interface would also aid in debugging of memory
    usage/consumption by each client & debug memleak related issues.
    
    With this new interface,
    1) In case of memleak scenarios, we can easily zero in on the culprit
    client which is unexpectedly holding on the Graphics buffers for an
    inordinate amount of time.
    2) We can get an estimate of the instantaneous memory footprint of
    every Graphics client.
    3) We can now trace all the processes sharing a particular Graphics buffer.
    
    By means of this patch we try to provide a sysfs interface to achieve
    the mentioned functionalities.
    
    There are two files created in sysfs:
    'i915_gem_meminfo' will provide summary of the graphics resources used by
    each graphics client.
    'i915_gem_objinfo' will provide detailed view of each object created by
    individual clients.
    
    Rebased for kernel v4.19 1A (Fan Yugang)
    1) Removes unuseful shmem_inode_info->info_lock per Jeremy's optimization.
    2) Removes unuseful object info for each pid for performance optimization.
    3) Adds CONFIG_DRM_I915_MEMTRACK which depends on
       CONFIG_DRM_I915_CAPTURE_ERROR to pass kernel config test.
    
    Change-Id: If9705d001b922de3570693b2ad39125babd8e860
    Signed-off-by: Sourab Gupta <sourab.gupta@intel.com>
    Sgined-off-by: Deepak S <deepak.s@intel.com>
    Signed-off-by: Hu Beiyuan <beiyuan.hu@intel.com>
    Signed-off-by: Mingwei Wang <mingwei.wang@intel.com>
    Signed-off-by: Harish Krupo <harish.krupo.kps@intel.com>
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Signed-off-by: Yugang Fan

--- drivers/gpu/drm/drm_internal.h
+++ drivers/gpu/drm/drm_internal.h
@@ -48,6 +48,11 @@ void drm_prime_remove_buf_handle_locked(struct drm_prime_file_private *prime_fpr
 					struct dma_buf *dma_buf);
 
 /* drm_drv.c */
+
+#if IS_ENABLED(CONFIG_DRM_I915_MEMTRACK)
+#define DRM_MAGIC_HASH_ORDER  4  /**< Size of key hash table. Must be power of 2. */
+#endif
+
 struct drm_minor *drm_minor_acquire(unsigned int minor_id);
 void drm_minor_release(struct drm_minor *minor);
 
