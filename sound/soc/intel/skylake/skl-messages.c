7a9df0bb7b3325be33d452547b508d2e27ea3ddc
Amadeusz Sławiński <amadeuszx.slawinski@intel.com>
2018-10-15 15:43:05 +0200

    ASoC: Intel: Skylake: print module type instead of id
    When we are printing module params, we were actually printing module id
    instead of type, but debug message was saying that number we get is type.
    So print module type, as it is useful when debugging paths, but also
    keep printing module id, as it is used in all other logs.
    
    Change-Id: I8fabcd3443dd88e9281329a578ece5d9033f0728
    Signed-off-by: Amadeusz Sławiński <amadeuszx.slawinski@intel.com>
    Reviewed-by: Bozek, DominikX <dominikx.bozek@intel.com>
    Reviewed-by: Rojewski, Cezary <cezary.rojewski@intel.com>
    Tested-by: gkblditp <gkblditp@intel.com>

--- sound/soc/intel/skylake/skl-messages.c
+++ sound/soc/intel/skylake/skl-messages.c
@@ -2001,8 +2001,9 @@ static int skl_set_module_format(struct skl_sst *ctx,
 
 	}
 
-	dev_dbg(ctx->dev, "Module type=%d config size: %d bytes\n",
-			module_config->id.module_id, param_size);
+	dev_dbg(ctx->dev, "Module type=%d id=%d config size: %d bytes\n",
+			module_config->m_type, module_config->id.module_id,
+			param_size);
 	print_hex_dump_debug("Module params:", DUMP_PREFIX_OFFSET, 8, 4,
 			*param_data, param_size, false);
 	return 0;
