e787d6f316b11457b6e1b80b18ff4369962380d0
Qi Yadong <yadong.qi@intel.com>
2018-10-17 15:10:41 +0800

    rpmb: mux: add key retrieval for RPMB multiplexor
    Retrieve a RPMB key from a bootloader.
    Currently automotive bootloader and slim bootloader are
    supported.
    
    Change-Id: If5ab4024fc1cf02967fdc88f097f6918d3833b2b
    Signed-off-by: Qi Yadong <yadong.qi@intel.com>
    Signed-off-by: Huang, Yang <yang.huang@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

---
+++ drivers/char/rpmb/mux/key_abl.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0 */
+
+#ifndef __RPMB_KEY_ABL__
+#define __RPMB_KEY_ABL__
+
+int rpmb_key_abl_get(ulong params_addr, const u8 *dev_id, size_t dev_id_len,
+		     size_t max_partition_num, u8 rpmb_key[][RPMB_KEY_LENGTH]);
+
+#endif /* !__RPMB_KEY_ABL__ */
