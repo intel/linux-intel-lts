10e89e9be01233772bc33b3c93e9599a25538822
Philippe Gerum <rpm@xenomai.org>
2018-12-20 15:28:26 +0100

    kasan: irq_pipeline: serialize reports from oob contexts
    KASAN may detect issues in code running out-of-band, so we have to
    serialize the report handler accordingly by turning the regular
    spinlock into a hard lock.
    
    This will trigger massive a latency spot when a report is emitted, but
    since kernel sanity is at stake when this happens, such spike is not
    the most urgent issue.
    
    This also means that the KASAN report lock becomes a raw lock -
    therefore cannot sleep with full preemption enabled.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- mm/kasan/report.c
+++ mm/kasan/report.c
@@ -141,7 +141,7 @@ static void update_kunit_status(bool sync)
 static void update_kunit_status(bool sync) { }
 #endif
 
-static DEFINE_SPINLOCK(report_lock);
+static DEFINE_HARD_SPINLOCK(report_lock);
 
 static void start_report(unsigned long *flags, bool sync)
 {
@@ -153,7 +153,7 @@ static void start_report(unsigned long *flags, bool sync)
 	lockdep_off();
 	/* Make sure we don't end up in loop. */
 	kasan_disable_current();
-	spin_lock_irqsave(&report_lock, *flags);
+	raw_spin_lock_irqsave(&report_lock, *flags);
 	pr_err("==================================================================\n");
 }
 
@@ -163,7 +163,7 @@ static void end_report(unsigned long *flags, void *addr)
 		trace_error_report_end(ERROR_DETECTOR_KASAN,
 				       (unsigned long)addr);
 	pr_err("==================================================================\n");
-	spin_unlock_irqrestore(&report_lock, *flags);
+	raw_spin_unlock_irqrestore(&report_lock, *flags);
 	if (!test_bit(KASAN_BIT_MULTI_SHOT, &kasan_flags))
 		check_panic_on_warn("KASAN");
 	if (kasan_arg_fault == KASAN_ARG_FAULT_PANIC)
