1c8b7e94e15944803a9397b63c37ce75a58ad1a7
Meng Wei <wei.meng@intel.com>
2019-02-11 10:04:24 +0800

    media: intel-ipu4: Fix IPC timeout in ipu buttress authentication
    IRQ for boot_load was not waited as require_resp is not set, causing
    the next command failed to be received.
    This patch add the response setting to make sure IPC procedure is
    completed before sending new command.
    
    Change-Id: Ibf64cd0ca3df36c9c27135d8acd7055267b724d4
    Tracked-On: PKT-1760
    Tracked-On: OAM-73567
    Signed-off-by: khaiwenn <khai.wen.ng@intel.com>
    Signed-off-by: jinchung <jin.chung.teng@intel.com>
    Signed-off-by: Meng Wei <wei.meng@intel.com>

--- drivers/media/pci/intel/ipu-buttress.c
+++ drivers/media/pci/intel/ipu-buttress.c
@@ -368,11 +368,14 @@ EXPORT_SYMBOL_GPL(ipu_buttress_ipc_send_bulk);
 static int
 ipu_buttress_ipc_send(struct ipu_device *isp,
 		      enum ipu_buttress_ipc_domain ipc_domain,
-		      u32 ipc_msg, u32 size)
+		      u32 ipc_msg, u32 size, bool require_resp,
+		      u32 expected_resp)
 {
 	struct ipu_ipc_buttress_bulk_msg msg = {
 		.cmd = ipc_msg,
 		.cmd_size = size,
+		.require_resp = require_resp,
+		.expected_resp = expected_resp,
 	};
 
 	return ipu_buttress_ipc_send_bulk(isp, ipc_domain, &msg, 1);
@@ -875,7 +878,9 @@ int ipu_buttress_authenticate(struct ipu_device *isp)
 	 */
 	dev_info(&isp->pdev->dev, "Sending BOOT_LOAD to CSE\n");
 	rval = ipu_buttress_ipc_send(isp, IPU_BUTTRESS_IPC_CSE,
-				     BUTTRESS_IU2CSEDATA0_IPC_BOOT_LOAD, 1);
+				     BUTTRESS_IU2CSEDATA0_IPC_BOOT_LOAD,
+				     1, 1,
+				     BUTTRESS_CSE2IUDATA0_IPC_BOOT_LOAD_DONE);
 	if (rval) {
 		dev_err(&isp->pdev->dev, "CSE boot_load failed\n");
 		goto iunit_power_off;
@@ -930,8 +935,9 @@ int ipu_buttress_authenticate(struct ipu_device *isp)
 	 */
 	dev_info(&isp->pdev->dev, "Sending AUTHENTICATE_RUN to CSE\n");
 	rval = ipu_buttress_ipc_send(isp, IPU_BUTTRESS_IPC_CSE,
-				     BUTTRESS_IU2CSEDATA0_IPC_AUTHENTICATE_RUN,
-				     1);
+				     BUTTRESS_IU2CSEDATA0_IPC_AUTH_RUN,
+				     1, 1,
+				     BUTTRESS_CSE2IUDATA0_IPC_AUTH_RUN_DONE);
 	if (rval) {
 		dev_err(&isp->pdev->dev, "CSE authenticate_run failed\n");
 		goto iunit_power_off;
