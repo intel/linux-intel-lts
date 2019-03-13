bce47b26cf44f32279c42edd7e2d6ae2e628c089
kgopala2 <karthik.l.gopalakrishnan@intel.com>
2019-03-14 02:13:23 +0800

    media: intel-ipu4: Generate error signal only for faulty stream.
    It is more appropriate to send error codes for faulty stream only
    instead of all active streams.
    
    Change-Id: I3444c27cfdee0469bf48516e8482d29455f9510b
    Tracked-On: OLINUX-2730
    Tracked-On: PKT-1822
    Signed-off-by: Bandi, Kushal <kushal.bandi@intel.com>
    Signed-off-by: kgopala2 <karthik.l.gopalakrishnan@intel.com>

--- drivers/media/pci/intel/ipu4/ipu4-isys-csi2.c
+++ drivers/media/pci/intel/ipu4/ipu4-isys-csi2.c
@@ -54,6 +54,12 @@ static void trigger_error(struct ipu_isys_csi2 *csi2)
 	spin_unlock_irqrestore(&csi2->isys->lock, flags);
 }
 
+void ipu_isys_csi2_trigger_error(struct ipu_isys_csi2 *csi2)
+{
+	csi2->isys->reset_needed = true;
+	trigger_error(csi2);
+}
+
 void ipu_isys_csi2_trigger_error_all(struct ipu_isys *isys)
 {
 	int i;
@@ -339,7 +345,7 @@ void ipu_isys_csi2_error(struct ipu_isys_csi2 *csi2)
 		dev_err_ratelimited(&csi2->isys->adev->dev,
 				"csi2-%i received fatal error\n",
 				csi2->index);
-		ipu_isys_csi2_trigger_error_all(csi2->isys);
+		ipu_isys_csi2_trigger_error(csi2);
 	}
 
 	for (i = 0; i < ARRAY_SIZE(errors); i++) {
