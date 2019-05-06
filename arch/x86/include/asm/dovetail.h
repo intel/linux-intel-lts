b78c575c12480dc700cbfa7e039980995d767bf7
Philippe Gerum <rpm@xenomai.org>
2019-05-06 17:09:23 +0200

    x86: dovetail: enable alternate scheduling
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- arch/x86/include/asm/dovetail.h
+++ arch/x86/include/asm/dovetail.h
@@ -6,4 +6,19 @@
 #ifndef _ASM_X86_DOVETAIL_H
 #define _ASM_X86_DOVETAIL_H
 
+#if !defined(__ASSEMBLY__) && defined(CONFIG_DOVETAIL)
+
+static inline void arch_dovetail_exec_prepare(void)
+{ }
+
+static inline
+void arch_dovetail_switch_prepare(bool leave_inband)
+{ }
+
+static inline
+void arch_dovetail_switch_finish(bool enter_inband)
+{ }
+
+#endif
+
 #endif /* _ASM_X86_DOVETAIL_H */
