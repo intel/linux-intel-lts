ceb853a1c08a71b6df8bb6f041aba19405686851
Philippe Gerum <rpm@xenomai.org>
2019-05-07 08:39:26 +0200

    x86/fpu: dovetail: enable alternate scheduling
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- arch/x86/include/asm/dovetail.h
+++ arch/x86/include/asm/dovetail.h
@@ -8,16 +8,29 @@
 
 #if !defined(__ASSEMBLY__) && defined(CONFIG_DOVETAIL)
 
+#include <asm/fpu/api.h>
+
 static inline void arch_dovetail_exec_prepare(void)
-{ }
+{
+	clear_thread_flag(TIF_NEED_FPU_LOAD);
+}
 
 static inline
 void arch_dovetail_switch_prepare(bool leave_inband)
-{ }
+{
+	if (leave_inband)
+		fpu__suspend_inband();
+}
 
 static inline
 void arch_dovetail_switch_finish(bool enter_inband)
-{ }
+{
+	if (enter_inband)
+		fpu__resume_inband();
+	else if (!(current->flags & PF_KTHREAD) &&
+		test_thread_flag(TIF_NEED_FPU_LOAD))
+		switch_fpu_return();
+}
 
 #endif
 
