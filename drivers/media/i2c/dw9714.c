9dba5ec612261fdf3ff0cfa07f4b9c66506b51f5
qianmenx <qianx.meng@intel.com>
2019-07-25 10:40:28 +0800

    media: intel-ipu4: add header file to some file for build warnings
    add header file to dw9714.c as3638.h ipu-isys-isa-fw.h
      ipu-psys.h
    
    Change-Id: If4c714c4adb771452417e943de1410875cc06e25
    Tracked-On: PKT-2588
    Tracked-On: #H1507330328
    Signed-off-by: qianmenx <qianx.meng@intel.com>
    Signed-off-by: Meng Wei <wei.meng@intel.com>

--- drivers/media/i2c/dw9714.c
+++ drivers/media/i2c/dw9714.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 // Copyright (c) 2015--2017 Intel Corporation.
 
+#include <linux/version.h>
 #include <linux/delay.h>
 #include <linux/i2c.h>
 #include <linux/module.h>
