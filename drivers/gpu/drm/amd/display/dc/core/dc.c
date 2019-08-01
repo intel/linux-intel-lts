d049d4689ce2f55cd634828b17adbc354aedaf43
Ahmad Othman <ahmad.othman@amd.com>
2019-08-01 15:05:04 -0400

    drm/amd/display: Adding VTEM to dc
    [Why]
    Video Timing Extended Metadata packet (VTEM) is required for features
    like VRR and FVA
    
    [How]
    Adding support for VTEM transmission to stream encoders in DCN20 and DCN30
    as part of FVA support
    
    Acked-by: Alan Liu <HaoPing.Liu@amd.com>
    Signed-off-by: Ahmad Othman <ahmad.othman@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc.c
+++ drivers/gpu/drm/amd/display/dc/core/dc.c
@@ -2654,6 +2654,9 @@ static void copy_stream_update_to_stream(struct dc *dc,
 	if (update->hfvsif_infopacket)
 		stream->hfvsif_infopacket = *update->hfvsif_infopacket;
 
+	if (update->vtem_infopacket)
+		stream->vtem_infopacket = *update->vtem_infopacket;
+
 	if (update->vsc_infopacket)
 		stream->vsc_infopacket = *update->vsc_infopacket;
 
@@ -2729,7 +2732,8 @@ static void commit_planes_do_stream_update(struct dc *dc,
 					stream_update->vrr_infopacket ||
 					stream_update->vsc_infopacket ||
 					stream_update->vsp_infopacket ||
-					stream_update->hfvsif_infopacket) {
+					stream_update->hfvsif_infopacket ||
+					stream_update->vtem_infopacket) {
 				resource_build_info_frame(pipe_ctx);
 				dc->hwss.update_info_frame(pipe_ctx);
 
