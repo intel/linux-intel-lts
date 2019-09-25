15b02dab58a361bf09d9e0c17b51ba75a6935dfe
Navid Emamdoost <navid.emamdoost@gmail.com>
2019-09-24 23:23:56 -0500

    drm/amd/display: prevent memory leak
    In dcn*_create_resource_pool the allocated memory should be released if
    construct pool fails.
    
    Change-Id: I24a83555dd34f362bba3a5bd93073c589f980e81
    Tracked-On: PKT-2828
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Navid Emamdoost <navid.emamdoost@gmail.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dce112/dce112_resource.c
+++ drivers/gpu/drm/amd/display/dc/dce112/dce112_resource.c
@@ -1288,6 +1288,7 @@ struct resource_pool *dce112_create_resource_pool(
 	if (construct(num_virtual_links, dc, pool))
 		return &pool->base;
 
+	kfree(pool);
 	BREAK_TO_DEBUGGER();
 	return NULL;
 }
