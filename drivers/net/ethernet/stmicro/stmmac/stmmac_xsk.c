b7f55887f83ec7bdcaaf7044ea787a771295c5fe
Wong, Vincent Por Yin <vincent.por.yin.wong@intel.com>
2019-10-14 14:24:43 +0800

    net: xdp: introduce AF_XDP per-packet time-based scheduling
    Introduce per-packet time-based scheduling via the xdp descriptor. Users
    specify per-packet transmit time via the xdp descriptor's txtime field.
    Users are expected to specify the transmit time sufficiently ahead of
    time based on their system's scheduling latency. Driver is reponsible to
    either drop or transmit already late packets.
    
    Signed-off-by: Wong, Vincent Por Yin <vincent.por.yin.wong@intel.com>
    Signed-off-by: Voon Weifeng <weifeng.voon@intel.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_xsk.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_xsk.c
@@ -730,6 +730,14 @@ static bool stmmac_xmit_zc(struct stmmac_tx_queue *xdp_q, unsigned int budget)
 
 		stmmac_set_desc_addr(priv, tx_desc, dma);
 
+		if (stmmac_enabled_xdp(priv) && desc.txtime > 0) {
+			if (stmmac_set_tbs_launchtime(priv, tx_desc,
+						      desc.txtime)) {
+				netdev_warn(priv->dev, "Launch time setting"
+						       "failed\n");
+			}
+		}
+
 		stmmac_prepare_tx_desc(priv, tx_desc, /* Tx descriptor */
 				       1, /* is first descriptor */
 				       desc.len,
