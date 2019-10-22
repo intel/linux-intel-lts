0f102a850de1b6e6bc6d630be0aa8b9c8a876f66
Chen Meng J <meng.j.chen@intel.com>
2019-10-22 13:00:17 +0800

    media entity graph walk starting from pad
    Add new parameter in media_entity_graph_walk_start()
    Start walking the media graph at a given entity and pad
    
    This function initializes the graph traversal structure to walk
    the entities graph starting at the given entity and pad.
    As some entity may be on the cross node on pipeline, so add one
    parameter to start the graph traverse.
    
    Signed-off-by: Jianxu Zheng <jian.xu.zheng@intel.com>
    Signed-off-by: wu xia <xia.wu@intel.com>
    Signed-off-by: Meng Wei <wei.meng@intel.com>
    Signed-off-by: Chen Meng J <meng.j.chen@intel.com>
    Signed-off-by: zouxiaoh <xiaohong.zou@intel.com>

--- drivers/media/mc/mc-entity.c
+++ drivers/media/mc/mc-entity.c
@@ -316,16 +316,16 @@ void media_graph_walk_cleanup(struct media_graph *graph)
 EXPORT_SYMBOL_GPL(media_graph_walk_cleanup);
 
 void media_graph_walk_start(struct media_graph *graph,
-			    struct media_entity *entity)
+			    struct media_pad *pad)
 {
 	media_entity_enum_zero(&graph->ent_enum);
-	media_entity_enum_set(&graph->ent_enum, entity);
+	media_entity_enum_set(&graph->ent_enum, pad->entity);
 
 	graph->top = 0;
 	graph->stack[graph->top].entity = NULL;
 	stack_push(graph, pad->entity, pad->index, -1);
-	dev_dbg(entity->graph_obj.mdev->dev,
-		"begin graph walk at '%s'\n", entity->name);
+	dev_dbg(pad->entity->graph_obj.mdev->dev,
+		"begin graph walk at '%s'\n", pad->entity->name);
 }
 EXPORT_SYMBOL_GPL(media_graph_walk_start);
 
@@ -471,7 +471,7 @@ __must_check int __media_pipeline_start(struct media_entity *entity,
 			goto error_graph_walk_start;
 	}
 
-	media_graph_walk_start(&pipe->graph, entity);
+	media_graph_walk_start(graph, &entity->pads[0]);
 
 	while ((entity = media_graph_walk_next(graph))) {
 		DECLARE_BITMAP(active, MEDIA_ENTITY_MAX_PADS);
@@ -543,7 +543,7 @@ error:
 	 * Link validation on graph failed. We revert what we did and
 	 * return the error.
 	 */
-	media_graph_walk_start(graph, entity_err);
+	media_graph_walk_start(graph, &entity_err->pads[0]);
 
 	while ((entity_err = media_graph_walk_next(graph))) {
 		/* Sanity check for negative stream_count */
@@ -594,7 +594,7 @@ void __media_pipeline_stop(struct media_entity *entity)
 	if (WARN_ON(!pipe))
 		return;
 
-	media_graph_walk_start(graph, entity);
+	media_graph_walk_start(graph, &entity->pads[0]);
 
 	while ((entity = media_graph_walk_next(graph))) {
 		/* Sanity check for negative stream_count */
