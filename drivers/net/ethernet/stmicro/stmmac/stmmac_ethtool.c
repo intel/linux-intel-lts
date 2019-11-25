intel/mainline-tracking

net: stmmac: gcl errors reporting and its interrupt handling
Enabled interrupt for Constant Gate Control Error (CGCE), Head-of-Line
Blocking due to scheduling error (HLBS) and Head-of-Line Blocking due to
frame size error (HLBF).

CGCE should not happen as the driver has already implemented a check
before applying the settings. CGCE handling is added as a safety
check so that we can catch it if there is such error being fired. For
HLBS, the user will get the info of all the queues that shows this
error. For HLBF, the user will get the info of all the queue with the
latest frame size which causes the error. Frame size 0 indicates no
error.

The ISR handling takes place when EST feature is enabled by user.

This patch is also co-authored by Ong Boon Leong for the hook into
stmmac_ethtool.c to support EST error statistic showing.

Change-Id: I9f4326bc121a5b5e4dcb41820d45e121f1c5c635
Signed-off-by: Voon Weifeng <weifeng.voon@intel.com>
Signed-off-by: Ong Boon Leong <boon.leong.ong@intel.com>

drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
@@ -473,7 +473,14 @@ static void stmmac_get_ethtool_stats(struct net_device *dev,
 				data[j++] = count;
 		}
 	}
-
+	if (priv->hw->tsn_info.cap.est_support) {
+		for (i = 0; i < STMMAC_TSN_STAT_SIZE; i++) {
+			if (!stmmac_dump_tsn_err(priv,
+						 priv->hw, i,
+						 &count, NULL))
+				data[j++] = count;
+		}
+	}
 	/* Update the DMA HW counters for dwmac10/100 */
 	ret = stmmac_dma_diagnostic_fr(priv, &dev->stats, (void *) &priv->xstats,
 			priv->ioaddr);
@@ -512,7 +519,7 @@ static void stmmac_get_ethtool_stats(struct net_device *dev,
 static int stmmac_get_sset_count(struct net_device *netdev, int sset)
 {
 	struct stmmac_priv *priv = netdev_priv(netdev);
-	int i, len, safety_len = 0;
+	int i, len, safety_len = 0, tsn_len = 0;
 
 	switch (sset) {
 	case ETH_SS_STATS:
@@ -529,6 +536,16 @@ static int stmmac_get_sset_count(struct net_device *netdev, int sset)
 			}
 
 			len += safety_len;
+		}
+		if (priv->hw->tsn_info.cap.est_support) {
+			for (i = 0; i < STMMAC_TSN_STAT_SIZE; i++) {
+				if (!stmmac_dump_tsn_err(priv,
+							 priv->hw, i,
+							 NULL, NULL))
+					tsn_len++;
+			}
+
+			len += tsn_len;
 		}
 
 		return len;
@@ -553,6 +570,18 @@ static void stmmac_get_strings(struct net_device *dev, u32 stringset, u8 *data)
 				if (!stmmac_safety_feat_dump(priv,
 							&priv->sstats, i,
 							NULL, &desc)) {
+					memcpy(p, desc, ETH_GSTRING_LEN);
+					p += ETH_GSTRING_LEN;
+				}
+			}
+		}
+		if (priv->hw->tsn_info.cap.est_support) {
+			for (i = 0; i < STMMAC_TSN_STAT_SIZE; i++) {
+				const char *desc;
+
+				if (!stmmac_dump_tsn_err(priv,
+							 priv->hw, i,
+							 NULL, &desc)) {
 					memcpy(p, desc, ETH_GSTRING_LEN);
 					p += ETH_GSTRING_LEN;
 				}

intel/linux-intel-lts

drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
@@ -489,7 +489,14 @@ static void stmmac_get_ethtool_stats(struct net_device *dev,
 				data[j++] = count;
 		}
 	}
-
+	if (priv->hw->tsn_info.cap.est_support) {
+		for (i = 0; i < STMMAC_TSN_STAT_SIZE; i++) {
+			if (!stmmac_dump_tsn_mmc(priv,
+						 priv->hw, i,
+						 &count, NULL))
+				data[j++] = count;
+		}
+	}
 	/* Update the DMA HW counters for dwmac10/100 */
 	ret = stmmac_dma_diagnostic_fr(priv, &dev->stats, (void *) &priv->xstats,
 			priv->ioaddr);
@@ -528,7 +535,7 @@ static void stmmac_get_ethtool_stats(struct net_device *dev,
 static int stmmac_get_sset_count(struct net_device *netdev, int sset)
 {
 	struct stmmac_priv *priv = netdev_priv(netdev);
-	int i, len, safety_len = 0;
+	int i, len, safety_len = 0, tsn_len = 0;
 
 	switch (sset) {
 	case ETH_SS_STATS:
@@ -545,6 +552,16 @@ static int stmmac_get_sset_count(struct net_device *netdev, int sset)
 			}
 
 			len += safety_len;
+		}
+		if (priv->hw->tsn_info.cap.est_support) {
+			for (i = 0; i < STMMAC_TSN_STAT_SIZE; i++) {
+				if (!stmmac_dump_tsn_mmc(priv,
+							 priv->hw, i,
+							 NULL, NULL))
+					tsn_len++;
+			}
+
+			len += tsn_len;
 		}
 
 		return len;
@@ -569,6 +586,18 @@ static void stmmac_get_strings(struct net_device *dev, u32 stringset, u8 *data)
 				if (!stmmac_safety_feat_dump(priv,
 							&priv->sstats, i,
 							NULL, &desc)) {
+					memcpy(p, desc, ETH_GSTRING_LEN);
+					p += ETH_GSTRING_LEN;
+				}
+			}
+		}
+		if (priv->hw->tsn_info.cap.est_support) {
+			for (i = 0; i < STMMAC_TSN_STAT_SIZE; i++) {
+				const char *desc;
+
+				if (!stmmac_dump_tsn_mmc(priv,
+							 priv->hw, i,
+							 NULL, &desc)) {
 					memcpy(p, desc, ETH_GSTRING_LEN);
 					p += ETH_GSTRING_LEN;
 				}