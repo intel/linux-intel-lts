fc0cf6cafa14a0496bd767746e06cebbe3fd0779
Maciej Żenczykowski <maze@google.com>
2019-11-26 16:13:13 -0800

    ANDROID: net: introduce ip_local_unbindable_ports sysctl
    and associated inet_is_local_unbindable_port() helper function:
    use it to make explicitly binding to an unbindable port return
    -EPERM 'Operation not permitted'.
    
    Autobind doesn't honour this new sysctl since:
      (a) you can simply set both if that's the behaviour you desire
      (b) there could be a use for preventing explicit while allowing auto
      (c) it's faster in the relatively critical path of doing port selection
          during connect() to only check one bitmap instead of both
    
    Various ports may have special use cases which are not suitable for
    use by general userspace applications. Currently, ports specified in
    ip_local_reserved_ports sysctl will not be returned only in case of
    automatic port assignment, but nothing prevents you from explicitly
    binding to them - even from an entirely unprivileged process.
    
    In certain cases it is desirable to prevent the host from assigning the
    ports even in case of explicit binds, even from superuser processes.
    
    Example use cases might be:
     - a port being stolen by the nic for remote serial console, remote
       power management or some other sort of debugging functionality
       (crash collection, gdb, direct access to some other microcontroller
       on the nic or motherboard, remote management of the nic itself).
     - a transparent proxy where packets are being redirected: in case
       a socket matches this connection, packets from this application
       would be incorrectly sent to one of the endpoints.
    
    Initially I wanted to solve this problem via the simple one line:
    
    static inline bool inet_port_requires_bind_service(struct net *net, unsigned short port) {
    -       return port < net->ipv4.sysctl_ip_prot_sock;
    +       return port < net->ipv4.sysctl_ip_prot_sock || inet_is_local_reserved_port(net, port);
    }
    
    However, this doesn't work for two reasons:
      (a) it changes userspace visible behaviour of the existing local
          reserved ports sysctl, and there appears to be enough documentation
          on the internet talking about setting it to make this a bad idea
      (b) it doesn't prevent privileged apps from using these ports,
          CAP_BIND_SERVICE is relatively likely to be available to, for example,
          a recursive DNS server so it can listed on port 53, which also needs
          to do src port randomization for outgoing queries due to security
          reasons (and it thus does manual port binding).
    
    If we *know* that certain ports are simply unusable, then it's better
    nothing even gets the opportunity to try to use them.  This way we at
    least get a quick failure, instead of some sort of timeout (or possibly
    even corruption of the data stream of the non-kernel based use case).
    
    Test:
      vm:~# cat /proc/sys/net/ipv4/ip_local_unbindable_ports
    
      vm:~# python -c 'import socket; s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM, 0); s.bind(("::", 3967))'
      vm:~# python -c 'import socket; s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, 0); s.bind(("::", 3967))'
      vm:~# echo 3967 > /proc/sys/net/ipv4/ip_local_unbindable_ports
      vm:~# cat /proc/sys/net/ipv4/ip_local_unbindable_ports
      3967
      vm:~# python -c 'import socket; s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM, 0); s.bind(("::", 3967))'
      socket.error: (1, 'Operation not permitted')
      vm:~# python -c 'import socket; s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, 0); s.bind(("::", 3967))'
      socket.error: (1, 'Operation not permitted')
    
    Cc: Sean Tranchetti <stranche@codeaurora.org>
    Cc: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
    Cc: Eric Dumazet <edumazet@google.com>
    Cc: Linux SCTP <linux-sctp@vger.kernel.org>
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Bug: 140404597
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -798,6 +798,13 @@ static struct ctl_table ipv4_net_table[] = {
 		.proc_handler	= proc_do_large_bitmap,
 	},
 	{
+		.procname	= "ip_local_unbindable_ports",
+		.data		= &init_net.ipv4.sysctl_local_unbindable_ports,
+		.maxlen		= 65536,
+		.mode		= 0644,
+		.proc_handler	= proc_do_large_bitmap,
+	},
+	{
 		.procname	= "ip_no_pmtu_disc",
 		.data		= &init_net.ipv4.sysctl_ip_no_pmtu_disc,
 		.maxlen		= sizeof(u8),
@@ -1610,13 +1617,19 @@ static __net_init int ipv4_sysctl_init_net(struct net *net)
 
 	net->ipv4.sysctl_local_reserved_ports = kzalloc(65536 / 8, GFP_KERNEL);
 	if (!net->ipv4.sysctl_local_reserved_ports)
-		goto err_ports;
+		goto err_reserved_ports;
+
+	net->ipv4.sysctl_local_unbindable_ports = kzalloc(65536 / 8, GFP_KERNEL);
+	if (!net->ipv4.sysctl_local_unbindable_ports)
+		goto err_unbindable_ports;
 
 	proc_fib_multipath_hash_set_seed(net, 0);
 
 	return 0;
 
-err_ports:
+err_unbindable_ports:
+	kfree(net->ipv4.sysctl_local_reserved_ports);
+err_reserved_ports:
 	unregister_net_sysctl_table(net->ipv4.ipv4_hdr);
 err_reg:
 	if (!net_eq(net, &init_net))
@@ -1629,6 +1642,7 @@ static __net_exit void ipv4_sysctl_exit_net(struct net *net)
 {
 	const struct ctl_table *table;
 
+	kfree(net->ipv4.sysctl_local_unbindable_ports);
 	kfree(net->ipv4.sysctl_local_reserved_ports);
 	table = net->ipv4.ipv4_hdr->ctl_table_arg;
 	unregister_net_sysctl_table(net->ipv4.ipv4_hdr);
