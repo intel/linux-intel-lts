79177700058397036df24d6f4609a3284486a756
Christopher Hall <christopher.s.hall@intel.com>
2019-12-11 13:48:49 -0800

    drivers/ptp: Add PEROUT2 ioctl frequency adjustment interface
    The Intel PMC TGPIO controller logic is driven by ART which is not adjust-
    able. Rather than adjusting the clock frequency, an output frequency
    adjustment method is added that doesn't require restarting or resetting
    output.
    
    Output frequency adjustment is achieved by adding a flag to be passed in
    the PEROUT2 ioctl argument. In this case, the driver should disregard the
    'start' time field and adjust the hardware output frequency if periodic
    output is already "running".
    
    For devices with an adjustable clock, the OFFSET_PRECISE* ioctl is used to
    compute the device clock offset with respect to the system clock. For
    non-adjustable clocks, the EVENT_COUNT_TSTAMP2 ioctl adds an analogous
    relation between output edges and elapsed time device time. This tuple is
    captured simultaneously in hardware and is used to precisely compute the
    *actual* average cumulative output frequency relative to the device clock.
    The actual average is used to adjust the output period to achieve the
    *desired* cumulative output frequency.
    
    Signed-off-by: Christopher Hall <christopher.s.hall@intel.com>

--- include/uapi/linux/ptp_clock.h
+++ include/uapi/linux/ptp_clock.h
@@ -35,6 +35,11 @@
 #define PTP_EXTTS_EDGES    (PTP_RISING_EDGE | PTP_FALLING_EDGE)
 
 /*
+ * Bits of the ptp_pin_desc.flags field:
+ */
+#define PTP_PINDESC_EVTCNTVALID	(1<<0)
+
+/*
  * flag fields valid for the new PTP_EXTTS_REQUEST2 ioctl.
  */
 #define PTP_EXTTS_VALID_FLAGS	(PTP_ENABLE_FEATURE |	\
@@ -54,11 +59,13 @@
  * Bits of the ptp_perout_request.flags field:
  */
 #define PTP_PEROUT_ONE_SHOT (1<<0)
+#define PTP_PEROUT_FREQ_ADJ (1<<1)
 
 /*
  * flag fields valid for the new PTP_PEROUT_REQUEST2 ioctl.
  */
-#define PTP_PEROUT_VALID_FLAGS	(PTP_PEROUT_ONE_SHOT)
+#define PTP_PEROUT_VALID_FLAGS	(PTP_PEROUT_ONE_SHOT |	\
+				 PTP_PEROUT_FREQ_ADJ)
 
 /*
  * No flags are valid for the original PTP_PEROUT_REQUEST ioctl
@@ -102,6 +109,14 @@ struct ptp_perout_request {
 	struct ptp_clock_time start;  /* Absolute start time. */
 	struct ptp_clock_time period; /* Desired period, zero means disable. */
 	unsigned int index;           /* Which channel to configure. */
+	unsigned int flags;
+	unsigned int rsv[4];          /* Reserved for future use. */
+};
+
+struct ptp_event_count_tstamp {
+	struct ptp_clock_time device_time;
+	unsigned long long event_count;
+	unsigned int index;
 	unsigned int flags;
 	unsigned int rsv[4];          /* Reserved for future use. */
 };
@@ -165,9 +180,13 @@ struct ptp_pin_desc {
 	 */
 	unsigned int chan;
 	/*
+	 * Per pin capability flag
+	 */
+	unsigned int flags;
+	/*
 	 * Reserved for future use.
 	 */
-	unsigned int rsv[5];
+	unsigned int rsv[4];
 };
 
 #define PTP_CLK_MAGIC '='
@@ -195,6 +214,8 @@ struct ptp_pin_desc {
 	_IOWR(PTP_CLK_MAGIC, 17, struct ptp_sys_offset_precise)
 #define PTP_SYS_OFFSET_EXTENDED2 \
 	_IOWR(PTP_CLK_MAGIC, 18, struct ptp_sys_offset_extended)
+#define PTP_EVENT_COUNT_TSTAMP2 \
+	_IOWR(PTP_CLK_MAGIC, 19, struct ptp_event_count_tstamp)
 
 struct ptp_extts_event {
 	struct ptp_clock_time t; /* Time event occured. */
