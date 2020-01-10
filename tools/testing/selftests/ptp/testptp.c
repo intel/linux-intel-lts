df1a1c9354ff655fa225dfb39f7aabbefe5770dc
Rajvi Jingar <rajvi.jingar@intel.com>
2020-01-10 15:57:32 -0800

    Added single-shot output mode option for TGPIO pin
    Added an option to configure TGPIO pin for single-shot output mode.
    
    Signed-off-by: Rajvi Jingar <rajvi.jingar@intel.com>

--- tools/testing/selftests/ptp/testptp.c
+++ tools/testing/selftests/ptp/testptp.c
@@ -179,6 +179,8 @@ static void usage(char *progname)
 		"            0 - none\n"
 		"            1 - external time stamp\n"
 		"            2 - periodic output\n"
+		" -O         enable single shot output for TGPIO pins\n"
+		"            this option is ignored for period val greater than 0\n"
 		" -p val     enable output with a period of 'val' nanoseconds\n"
 		"            period val 0 to set single shot output for TGPIO pins\n"
 		" -P val     enable or disable (val=1|0) the system clock PPS\n"
@@ -222,6 +224,7 @@ int main(int argc, char *argv[])
 	int pct_offset = 0;
 	int n_samples = 0;
 	int perout = -1;
+	int single_shot = -1;
 	int new_period = -1;
 	int pin_index = -1, pin_func;
 	int pps = -1;
@@ -234,7 +237,7 @@ int main(int argc, char *argv[])
 
 	progname = strrchr(argv[0], '/');
 	progname = progname ? 1+progname : argv[0];
-	while (EOF != (c = getopt(argc, argv, "a:cd:e:f:EGghi:k:lL:p:P:sSt:T:z"))) {
+	while (EOF != (c = getopt(argc, argv, "a:cd:e:f:EGghi:k:lL:Op:P:sSt:T:z"))) {
 		switch (c) {
 		case 'a':
 			new_period = atoi(optarg);
@@ -276,6 +279,9 @@ int main(int argc, char *argv[])
 				usage(progname);
 				return -1;
 			}
+			break;
+		case 'O':
+			single_shot = 1;
 			break;
 		case 'p':
 			perout = atoi(optarg);
