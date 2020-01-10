9adfff5e28ca262012a2fb5cf28f1fd9a3460881
Rajvi Jingar <rajvi.jingar@intel.com>
2020-01-10 15:47:12 -0800

    Added single-shot output mode support for TGPIO
    This patch adds an example to configure timed GPIO pin to single-shot
    output mode.
    
    Signed-off-by: Rajvi Jingar <rajvi.jingar@intel.com>

--- tools/testing/selftests/ptp/testptp.c
+++ tools/testing/selftests/ptp/testptp.c
@@ -26,6 +26,7 @@
 #include <linux/ptp_clock.h>
 
 #define DEVICE "/dev/ptp0"
+#define NSEC_PER_SEC 1000000000L
 
 #ifndef ADJ_SETOFFSET
 #define ADJ_SETOFFSET 0x0100
@@ -134,6 +135,7 @@ static void usage(char *progname)
 		"            1 - external time stamp\n"
 		"            2 - periodic output\n"
 		" -p val     enable output with a period of 'val' nanoseconds\n"
+		"            period val 0 to set single shot output for TGPIO pins\n"
 		" -P val     enable or disable (val=1|0) the system clock PPS\n"
 		" -s         set the ptp clock time from the system time\n"
 		" -S         set the system time from the ptp clock time\n"
@@ -420,7 +422,13 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	if (perout >= 0) {
+	if (perout >= 0 || single_shot == 1) {
+		memset(&desc, 0, sizeof(desc));
+		desc.index = index;
+		if (ioctl(fd, PTP_PIN_GETFUNC2, &desc)) {
+			perror("PTP_PIN_GETFUNC2");
+		}
+
 		if (clock_gettime(clkid, &ts)) {
 			perror("clock_gettime");
 			return -1;
@@ -430,11 +438,21 @@ int main(int argc, char *argv[])
 		perout_request.start.sec = ts.tv_sec + 2;
 		perout_request.start.nsec = 0;
 		perout_request.period.sec = 0;
-		perout_request.period.nsec = perout;
-		if (ioctl(fd, PTP_PEROUT_REQUEST, &perout_request)) {
-			perror("PTP_PEROUT_REQUEST");
+		if (perout <= 0 && (desc.flags & PTP_PINDESC_INPUTDISABLE)) {
+			perout_request.period.nsec = NSEC_PER_SEC / 2;
+			perout_request.flags = PTP_PEROUT_ONE_SHOT;
+			if (ioctl(fd, PTP_PEROUT_REQUEST2, &perout_request)) {
+				perror("PTP_PEROUT_REQUEST2");
+			} else {
+				puts("single shot output request okay");
+			}
 		} else {
-			puts("periodic output request okay");
+			perout_request.period.nsec = perout;
+			if (ioctl(fd, PTP_PEROUT_REQUEST2, &perout_request)) {
+				perror("PTP_PEROUT_REQUEST2");
+			} else {
+				puts("periodic output request okay");
+			}
 		}
 	}
 
