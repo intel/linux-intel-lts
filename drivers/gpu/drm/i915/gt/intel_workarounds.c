bcc8b5a187b3a628b5e78511b541d50e79bc8c1a
Mika Kuoppala <mika.kuoppala@linux.intel.com>
2020-02-12 18:57:07 +0200

    drm/i915: Use engine wa list for Wa_1607090982
    This is in mcr range of register, thus we can only verify
    it through mmio. Use engine wa list with mcr range verification
    skip.
    
    Fixes: 0db1a5f8706a ("drm/i915: Implement Wa_1607090982")
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200212165707.11143-1-mika.kuoppala@linux.intel.com

--- drivers/gpu/drm/i915/gt/intel_workarounds.c
+++ drivers/gpu/drm/i915/gt/intel_workarounds.c
@@ -1355,6 +1355,11 @@ rcs_engine_wa_init(struct intel_engine_cs *engine, struct i915_wa_list *wal)
 		wa_write_or(wal,
 			    GEN7_FF_THREAD_MODE,
 			    GEN12_FF_TESSELATION_DOP_GATE_DISABLE);
+
+		/* Wa_1606931601:tgl */
+		wa_masked_en(wal,
+			     GEN7_ROW_CHICKEN2,
+			     GEN12_DISABLE_EARLY_READ);
 	}
 
 	if (IS_GEN(i915, 11)) {
