16989436d6814b203679c110e7873817725be4fd
José Roberto de Souza <jose.souza@intel.com>
2020-02-27 14:01:00 -0800

    drm/i915/tgl: Add Wa number to WaAllowPMDepthAndInvocationCountAccessFromUMD
    Just to make easier to check that the Wa was implemetend when
    comparing to the number in BSpec.
    
    BSpec: 52890
    Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200227220101.321671-10-jose.souza@intel.com

--- drivers/gpu/drm/i915/gt/intel_workarounds.c
+++ drivers/gpu/drm/i915/gt/intel_workarounds.c
@@ -1251,6 +1251,7 @@ static void tgl_whitelist_build(struct intel_engine_cs *engine)
 	case RENDER_CLASS:
 		/*
 		 * WaAllowPMDepthAndInvocationCountAccessFromUMD:tgl
+		 * Wa_1408556865:tgl
 		 *
 		 * This covers 4 registers which are next to one another :
 		 *   - PS_INVOCATION_COUNT
