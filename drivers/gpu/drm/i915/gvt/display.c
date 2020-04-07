projectacrn/acrn-kernel

drm/i915/gvt: enable gvt gop support
Reserve 8MB memory block as graphics memory and pass it to Gop driver as
the guest framebuffer. This framebuffer guest physical address starts
from 0xDF000000 and works with fixed display mode 1920x1080@32bpp

Tracked-On: projectacrn/acrn-hypervisor#3624
Co-Developed-by: He Min <min.he@intel.com>
Signed-off-by: Liu Xinyun <xinyun.liu@intel.com>
Reviewed-by: Zhao Yakui <yakui.zhao@intel.com>

drivers/gpu/drm/i915/gvt/display.c
@@ -646,3 +646,144 @@ void intel_vgpu_reset_display(struct intel_vgpu *vgpu)
 {
 	emulate_monitor_status_change(vgpu);
 }
+
+/*
+ * prepare_gop_fb will allocate a arrange of memory, then map them into the
+ * ggtt table of the guest partition in the aperture.
+ */
+static int prepare_gop_fb(struct intel_vgpu *vgpu, u32 size)
+{
+	struct drm_i915_private *dev_priv = vgpu->gvt->dev_priv;
+	struct page **pages = NULL;
+	u32 count, npages = size >> PAGE_SHIFT;
+	struct i915_ggtt *ggtt = &dev_priv->ggtt;
+	struct i915_vma vma;
+	struct drm_mm_node *node = &vgpu->gm.high_gm_node;
+	struct sg_table st;
+	unsigned int cache_level = HAS_LLC(dev_priv) ?
+				I915_CACHE_LLC : I915_CACHE_NONE;
+	int ret = 0;
+
+	pages = kmalloc_array(npages, sizeof(struct page *), GFP_KERNEL);
+	if (!pages)
+		return -ENOMEM;
+
+	for (count = 0; count < npages; count++) {
+		struct page *page = alloc_page(GFP_KERNEL);
+		if (!page) {
+			ret = -ENOMEM;
+			goto free_pgs;
+		}
+		pages[count] = page;
+
+		intel_gvt_hypervisor_map_gfn_to_mfn (vgpu,
+				(GOP_FB_BASE >> PAGE_SHIFT) + count,
+				page_to_pfn(page), 1, true);
+	}
+
+	ret = sg_alloc_table_from_pages(&st, pages, npages,
+			0, npages << PAGE_SHIFT, GFP_KERNEL);
+	if (ret)
+		goto free_pgs;
+
+	if(!dma_map_sg(&dev_priv->drm.pdev->dev, st.sgl, st.nents,
+				PCI_DMA_BIDIRECTIONAL)) {
+		ret = -ENOMEM;
+		goto free_sg;
+	}
+
+	memset(&vma, 0, sizeof(vma));
+	vma.node.start = node->start;
+	vma.node.size = size;
+	vma.pages = &st;
+	ggtt->vm.insert_entries(&ggtt->vm, &vma, cache_level, 0);
+	sg_free_table(&st);
+
+	vgpu->gm.gop_fb_pages = pages;
+	vgpu->gm.gop_fb_size = count;
+	return 0;
+
+free_sg:
+	sg_free_table(&st);
+
+free_pgs:
+	release_pages(pages, count);
+	kfree(pages);
+	return ret;
+}
+
+#define GOP_DISPLAY_WIDTH 1920
+#define GOP_DISPLAY_HEIGHT 1080
+static int setup_gop_display(struct intel_vgpu *vgpu)
+{
+	int ret = 0;
+	unsigned int pipe, plane;
+	struct drm_i915_private *dev_priv = vgpu->gvt->dev_priv;
+	bool found = false;
+	u32 width = GOP_DISPLAY_WIDTH, height = GOP_DISPLAY_HEIGHT;
+	u32 stride, ctl, surf;
+	unsigned long irqflags;
+
+
+	/* we will display the gop output on the first plane the vgpu ownes */
+	for_each_pipe(dev_priv, pipe) {
+		for_each_universal_plane(dev_priv, pipe, plane) {
+			if(vgpu->gvt->pipe_info[pipe].plane_owner[plane]
+				    == vgpu->id) {
+				found = true;
+				break;
+			}
+		}
+		if (found)
+			break;
+	}
+
+	if (!found) {
+		gvt_dbg_dpy("Failed to find owned plane for %d", vgpu->id);
+		return -ENODEV;
+	}
+
+	/* Sizes are 0 based */
+	stride = width * 4 / 64; /* 32bit per pixel */
+	width--;
+	height--;
+	surf = vgpu->gm.high_gm_node.start;
+	ctl = PLANE_CTL_ENABLE | PLANE_CTL_FORMAT_XRGB_8888;
+	ctl |= PLANE_CTL_PIPE_GAMMA_ENABLE |
+		PLANE_CTL_PIPE_CSC_ENABLE |
+		PLANE_CTL_PLANE_GAMMA_DISABLE;
+	spin_lock_irqsave(&dev_priv->uncore.lock, irqflags);
+	I915_WRITE_FW(PLANE_OFFSET(pipe, plane), 0);
+	I915_WRITE_FW(PLANE_STRIDE(pipe, plane), stride);
+	I915_WRITE_FW(PLANE_SIZE(pipe, plane), (height << 16) | width);
+	I915_WRITE_FW(PLANE_AUX_DIST(pipe, plane), 0xFFFFF000);
+	I915_WRITE_FW(PLANE_AUX_OFFSET(pipe, plane), 0);
+	I915_WRITE_FW(PLANE_POS(pipe, plane), 0);
+	I915_WRITE_FW(PLANE_CTL(pipe, plane), ctl);
+	I915_WRITE_FW(PLANE_SURF(pipe, plane), surf);
+	POSTING_READ_FW(PLANE_SURF(pipe, plane));
+	spin_unlock_irqrestore(&dev_priv->uncore.lock, irqflags);
+	return ret;
+}
+
+#define GOP_FB_SIZE 0x800000  /* 8M FB size */
+int intel_vgpu_g2v_setup_gop(struct intel_vgpu *vgpu)
+{
+	int ret = 0;
+
+	if(vgpu->gm.gop_fb_pages)
+		goto Done;
+
+	ret = prepare_gop_fb(vgpu, GOP_FB_SIZE);
+	if (ret) {
+		gvt_dbg_dpy("gop prepared failed %d\n", ret);
+		goto Done;
+	}
+
+	ret = setup_gop_display(vgpu);
+	if (ret) {
+		gvt_dbg_dpy("gop display setup failed %d\n", ret);
+	}
+Done:
+	return 0;
+}

intel/linux-intel-lts

drivers/gpu/drm/i915/gvt/display.c
@@ -677,3 +677,144 @@ void intel_vgpu_reset_display(struct intel_vgpu *vgpu)
 {
 	emulate_monitor_status_change(vgpu);
 }
+
+/*
+ * prepare_gop_fb will allocate a arrange of memory, then map them into the
+ * ggtt table of the guest partition in the aperture.
+ */
+static int prepare_gop_fb(struct intel_vgpu *vgpu, u32 size)
+{
+	struct drm_i915_private *dev_priv = vgpu->gvt->dev_priv;
+	struct page **pages = NULL;
+	u32 count, npages = size >> PAGE_SHIFT;
+	struct i915_ggtt *ggtt = &dev_priv->ggtt;
+	struct i915_vma vma;
+	struct drm_mm_node *node = &vgpu->gm.high_gm_node;
+	struct sg_table st;
+	unsigned int cache_level = HAS_LLC(dev_priv) ?
+				I915_CACHE_LLC : I915_CACHE_NONE;
+	int ret = 0;
+
+	pages = kmalloc_array(npages, sizeof(struct page *), GFP_KERNEL);
+	if (!pages)
+		return -ENOMEM;
+
+	for (count = 0; count < npages; count++) {
+		struct page *page = alloc_page(GFP_KERNEL);
+		if (!page) {
+			ret = -ENOMEM;
+			goto free_pgs;
+		}
+		pages[count] = page;
+
+		intel_gvt_hypervisor_map_gfn_to_mfn (vgpu,
+				(GOP_FB_BASE >> PAGE_SHIFT) + count,
+				page_to_pfn(page), 1, true);
+	}
+
+	ret = sg_alloc_table_from_pages(&st, pages, npages,
+			0, npages << PAGE_SHIFT, GFP_KERNEL);
+	if (ret)
+		goto free_pgs;
+
+	if (!dma_map_sg(&dev_priv->drm.pdev->dev, st.sgl, st.nents,
+				PCI_DMA_BIDIRECTIONAL)) {
+		ret = -ENOMEM;
+		goto free_sg;
+	}
+
+	memset(&vma, 0, sizeof(vma));
+	vma.node.start = node->start;
+	vma.node.size = size;
+	vma.pages = &st;
+	ggtt->vm.insert_entries(&ggtt->vm, &vma, cache_level, 0);
+	sg_free_table(&st);
+
+	vgpu->gm.gop_fb_pages = pages;
+	vgpu->gm.gop_fb_size = count;
+	return 0;
+
+free_sg:
+	sg_free_table(&st);
+
+free_pgs:
+	release_pages(pages, count);
+	kfree(pages);
+	return ret;
+}
+
+#define GOP_DISPLAY_WIDTH 1920
+#define GOP_DISPLAY_HEIGHT 1080
+static int setup_gop_display(struct intel_vgpu *vgpu)
+{
+	int ret = 0;
+	unsigned int pipe, plane;
+	struct drm_i915_private *dev_priv = vgpu->gvt->dev_priv;
+	bool found = false;
+	u32 width = GOP_DISPLAY_WIDTH, height = GOP_DISPLAY_HEIGHT;
+	u32 stride, ctl, surf;
+	unsigned long irqflags;
+
+
+	/* we will display the gop output on the first plane the vgpu ownes */
+	for_each_pipe(dev_priv, pipe) {
+		for_each_universal_plane(dev_priv, pipe, plane) {
+			if (vgpu->gvt->pipe_info[pipe].plane_owner[plane]
+					== vgpu->id) {
+				found = true;
+				break;
+			}
+		}
+		if (found)
+			break;
+	}
+
+	if (!found) {
+		gvt_dbg_dpy("Failed to find owned plane for %d", vgpu->id);
+		return -ENODEV;
+	}
+
+	/* Sizes are 0 based */
+	stride = width * 4 / 64; /* 32bit per pixel */
+	width--;
+	height--;
+	surf = vgpu->gm.high_gm_node.start;
+	ctl = PLANE_CTL_ENABLE | PLANE_CTL_FORMAT_XRGB_8888;
+	ctl |= PLANE_CTL_PIPE_GAMMA_ENABLE |
+		PLANE_CTL_PIPE_CSC_ENABLE |
+		PLANE_CTL_PLANE_GAMMA_DISABLE;
+	spin_lock_irqsave(&dev_priv->uncore.lock, irqflags);
+	I915_WRITE_FW(PLANE_OFFSET(pipe, plane), 0);
+	I915_WRITE_FW(PLANE_STRIDE(pipe, plane), stride);
+	I915_WRITE_FW(PLANE_SIZE(pipe, plane), (height << 16) | width);
+	I915_WRITE_FW(PLANE_AUX_DIST(pipe, plane), 0xFFFFF000);
+	I915_WRITE_FW(PLANE_AUX_OFFSET(pipe, plane), 0);
+	I915_WRITE_FW(PLANE_POS(pipe, plane), 0);
+	I915_WRITE_FW(PLANE_CTL(pipe, plane), ctl);
+	I915_WRITE_FW(PLANE_SURF(pipe, plane), surf);
+	I915_READ_FW(PLANE_SURF(pipe, plane));
+	spin_unlock_irqrestore(&dev_priv->uncore.lock, irqflags);
+	return ret;
+}
+
+#define GOP_FB_SIZE 0x800000  /* 8M FB size */
+int intel_vgpu_g2v_setup_gop(struct intel_vgpu *vgpu)
+{
+	int ret = 0;
+
+	if (vgpu->gm.gop_fb_pages)
+		goto Done;
+
+	ret = prepare_gop_fb(vgpu, GOP_FB_SIZE);
+	if (ret) {
+		gvt_dbg_dpy("gop prepared failed %d\n", ret);
+		goto Done;
+	}
+
+	ret = setup_gop_display(vgpu);
+	if (ret) {
+		gvt_dbg_dpy("gop display setup failed %d\n", ret);
+	}
+Done:
+	return 0;
+}