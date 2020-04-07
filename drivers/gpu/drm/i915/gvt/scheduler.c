projectacrn/acrn-kernel

drm/i915/gvt: Add a module parameter to disable GPU frequency adjustment for VGPU workload
Add a module parameter to disable GPU frequency adjustment for VGPU workload, by default this parameter is false.

Tracked-On: projectacrn/acrn-hypervisor#3918

Signed-off-by: Junming Liu <junming.liu@intel.com>
Reviewed-by: Zhao Yakui <yakui.zhao@intel.com>

drivers/gpu/drm/i915/gvt/scheduler.c
@@ -314,7 +314,8 @@ static int shadow_context_status_change(struct notifier_block *nb,
 
 	switch (action) {
 	case INTEL_CONTEXT_SCHEDULE_IN:
-		schedule_work(&gvt->active_hp_work);
+		if(!i915_modparams.disable_hp_work)
+			schedule_work(&gvt->active_hp_work);
 		if(!i915_modparams.enable_context_restore) {
 			atomic_set(&workload->shadow_ctx_active, 1);
 			break;

drivers/gpu/drm/i915/i915_params.c
@@ -261,6 +261,9 @@ i915_param_named_unsafe(domain_plane_owners, ullong, 0400,
 i915_param_named_unsafe(enable_context_restore, bool, 0400,
 	"To togger non-context regs save/restore feature(default:false)");
 
+i915_param_named(disable_hp_work, bool, 0400,
+	"To disable active the high-performance mode during vGPU busy(default:false)");
+
 static __always_inline void _print_param(struct drm_printer *p,
 					 const char *name,
 					 const char *type,

drivers/gpu/drm/i915/i915_params.h
@@ -78,7 +78,8 @@ struct drm_printer;
 	param(int, gvt_workload_priority, 0) \
 	param(bool, enable_initial_modeset, false) \
 	param(bool, enable_gvt, false) \
-	param(bool, enable_context_restore, false)
+	param(bool, enable_context_restore, false) \
+	param(bool, disable_hp_work, false)
 
 #define MEMBER(T, member, ...) T member;
 struct i915_params {

intel/linux-intel-lts

Signed-off-by: Liu Xinyun <xinyun.liu@intel.com>

drivers/gpu/drm/i915/gvt/scheduler.c
@@ -277,7 +277,9 @@ static int shadow_context_status_change(struct notifier_block *nb,
 
 	switch (action) {
 	case INTEL_CONTEXT_SCHEDULE_IN:
-		schedule_work(&gvt->active_hp_work);
+		if (!i915_modparams.enable_hp_work)
+			schedule_work(&gvt->active_hp_work);
+
 		if (!i915_modparams.enable_context_restore) {
 			atomic_set(&workload->shadow_ctx_active, 1);
 			break;

drivers/gpu/drm/i915/i915_params.c
@@ -266,6 +266,9 @@ i915_param_named_unsafe(domain_plane_owners, ulong, 0400,
 i915_param_named_unsafe(enable_context_restore, bool, 0400,
 	"To togger non-context regs save/restore feature(default:false)");
 
+i915_param_named(enable_hp_work, bool, 0400,
+	"To enable active the high-performance mode during vGPU busy(default:false)");
+
 static __always_inline void _print_param(struct drm_printer *p,
 					 const char *name,
 					 const char *type,

drivers/gpu/drm/i915/i915_params.h
@@ -85,7 +85,8 @@ struct drm_printer;
 	param(int, gvt_workload_priority, 0) \
 	param(unsigned long, avail_planes_per_pipe, 0) \
 	param(unsigned long, domain_plane_owners, 0) \
-	param(bool, enable_context_restore, false)
+	param(bool, enable_context_restore, false) \
+	param(bool, enable_hp_work, false)
 
 
 #define MEMBER(T, member, ...) T member;