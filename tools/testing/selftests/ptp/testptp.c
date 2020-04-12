666290847b7a04207132353f2130b215c0525170
hkoayx <henry.shueh.tzingx.koay@intel.com>
2020-04-13 01:11:09 +0800

    ptp: Introduce PTP_PINDESC_INPUTPOLL for Intel PMC TGPIO
    This patch is to align to the current implementation of Intel PMC TGPIO
    usage of INPUTDESC flag for non-interrupt based input. This will be
    removed once the migration of TGPIO to GPIO subsystem.
    
    Signed-off-by: Raymond Tan <raymond.tan@intel.com>

--- tools/testing/selftests/ptp/testptp.c
+++ tools/testing/selftests/ptp/testptp.c
@@ -487,7 +487,7 @@ int main(int argc, char *argv[])
 		if (ioctl(fd, PTP_PIN_GETFUNC2, &desc)) {
 			perror("PTP_PIN_GETFUNC2");
 		}
-		if (!(desc.flags & PTP_PINDESC_INPUTDISABLE)) {
+		if (!(desc.flags & PTP_PINDESC_INPUTPOLL)) {
 			for (; extts; extts--) {
 				cnt = read(fd, &event, sizeof(event));
 				if (cnt != sizeof(event)) {
@@ -557,7 +557,7 @@ int main(int argc, char *argv[])
 		perout_request.start.sec = ts.tv_sec + 2;
 		perout_request.start.nsec = 0;
 		perout_request.period.sec = 0;
-		if (perout <= 0 && (desc.flags & PTP_PINDESC_INPUTDISABLE)) {
+		if (perout <= 0 && (desc.flags & PTP_PINDESC_INPUTPOLL)) {
 			perout_request.period.nsec = NSEC_PER_SEC / 2;
 			perout_request.flags = PTP_PEROUT_ONE_SHOT;
 			if (ioctl(fd, PTP_PEROUT_REQUEST2, &perout_request)) {
