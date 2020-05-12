609f24eceac5bc30ee8dabb2168eba914c1a18e0
Philippe Gerum <rpm@xenomai.org>
2020-05-12 18:08:35 +0200

    ARM: dovetail: route traps
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- arch/arm/include/asm/dovetail.h
+++ arch/arm/include/asm/dovetail.h
@@ -6,4 +6,15 @@
 #ifndef _ASM_ARM_DOVETAIL_H
 #define _ASM_ARM_DOVETAIL_H
 
+/* ARM traps */
+#define ARM_TRAP_ACCESS		0	/* Data or instruction access exception */
+#define ARM_TRAP_SECTION	1	/* Section fault */
+#define ARM_TRAP_DABT		2	/* Generic data abort */
+#define ARM_TRAP_PABT		3	/* Prefetch abort */
+#define ARM_TRAP_BREAK		4	/* Instruction breakpoint */
+#define ARM_TRAP_FPU		5	/* Floating point exception */
+#define ARM_TRAP_VFP		6	/* VFP floating point exception */
+#define ARM_TRAP_UNDEFINSTR	7	/* Undefined instruction */
+#define ARM_TRAP_ALIGNMENT	8	/* Unaligned access exception */
+
 #endif /* _ASM_ARM_DOVETAIL_H */
