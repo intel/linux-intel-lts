f0301abffbcb736f35085d9ff57cfb36db43be39
Philippe Gerum <rpm@xenomai.org>
2020-05-12 18:08:35 +0200

    ARM: dovetail: route traps
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- arch/arm/kernel/ptrace.c
+++ arch/arm/kernel/ptrace.c
@@ -205,7 +205,9 @@ void ptrace_break(struct pt_regs *regs)
 
 static int break_trap(struct pt_regs *regs, unsigned int instr)
 {
+	oob_trap_notify(ARM_TRAP_BREAK, regs);
 	ptrace_break(regs);
+	oob_trap_unwind(ARM_TRAP_BREAK, regs);
 	return 0;
 }
 
