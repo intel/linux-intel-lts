minhe1/acrn-kernel

drm/i915/gvt: force to active the high-performance mode during vGPU busy
With the RPS interrupt, KMD can adjust the GPU frequency dynamically for
power saving. It works well in the non-virtualized environment, but there
is more latency imported by VMM and virtual interrupt handler which may
break the RPS policy work model, and GPU works in inefficient mode. Here
we force to active the high-performance mode when detect vgpu is busy until
the GPU runs into idle.

Signed-off-by: Weinan Li <weinan.z.li@intel.com>
Signed-off-by: Min He <min.he@intel.com>

drivers/gpu/drm/i915/gvt/scheduler.c
@@ -265,6 +265,22 @@ static void save_ring_hw_state(struct intel_vgpu *vgpu, int ring_id)
 }
 */
 
+static void active_hp_work(struct work_struct *work)
+{
+	struct intel_gvt *gvt =
+		container_of(work, struct intel_gvt, active_hp_work);
+	struct drm_i915_private *dev_priv = gvt->dev_priv;
+
+	gen6_disable_rps_interrupts(dev_priv);
+
+	if (READ_ONCE(dev_priv->gt_pm.rps.cur_freq) <
+	    READ_ONCE(dev_priv->gt_pm.rps.rp0_freq)) {
+		mutex_lock(&dev_priv->pcu_lock);
+		intel_set_rps(dev_priv, dev_priv->gt_pm.rps.rp0_freq);
+		mutex_unlock(&dev_priv->pcu_lock);
+	}
+}
+
 static int shadow_context_status_change(struct notifier_block *nb,
 		unsigned long action, void *data)
 {
@@ -284,6 +300,7 @@ static int shadow_context_status_change(struct notifier_block *nb,
 
 	switch (action) {
 	case INTEL_CONTEXT_SCHEDULE_IN:
+		schedule_work(&gvt->active_hp_work);
 		atomic_set(&workload->shadow_ctx_active, 1);
 		break;
 	case INTEL_CONTEXT_SCHEDULE_OUT:
@@ -1170,6 +1187,8 @@ int intel_gvt_init_workload_scheduler(struct intel_gvt *gvt)
 		atomic_notifier_chain_register(&engine->context_status_notifier,
 					&gvt->shadow_ctx_notifier_block[i]);
 	}
+	INIT_WORK(&gvt->active_hp_work, active_hp_work);
+
 	return 0;
 err:
 	intel_gvt_clean_workload_scheduler(gvt);

intel/linux-intel-lts

(cherry picked from commit c0fd26d469ba5d29ea4be31cf25003f9e3a058c3)
drm/i915/gvt: refine the active_high_performance_mode code
move the force active high-performance mode logic into one work item to
avoid long time softirq delay and suspicious RCU usage.
(cherry picked from commit f63219399ff3ff4d7be6071cfe09d32faa5a1ba2)
(cherry picked from commit 82561366c8e6d1d413479c03b40b1e6464b97851)
Signed-off-by: Colin Xu <colin.xu@intel.com>

drivers/gpu/drm/i915/gvt/scheduler.c
@@ -39,6 +39,7 @@
 #include "gem/i915_gem_pm.h"
 #include "gt/intel_context.h"
 #include "gt/intel_ring.h"
+#include "gt/intel_rps.h"
 
 #include "i915_drv.h"
 #include "gvt.h"
@@ -224,6 +225,23 @@ static void save_ring_hw_state(struct intel_vgpu *vgpu, int ring_id)
 	vgpu_vreg(vgpu, i915_mmio_reg_offset(reg)) = I915_READ_FW(reg);
 }
 
+static void active_hp_work(struct work_struct *work)
+{
+	struct intel_gvt *gvt =
+		container_of(work, struct intel_gvt, active_hp_work);
+	struct drm_i915_private *dev_priv = gvt->dev_priv;
+	struct intel_rps *rps = &dev_priv->gt.rps;
+
+	rps_disable_interrupts(rps);
+
+	if (READ_ONCE(dev_priv->gt.rps.cur_freq) !=
+	    READ_ONCE(dev_priv->gt.rps.max_freq)) {
+		mutex_lock(&rps->lock);
+		intel_rps_set(rps, dev_priv->gt.rps.max_freq);
+		mutex_unlock(&rps->lock);
+	}
+}
+
 static int shadow_context_status_change(struct notifier_block *nb,
 		unsigned long action, void *data)
 {
@@ -265,6 +283,7 @@ static int shadow_context_status_change(struct notifier_block *nb,
 			gvt_dbg_sched("skip ring %d mmio switch for vgpu%d\n",
 				      ring_id, workload->vgpu->id);
 		spin_unlock_irqrestore(&scheduler->mmio_context_lock, flags);
+		schedule_work(&gvt->active_hp_work);
 		atomic_set(&workload->shadow_ctx_active, 1);
 		break;
 	case INTEL_CONTEXT_SCHEDULE_OUT:
@@ -1125,6 +1144,8 @@ int intel_gvt_init_workload_scheduler(struct intel_gvt *gvt)
 		atomic_notifier_chain_register(&engine->context_status_notifier,
 					&gvt->shadow_ctx_notifier_block[i]);
 	}
+	INIT_WORK(&gvt->active_hp_work, active_hp_work);
+
 	return 0;
 err:
 	intel_gvt_clean_workload_scheduler(gvt);