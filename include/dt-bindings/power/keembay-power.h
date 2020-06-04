aef34cececb91609ca492fac2aaafd32f23974fb
Daniele Alessandrelli <daniele.alessandrelli@intel.com>
2020-06-04 14:13:18 +0100

    dt-bindings: arm: Add Keem Bay bindings
    Document Intel Movidius SoC code-named Keem Bay, along with the Keem Bay
    EVM board.
    
    Reviewed-by: Dinh Nguyen <dinguyen@kernel.org>
    Signed-off-by: Daniele Alessandrelli <daniele.alessandrelli@intel.com>

---
+++ include/dt-bindings/power/keembay-power.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020 Intel Corporation.
+ *
+ * Device tree defines for power domains in Keem Bay.
+ */
+
+#ifndef __DT_BINDINGS_KEEMBAY_POWER_H
+#define __DT_BINDINGS_KEEMBAY_POWER_H
+
+#define KEEM_BAY_PSS_POWER_DOMAIN 0
+#define KEEM_BAY_MSS_CPU_POWER_DOMAIN 1
+#define KEEM_BAY_VDEC_POWER_DOMAIN 2
+#define KEEM_BAY_VENC_POWER_DOMAIN 3
+#define KEEM_BAY_PCIE_POWER_DOMAIN 4
+#define KEEM_BAY_USS_POWER_DOMAIN 5
+#define KEEM_BAY_MSS_CAM_POWER_DOMAIN 6
+
+#endif /* __DT_BINDINGS_KEEMBAY_POWER_H */
