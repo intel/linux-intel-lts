7e10c1ce43e4212f23a48b994c184311a2c0dbec
Wong, Vincent Por Yin <vincent.por.yin.wong@intel.com>
2020-07-01 15:47:50 +0800

    WORKAROUND: net: stmmac: use DMA INTM 0 for TGL platforms
    During multi-queue TX/RX traffic, the driver may hang and get
    reset by the kernel repeatedly. Root caused to this setting which
    may re-surface a bug when using External Timestamping (aka AUXTS).
    A proper fix is still required, as this seems to be TGL-specific.
    
    Signed-off-by: Wong, Vincent Por Yin <vincent.por.yin.wong@intel.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac4_dma.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac4_dma.c
@@ -165,7 +165,10 @@ static void dwmac4_dma_init(void __iomem *ioaddr,
 
 	value = readl(ioaddr + DMA_BUS_MODE);
 
-	if (dma_cfg->multi_msi_en) {
+	if (dma_cfg->multi_msi_en && dma_cfg->tgl_wa) {
+		value &= ~DMA_BUS_MODE_INTM_MASK;
+		value |= (DMA_BUS_MODE_INTM_MODE0 << DMA_BUS_MODE_INTM_SHIFT);
+	} else if (dma_cfg->multi_msi_en) {
 		value &= ~DMA_BUS_MODE_INTM_MASK;
 		value |= (DMA_BUS_MODE_INTM_MODE1 << DMA_BUS_MODE_INTM_SHIFT);
 	}
