a27a1a3efc3660a3cc2cadd86142bae1a7cc0fd3
Daniele Alessandrelli <daniele.alessandrelli@intel.com>
2020-07-02 18:27:45 +0100

    dt-bindings: clk: Add CDCUN1208 clock buffer bindings
    Add dt-bindings documentation for CDCUN1208 driver.
    
    Signed-off-by: Daniele Alessandrelli <daniele.alessandrelli@intel.com>

---
+++ include/dt-bindings/clock/ti,cdcun1208.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * TI CDCUN1208 clock buffer driver.
+ *
+ * Copyright (C) 2019-2020 Intel Corporation
+ */
+
+#ifndef __DT_BINDINGS_CDCUN_1208_CLK_H
+#define __DT_BINDINGS_CDCUN_1208_CLK_H
+
+#define CDCUN_1208_OUT1	0
+#define CDCUN_1208_OUT2	1
+#define CDCUN_1208_OUT3	2
+#define CDCUN_1208_OUT4	3
+#define CDCUN_1208_OUT5	4
+#define CDCUN_1208_OUT6	5
+#define CDCUN_1208_OUT7	6
+#define CDCUN_1208_OUT8	7
+
+#endif /* __DT_BINDINGS_CDCUN_1208_CLK_H */
