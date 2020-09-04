11d6e41063ec1fb7afa07d5153e643207e0b8167
Qiang Rao <qiang.rao@intel.com>
2020-09-04 16:56:12 +0800

    tcc: update tcc range end_address.
    To report tcc range, start_address is inclusive; but end_address is not.
    
    Signed-off-by: Qiang Rao <qiang.rao@intel.com>

--- arch/x86/kernel/acpi/boot.c
+++ arch/x86/kernel/acpi/boot.c
@@ -1377,7 +1377,7 @@ static int __init acpi_parse_ptct(struct acpi_table_header *table)
 		if ((entry->type == ACPI_PTCT_ENTRY_PSEUDO_SRAM) && (id < total_psram_region)) {
 			psram = (struct acpi_ptct_psram *)(ptr+PTCT_ENTRY_HEADER_SIZE);
 			ptct_psram_regions[id].phyaddr_start = ((u64)(psram->phyaddr_lo))|((u64)(psram->phyaddr_hi)<<32);
-			ptct_psram_regions[id].phyaddr_end  = ptct_psram_regions[id].phyaddr_start + psram->size - 1;
+			ptct_psram_regions[id].phyaddr_end  = ptct_psram_regions[id].phyaddr_start + psram->size;
 			id++;
 		}
 		ptr += entry->size;
