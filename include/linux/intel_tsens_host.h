2f6f4eadac426e31b18ad69e9f0b0997c67d2e70
C, Udhayakumar <udhayakumar.c@intel.com>
2020-10-21 19:53:37 +0530

    misc: Intel tsens IA host driver.
    Add Intel tsens IA host driver for Intel Edge.AI Computer Vision
    platforms.
    
    About Intel Edge.AI Computer Vision platforms:
    ---------------------------------------------
    The Intel Edge.AI Computer Vision platforms are vision processing systems
    targeting machine vision applications for connected devices.
    
    They are based on ARM A53 CPU running Linux and acts as a PCIe
    endpoint device.
    
    High-level architecture:
    ------------------------
    
    Remote Host IA CPU                      Local Host ARM CPU
    ----------------                     --------------------------
    |  Platform    |                     |  Thermal Daemon        |
    | Management SW|                     |                        |
    ----------------                     --------------------------
    |  Intel tsens |                     |  intel tsens i2c slave |
    |  i2c client  |                     |  and thermal driver    |
    ----------------                     --------------------------
    |  XLINK I2C   |                     |  XLINK I2C Slave       |
    |  controller  |     <=========>     |   controller           |
    ----------------     xlink smbus     --------------------------
    
    intel tsens module:
    -------------------
    The tsens module enables reading of on chip sensors present
    in the Intel Edge.AI Computer Vision platforms.In the tsens module
    various junction and SoC temperatures are reported using thermal
    subsystem and i2c subsystem.
    
    Temperature data reported using thermal subsystem will be used for
    various cooling agents such as DVFS, fan control and shutdown the
    system in case of critical temperature.
    
    Temperature data reported using i2c subsystem will be used by
    platform manageability software running in IA host.
    
    - Remote Host driver
      * Intended for IA CPU
      * It is a I2C client driver
      * Driver path:
      {tree}/drivers/misc/intel_tsens/intel_tsens_host.c
    
    Local host and Remote host drivers communicates using
    I2C SMBUS protocol.
    
    Acked-by: Mark Mross <mgross@linux.intel.com>
    Signed-off-by: C, Udhayakumar <udhayakumar.c@intel.com>

---
+++ include/linux/intel_tsens_host.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ *
+ * Intel tsens host I2C thermal Driver
+ *
+ * Copyright (C) 2020 Intel Corporation
+ *
+ */
+
+#ifndef _LINUX_INTEL_TSENS_HOST_DEVICE_H
+#define _LINUX_INTEL_TSENS_HOST_DEVICE_H
+
+struct intel_tsens_host_trip_info {
+	enum thermal_trip_type trip_type;
+	int temp;
+} __packed __aligned(4);
+
+struct intel_tsens_host {
+	const char *sensor_name_smbus;
+	const char *sensor_name_xlk;
+	struct intel_tsens_data *t_data;
+	struct intel_tsens_host_trip_info **trip_info;
+	u32 device_id;
+	struct i2c_client *i2c_xlk;
+	struct i2c_client *i2c_smbus;
+	struct thermal_zone_device *tz_xlk;
+	struct thermal_zone_device *tz_smbus;
+};
+
+struct intel_tsens_host_plat_data {
+	int nsens;
+	struct intel_tsens_host **tsens;
+};
+#endif /*_LINUX_INTEL_TSENS_HOST_DEVICE_H*/
