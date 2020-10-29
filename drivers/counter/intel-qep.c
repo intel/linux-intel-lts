7aace21025a944703d8355a3196dc27680f7fbe4
Jarkko Nikula <jarkko.nikula@linux.intel.com>
2020-10-29 16:55:34 +0200

    counter: intel-qep: Remove a few include files
    It looks code doesn't use anything from these includes directly and code
    compiles without them so remove them.
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>

--- drivers/counter/intel-qep.c
+++ drivers/counter/intel-qep.c
@@ -10,15 +10,12 @@
  */
 #include <linux/bitops.h>
 #include <linux/counter.h>
-#include <linux/err.h>
 #include <linux/interrupt.h>
-#include <linux/io.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/pci.h>
 #include <linux/pm_runtime.h>
-#include <linux/sysfs.h>
 
 #define INTEL_QEPCON			0x00
 #define INTEL_QEPFLT			0x04
