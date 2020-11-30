b4b5007060d38005935ecd92c11bc78868d57f46
Arnaldo Carvalho de Melo <acme@redhat.com>
2020-11-30 15:01:08 -0300

    perf evlist: Use the right prefix for 'struct evlist' event selection methods
    perf_evlist__ is for 'struct perf_evlist' methods, in tools/lib/perf/,
    go on completing this split.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/evlist.c
+++ tools/perf/util/evlist.c
@@ -1265,8 +1265,7 @@ bool evlist__sample_id_all(struct evlist *evlist)
 	return first->core.attr.sample_id_all;
 }
 
-void perf_evlist__set_selected(struct evlist *evlist,
-			       struct evsel *evsel)
+void evlist__set_selected(struct evlist *evlist, struct evsel *evsel)
 {
 	evlist->selected = evsel;
 }
