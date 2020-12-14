13878c98a60e132e1af1e6d420c53b8c532e84a4
Daniel Vetter <daniel.vetter@ffwll.ch>
2020-12-14 18:16:22 +0100

    dma-buf: begin/end_cpu might lock the dma_resv lock
    At least amdgpu and i915 do, so lets just document this as the rule.
    
    v2: Works better with less typos (intel-gfx-ci)
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Cc: Thomas Zimmermann <tzimmermann@suse.de>
    Cc: Sumit Semwal <sumit.semwal@linaro.org>
    Cc: "Christian König" <christian.koenig@amd.com>
    Cc: linux-media@vger.kernel.org
    Cc: linaro-mm-sig@lists.linaro.org
    Link: https://patchwork.freedesktop.org/patch/msgid/20201214171622.3868883-1-daniel.vetter@ffwll.ch

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -1131,6 +1131,8 @@ int dma_buf_begin_cpu_access(struct dma_buf *dmabuf,
 	if (WARN_ON(!dmabuf))
 		return -EINVAL;
 
+	might_lock(&dmabuf->resv->lock.base);
+
 	if (dmabuf->ops->begin_cpu_access)
 		ret = dmabuf->ops->begin_cpu_access(dmabuf, direction);
 
@@ -1163,6 +1165,8 @@ int dma_buf_end_cpu_access(struct dma_buf *dmabuf,
 	int ret = 0;
 
 	WARN_ON(!dmabuf);
+
+	might_lock(&dmabuf->resv->lock.base);
 
 	if (dmabuf->ops->end_cpu_access)
 		ret = dmabuf->ops->end_cpu_access(dmabuf, direction);
