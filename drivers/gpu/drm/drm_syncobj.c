05cff381bcc23b37f3c65302d47c6dd3067bf8bd
Ranjan Dutta <ranjan.dutta@intel.com>
2021-01-29 21:29:12 +0800

    Revert "drm/syncobj: Fix use-after-free"
    This reverts commit eab4b3e27413f3cc80d301697f04e09dd4552fd6.

--- drivers/gpu/drm/drm_syncobj.c
+++ drivers/gpu/drm/drm_syncobj.c
@@ -388,18 +388,19 @@ int drm_syncobj_find_fence(struct drm_file *file_private,
 		return -ENOENT;
 
 	*fence = drm_syncobj_fence_get(syncobj);
+	drm_syncobj_put(syncobj);
 
 	if (*fence) {
 		ret = dma_fence_chain_find_seqno(fence, point);
 		if (!ret)
-			goto out;
+			return 0;
 		dma_fence_put(*fence);
 	} else {
 		ret = -EINVAL;
 	}
 
 	if (!(flags & DRM_SYNCOBJ_WAIT_FLAGS_WAIT_FOR_SUBMIT))
-		goto out;
+		return ret;
 
 	memset(&wait, 0, sizeof(wait));
 	wait.task = current;
@@ -430,9 +431,6 @@ int drm_syncobj_find_fence(struct drm_file *file_private,
 
 	if (wait.node.next)
 		drm_syncobj_remove_wait(syncobj, &wait);
-
-out:
-	drm_syncobj_put(syncobj);
 
 	return ret;
 }
