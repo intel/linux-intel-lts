9efb95f7f17d011305863fe318bbf7a0630a116c
Seamus Kelly <seamus.kelly@intel.com>
2021-02-04 14:59:46 +0000

    xlink-ipc: Add Keem Bay XLink IPC module
    XLink IPC module interfaces the xLink Core driver with the Keem
    Bay VPU IPC driver, thus enabling xLink to control and communicate with
    the VPU IP present on the Intel Keem Bay SoC.
    
    Specifically the driver enables xLink Core to:
    
    * Boot / Reset the VPU IP
    * Register to VPU IP event notifications (device connected, device
      disconnected, WDT event)
    * Query the status of the VPU IP (OFF, BUSY, READY, ERROR, RECOVERY)
    * Exchange data with the VPU IP, using the Keem Bay IPC mechanism
      - Including the ability to send 'volatile' data (i.e., small amount of
        data, up to 128-bytes that was not allocated in the CPU/VPU shared
        memory region)
    
    Signed-off-by: Seamus Kelly <seamus.kelly@intel.com>

---
+++ include/linux/xlink-ipc.h
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: GPL-2.0 only */
+/*****************************************************************************
+ *
+ * Intel Keem Bay xlink IPC Driver
+ *
+ * Copyright (C) 2019 Intel Corporation
+ *
+ ****************************************************************************/
+
+#ifndef _XLINK_IPC_H_
+#define _XLINK_IPC_H_
+
+#include <linux/types.h>
+
+struct xlink_ipc_context {
+	u16 chan;
+	u8 is_volatile;
+};
+
+int xlink_ipc_connect(u32 sw_device_id);
+
+int xlink_ipc_write(u32 sw_device_id, void *data, size_t * const size,
+		u32 timeout, void *context);
+
+int xlink_ipc_read(u32 sw_device_id, void *data, size_t * const size,
+		u32 timeout, void *context);
+
+int xlink_ipc_get_device_list(u32 *sw_device_id_list, u32 *num_devices);
+
+int xlink_ipc_get_device_name(u32 sw_device_id, char *device_name,
+		size_t name_size);
+
+int xlink_ipc_get_device_status(u32 sw_device_id, u32 *device_status);
+
+int xlink_ipc_boot_device(u32 sw_device_id, const char *binary_name);
+
+int xlink_ipc_reset_device(u32 sw_device_id);
+
+int xlink_ipc_open_channel(u32 sw_device_id, u32 channel);
+
+int xlink_ipc_close_channel(u32 sw_device_id, u32 channel);
+
+int xlink_ipc_register_for_events(u32 sw_device_id,
+		int (*callback)(u32 sw_device_id, u32 event));
+
+int xlink_ipc_unregister_for_events(u32 sw_device_id);
+
+int xlink_ipc_get_device_mode(u32 sw_device_id, u32 *power_mode);
+
+int xlink_ipc_set_device_mode(u32 sw_device_id, u32 power_mode);
+
+#endif /* _XLINK_IPC_H_ */
