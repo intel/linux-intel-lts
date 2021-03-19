bb75736823e3e1a265496f4b7fc7138a0f4a1907
Peter Collingbourne <pcc@google.com>
2021-03-18 20:10:54 -0700

    BACKPORT: arm64: pac: Optimize kernel entry/exit key installation code paths
    The kernel does not use any keys besides IA so we don't need to
    install IB/DA/DB/GA on kernel exit if we arrange to install them
    on task switch instead, which we can expect to happen an order of
    magnitude less often.
    
    Furthermore we can avoid installing the user IA in the case where the
    user task has IA disabled and just leave the kernel IA installed. This
    also lets us avoid needing to install IA on kernel entry.
    
    On an Apple M1 under a hypervisor, the overhead of kernel entry/exit
    has been measured to be reduced by 15.6ns in the case where IA is
    enabled, and 31.9ns in the case where IA is disabled.
    
    Signed-off-by: Peter Collingbourne <pcc@google.com>
    Link: https://linux-review.googlesource.com/id/Ieddf6b580d23c9e0bed45a822dabe72d2ffc9a8e
    Link: https://lore.kernel.org/r/2d653d055f38f779937f2b92f8ddd5cf9e4af4f4.1616123271.git.pcc@google.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Bug: 192536783
    (cherry picked from commit b90e483938ce387c256e03fb144f82f64551847b)
    Change-Id: Ia01f55f50c55b53e65ea7089a3bb61ee7660e578
    [quic_eberman@quicinc.com: resolve merge conflict in
     arch/arm64/kernel/process.c]
    Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>
    Signed-off-by: Peter Collingbourne <pcc@google.com>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -578,6 +578,7 @@ __notrace_funcgraph struct task_struct *__switch_to(struct task_struct *prev,
 	uao_thread_switch(next);
 	ssbs_thread_switch(next);
 	erratum_1418040_thread_switch(prev, next);
+	ptrauth_thread_switch_user(next);
 
 	/*
 	 * Complete any pending TLB or cache maintenance on this CPU in case
