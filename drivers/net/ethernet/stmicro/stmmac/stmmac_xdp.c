dd65868e248ba69c238ecb4089309700f12aa30b
Ong Boon Leong <boon.leong.ong@intel.com>
2021-03-23 23:26:49 +0800

    net: stmmac: Add TX via XDP zero-copy socket
    We add the support of XDP ZC TX submission and cleaning into
    stmmac_tx_clean(). The function is made to clean as many TX complete
    frames as possible, i.e. limit by priv->dma_tx_size instead of NAPI
    budget. For TX ring that is associated with XSK pool, the function
    stmmac_xdp_xmit_zc() is introduced to TX frame buffers from XSK pool by
    using xsk_tx_peek_desc(). To make stmmac_tx_clean() support the cleaning
    of XSK TX frames, STMMAC_TXBUF_T_XSK_TX TX buffer type is introduced.
    
    As stmmac_tx_clean() uses the return value to cue whether NAPI function
    should continue to poll, we augment the caller of stmmac_tx_clean() to
    pass NAPI budget instead of priv->dma_tx_size through 'budget' input and
    made stmmac_tx_clean() to always clean up-to the TX ring size instead.
    This allows us to use the return boolean status of stmmac_xdp_xmit_zc()
    to decide if XSK TX work is done or not: If true, set 'xmits' to return
    'budget - 1' so that NAPI poll may exit. Else, set 'xmits' to return
    'budget' to make NAPI poll continue to poll since XSK TX work is not
    done. Finally, at the end of stmmac_tx_clean(), the function now take
    a maximum value between 'count' and 'xmits' so that status from both
    TX cleaning and XSK TX (only for XDP ZC) is considered.
    
    This patch adds a new NAPI poll called stmmac_napi_poll_rxtx() that is
    meant to be enabled/disabled for RX and TX ring that are bound to XSK
    pool. This NAPI poll function starts with cleaning TX ring, then submits
    XSK TX frames to TX ring before proceed to perform RX operations, i.e.
    , receiving RX frames and replenishing RX ring with RX free buffers
    obtained from XSK pool. Therefore, during XSK RX and TX setup, the driver
    enables stmmac_napi_poll_rxtx() for RX and TX operations, then during
    XSK RX and TX pool tear-down, the driver reenables the exisiting
    independent NAPI poll functions accordingly: stmmac_napi_poll_rx() and
    stmmac_napi_poll_tx().
    
    Signed-off-by: Ong Boon Leong <boon.leong.ong@intel.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_xdp.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_xdp.c
@@ -13,7 +13,8 @@ static int stmmac_xdp_enable_pool(struct stmmac_priv *priv,
 	u32 frame_size;
 	int err;
 
-	if (queue >= priv->plat->rx_queues_to_use)
+	if (queue >= priv->plat->rx_queues_to_use ||
+	    queue >= priv->plat->tx_queues_to_use)
 		return -EINVAL;
 
 	frame_size = xsk_pool_get_rx_frame_size(pool);
@@ -33,14 +34,17 @@ static int stmmac_xdp_enable_pool(struct stmmac_priv *priv,
 
 	if (need_update) {
 		stmmac_disable_rx_queue(priv, queue);
+		stmmac_disable_tx_queue(priv, queue);
 		napi_disable(&ch->rx_napi);
+		napi_disable(&ch->tx_napi);
 	}
 
 	set_bit(queue, priv->af_xdp_zc_qps);
 
 	if (need_update) {
-		napi_enable(&ch->rx_napi);
+		napi_enable(&ch->rxtx_napi);
 		stmmac_enable_rx_queue(priv, queue);
+		stmmac_enable_tx_queue(priv, queue);
 
 		err = stmmac_xsk_wakeup(priv->dev, queue, XDP_WAKEUP_RX);
 		if (err)
@@ -56,7 +60,8 @@ static int stmmac_xdp_disable_pool(struct stmmac_priv *priv, u16 queue)
 	struct xsk_buff_pool *pool;
 	bool need_update;
 
-	if (queue >= priv->plat->rx_queues_to_use)
+	if (queue >= priv->plat->rx_queues_to_use ||
+	    queue >= priv->plat->tx_queues_to_use)
 		return -EINVAL;
 
 	pool = xsk_get_pool_from_qid(priv->dev, queue);
@@ -67,7 +72,8 @@ static int stmmac_xdp_disable_pool(struct stmmac_priv *priv, u16 queue)
 
 	if (need_update) {
 		stmmac_disable_rx_queue(priv, queue);
-		napi_disable(&ch->rx_napi);
+		stmmac_disable_tx_queue(priv, queue);
+		napi_disable(&ch->rxtx_napi);
 	}
 
 	xsk_pool_dma_unmap(pool, STMMAC_RX_DMA_ATTR);
@@ -76,7 +82,9 @@ static int stmmac_xdp_disable_pool(struct stmmac_priv *priv, u16 queue)
 
 	if (need_update) {
 		napi_enable(&ch->rx_napi);
+		napi_enable(&ch->tx_napi);
 		stmmac_enable_rx_queue(priv, queue);
+		stmmac_enable_tx_queue(priv, queue);
 	}
 
 	return 0;
