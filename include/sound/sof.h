8e457b5d275e7da16152603627dff43f675c989a
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2021-04-09 15:01:21 -0700

    ASOC: SOF: simplify nocodec mode
    Replace ugly #if (!IS_ENABLED) by if (!IS_ENABLED), remove
    cross-module dependencies and use classic mechanism to pass
    information to the machine driver.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@gmail.com>
    Reviewed-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Link: https://lore.kernel.org/r/20210409220121.1542362-7-ranjani.sridharan@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/sound/sof.h
+++ include/sound/sof.h
@@ -100,8 +100,6 @@ struct sof_dev_desc {
 	const struct snd_sof_dsp_ops *ops;
 };
 
-int sof_nocodec_setup(struct device *dev,
-		      const struct snd_sof_dsp_ops *ops);
 int sof_dai_get_mclk(struct snd_soc_pcm_runtime *rtd);
 int sof_dai_get_bclk(struct snd_soc_pcm_runtime *rtd);
 
