eac065f8ebdc0417a8adb5da04696ef1989ed3fb
Kan Liang <kan.liang@linux.intel.com>
2021-04-12 07:30:45 -0700

    perf/x86: Hybrid PMU support for intel_ctrl
    The intel_ctrl is the counter mask of a PMU. The PMU counter information
    may be different among hybrid PMUs, each hybrid PMU should use its own
    intel_ctrl to check and access the counters.
    
    When handling a certain hybrid PMU, apply the intel_ctrl from the
    corresponding hybrid PMU.
    
    When checking the HW existence, apply the PMU and number of counters
    from the corresponding hybrid PMU as well. Perf will check the HW
    existence for each Hybrid PMU before registration. Expose the
    check_hw_exists() for a later patch.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Link: https://lkml.kernel.org/r/1618237865-33448-6-git-send-email-kan.liang@linux.intel.com

--- arch/x86/events/perf_event.h
+++ arch/x86/events/perf_event.h
@@ -635,6 +635,7 @@ enum {
 struct x86_hybrid_pmu {
 	struct pmu			pmu;
 	union perf_capabilities		intel_cap;
+	u64				intel_ctrl;
 };
 
 static __always_inline struct x86_hybrid_pmu *hybrid_pmu(struct pmu *pmu)
@@ -999,6 +1000,9 @@ static inline int x86_pmu_rdpmc_index(int index)
 	return x86_pmu.rdpmc_index ? x86_pmu.rdpmc_index(index) : index;
 }
 
+bool check_hw_exists(struct pmu *pmu, int num_counters,
+		     int num_counters_fixed);
+
 int x86_add_exclusive(unsigned int what);
 
 void x86_del_exclusive(unsigned int what);
@@ -1103,9 +1107,11 @@ ssize_t events_sysfs_show(struct device *dev, struct device_attribute *attr,
 ssize_t events_ht_sysfs_show(struct device *dev, struct device_attribute *attr,
 			  char *page);
 
-static inline bool fixed_counter_disabled(int i)
+static inline bool fixed_counter_disabled(int i, struct pmu *pmu)
 {
-	return !(x86_pmu.intel_ctrl >> (i + INTEL_PMC_IDX_FIXED));
+	u64 intel_ctrl = hybrid(pmu, intel_ctrl);
+
+	return !(intel_ctrl >> (i + INTEL_PMC_IDX_FIXED));
 }
 
 #ifdef CONFIG_CPU_SUP_AMD
