b6847fcf735fe64b053674c2ee061e2cf368c070
Kan Liang <kan.liang@linux.intel.com>
2021-05-31 06:51:19 -0700

    perf: Save PMU specific data in task_struct
    Some PMU specific data has to be saved/restored during context switch,
    e.g. LBR call stack data. Currently, the data is saved in event context
    structure, but only for per-process event. For system-wide event,
    because of missing the LBR call stack data after context switch, LBR
    callstacks are always shorter in comparison to per-process mode.
    
    For example,
      Per-process mode:
      $perf record --call-graph lbr -- taskset -c 0 ./tchain_edit
    
      -   99.90%    99.86%  tchain_edit  tchain_edit       [.] f3
           99.86% _start
              __libc_start_main
              generic_start_main
              main
              f1
            - f2
                 f3
    
      System-wide mode:
      $perf record --call-graph lbr -a -- taskset -c 0 ./tchain_edit
    
      -   99.88%    99.82%  tchain_edit  tchain_edit        [.] f3
       - 62.02% main
            f1
            f2
            f3
       - 28.83% f1
          - f2
            f3
       - 28.83% f1
          - f2
               f3
       - 8.88% generic_start_main
            main
            f1
            f2
            f3
    
    It isn't practical to simply allocate the data for system-wide event in
    CPU context structure for all tasks. We have no idea which CPU a task
    will be scheduled to. The duplicated LBR data has to be maintained on
    every CPU context structure. That's a huge waste. Otherwise, the LBR
    data still lost if the task is scheduled to another CPU.
    
    Save the pmu specific data in task_struct. The size of pmu specific data
    is 788 bytes for LBR call stack. Usually, the overall amount of threads
    doesn't exceed a few thousands. For 10K threads, keeping LBR data would
    consume additional ~8MB. The additional space will only be allocated
    during LBR call stack monitoring. It will be released when the
    monitoring is finished.
    
    Furthermore, moving task_ctx_data from perf_event_context to task_struct
    can reduce complexity and make things clearer. E.g. perf doesn't need to
    swap task_ctx_data on optimized context switch path.
    This patch set is just the first step. There could be other
    optimization/extension on top of this patch set. E.g. for cgroup
    profiling, perf just needs to save/store the LBR call stack information
    for tasks in specific cgroup. That could reduce the additional space.
    Also, the LBR call stack can be available for software events, or allow
    even debugging use cases, like LBRs on crash later.
    
    The Kmem cache of pmu specific data is saved in struct perf_ctx_data.
    It's required when child task allocates the space.
    The refcount in struct perf_ctx_data is used to track the users of pmu
    specific data.
    
    Reviewed-by: Alexey Budankov <alexey.budankov@linux.intel.com>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -843,6 +843,36 @@ struct perf_event_context {
 	struct rcu_head			rcu_head;
 };
 
+/**
+ * struct perf_ctx_data - PMU specific data for a task
+ * @rcu_head:  To avoid the race on free PMU specific data
+ * @refcount:  To track users
+ * @global:    To track system-wide users
+ * @ctx_cache: Kmem cache of PMU specific data
+ * @data:      PMU specific data
+ *
+ * Currently, the struct is only used in Intel LBR call stack mode to
+ * save/restore the call stack of a task on context switches.
+ * The data only be allocated when Intel LBR call stack mode is enabled.
+ * The data will be freed when the mode is disabled. The rcu_head is
+ * used to prevent the race on free the data.
+ * The content of the data will only be accessed in context switch, which
+ * should be protected by rcu_read_lock().
+ *
+ * Careful: Struct perf_ctx_data is added as a pointor in struct task_struct.
+ * When system-wide Intel LBR call stack mode is enabled, a buffer with
+ * constant size will be allocated for each task.
+ * Also, system memory consumption can further grow when the size of
+ * struct perf_ctx_data enlarges.
+ */
+struct perf_ctx_data {
+	struct rcu_head			rcu_head;
+	refcount_t			refcount;
+	int				global;
+	struct kmem_cache		*ctx_cache;
+	void				*data;
+};
+
 /*
  * Number of contexts where an event can trigger:
  *	task, softirq, hardirq, nmi.
--- kernel/events/core.c
+++ kernel/events/core.c
@@ -13253,6 +13253,7 @@ int perf_event_init_task(struct task_struct *child, u64 clone_flags)
 	memset(child->perf_event_ctxp, 0, sizeof(child->perf_event_ctxp));
 	mutex_init(&child->perf_event_mutex);
 	INIT_LIST_HEAD(&child->perf_event_list);
+	child->perf_ctx_data = NULL;
 
 	for_each_task_context_nr(ctxn) {
 		ret = perf_event_init_context(child, ctxn, clone_flags);
