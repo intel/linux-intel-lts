f314c23cf239b647bc805f0c02bc0a3f7669d573
Jan Kiszka <jan.kiszka@siemens.com>
2021-06-07 08:19:37 +0200

    irq_pipeline: Clean up stage_info field and users
    This field represents mutually exclusive states, namely
    
    - IRQENTRY_INBAND_UNSTALLED
    - IRQENTRY_INBAND_STALLED
    - IRQENTRY_OOB
    
    Encodes them as enum and test against them, rather than against state
    bits that suggest they could be combined.
    
    Also flip the inverted naming of INBAND_STALLED vs. INBAND_UNSTALLED:
    Only when we entered under INBAND_UNSTALLED, certain actions need to be
    taken on exit.
    
    Finally, document the stage_info field of irqentry_state.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- kernel/entry/common.c
+++ kernel/entry/common.c
@@ -359,13 +359,13 @@ noinstr irqentry_state_t irqentry_enter(struct pt_regs *regs)
 	irqentry_state_t ret = {
 		.exit_rcu = false,
 #ifdef CONFIG_IRQ_PIPELINE
-		.stage_info = 0,
+		.stage_info = IRQENTRY_INBAND_STALLED,
 #endif
 	};
 
 #ifdef CONFIG_IRQ_PIPELINE
 	if (running_oob()) {
-		ret.stage_info = IRQENTRY_OOB_ENTRY;
+		ret.stage_info = IRQENTRY_OOB;
 		return ret;
 	}
 #endif
@@ -374,7 +374,7 @@ noinstr irqentry_state_t irqentry_enter(struct pt_regs *regs)
 #ifdef CONFIG_IRQ_PIPELINE
 		WARN_ON_ONCE(irq_pipeline_debug() && irqs_disabled());
 		stall_inband_nocheck();
-		ret.stage_info = IRQENTRY_INBAND_STALLED;
+		ret.stage_info = IRQENTRY_INBAND_UNSTALLED;
 #endif
 		irqentry_enter_from_user_mode(regs);
 		return ret;
@@ -387,7 +387,7 @@ noinstr irqentry_state_t irqentry_enter(struct pt_regs *regs)
 	 * irqs are off on entry, we have to stall the in-band stage.
 	 */
 	if (!test_and_stall_inband_nocheck())
-		ret.stage_info = IRQENTRY_INBAND_STALLED;
+		ret.stage_info = IRQENTRY_INBAND_UNSTALLED;
 #endif
 
 	/*
@@ -462,7 +462,7 @@ static inline
 bool irqexit_may_preempt_schedule(irqentry_state_t state,
 				struct pt_regs *regs)
 {
-	return !!(state.stage_info & IRQENTRY_INBAND_STALLED);
+	return state.stage_info == IRQENTRY_INBAND_UNSTALLED;
 }
 
 #else
@@ -526,7 +526,7 @@ out:
 	 * irqentry_enter() raised it in order to mirror the hardware
 	 * state.
 	 */
-	if (state.stage_info & IRQENTRY_INBAND_STALLED)
+	if (state.stage_info == IRQENTRY_INBAND_UNSTALLED)
 		unstall_inband();
 #endif
 	return;
