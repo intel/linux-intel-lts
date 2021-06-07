f7aea753aeda533d2135fb2861e034c9bc41e12f
Ranjan Dutta <ranjan.dutta@intel.com>
2021-06-07 19:08:37 +0800

    Revert "drm/qxl: use ttm bo priorities"
    This reverts commit 08f6e8a7538d99909e54ce247e689e5f1298ccbb.

--- drivers/gpu/drm/qxl/qxl_release.c
+++ drivers/gpu/drm/qxl/qxl_release.c
@@ -199,12 +199,11 @@ qxl_release_free(struct qxl_device *qdev,
 }
 
 static int qxl_release_bo_alloc(struct qxl_device *qdev,
-				struct qxl_bo **bo,
-				u32 priority)
+				struct qxl_bo **bo)
 {
 	/* pin releases bo's they are too messy to evict */
 	return qxl_bo_create(qdev, PAGE_SIZE, false, true,
-			     QXL_GEM_DOMAIN_VRAM, priority, NULL, bo);
+			     QXL_GEM_DOMAIN_VRAM, NULL, bo);
 }
 
 int qxl_release_list_add(struct qxl_release *release, struct qxl_bo *bo)
@@ -327,18 +326,13 @@ int qxl_alloc_release_reserved(struct qxl_device *qdev, unsigned long size,
 	int ret = 0;
 	union qxl_release_info *info;
 	int cur_idx;
-	u32 priority;
 
-	if (type == QXL_RELEASE_DRAWABLE) {
+	if (type == QXL_RELEASE_DRAWABLE)
 		cur_idx = 0;
-		priority = 0;
-	} else if (type == QXL_RELEASE_SURFACE_CMD) {
+	else if (type == QXL_RELEASE_SURFACE_CMD)
 		cur_idx = 1;
-		priority = 1;
-	} else if (type == QXL_RELEASE_CURSOR_CMD) {
+	else if (type == QXL_RELEASE_CURSOR_CMD)
 		cur_idx = 2;
-		priority = 1;
-	}
 	else {
 		DRM_ERROR("got illegal type: %d\n", type);
 		return -EINVAL;
@@ -358,7 +352,7 @@ int qxl_alloc_release_reserved(struct qxl_device *qdev, unsigned long size,
 		qdev->current_release_bo[cur_idx] = NULL;
 	}
 	if (!qdev->current_release_bo[cur_idx]) {
-		ret = qxl_release_bo_alloc(qdev, &qdev->current_release_bo[cur_idx], priority);
+		ret = qxl_release_bo_alloc(qdev, &qdev->current_release_bo[cur_idx]);
 		if (ret) {
 			mutex_unlock(&qdev->release_mutex);
 			qxl_release_free(qdev, *release);
