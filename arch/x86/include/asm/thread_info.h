b0b90fd21d6b43b0b6ccbd9471329f03241bdfba
Jan Kiszka <jan.kiszka@siemens.com>
2021-06-07 12:05:33 +0200

    x86: dovetail: Fix TS flag reservation
    We had an overlap with compat flags so that, e.g., TS_COMPAT_RESTART
    made a 32-bit standard task also a dovetail one.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- arch/x86/include/asm/thread_info.h
+++ arch/x86/include/asm/thread_info.h
@@ -232,10 +232,11 @@ static inline int arch_within_stack_frames(const void * const stack,
  * have to worry about atomic accesses.
  */
 #define TS_COMPAT		0x0002	/* 32bit syscall active (64BIT)*/
-#define TS_OOB			0x0004	/* Thread is running out-of-band */
-#define TS_DOVETAIL		0x0008  /* Dovetail notifier enabled */
-#define TS_OFFSTAGE		0x0010	/* Thread is in-flight to OOB context */
-#define TS_OOBTRAP		0x0020	/* Handling a trap from OOB context */
+/* bits 2 and 3 reserved for compat */
+#define TS_OOB			0x0010	/* Thread is running out-of-band */
+#define TS_DOVETAIL		0x0020  /* Dovetail notifier enabled */
+#define TS_OFFSTAGE		0x0040	/* Thread is in-flight to OOB context */
+#define TS_OOBTRAP		0x0080	/* Handling a trap from OOB context */
 
 #define _TLF_OOB		TS_OOB
 #define _TLF_DOVETAIL		TS_DOVETAIL
