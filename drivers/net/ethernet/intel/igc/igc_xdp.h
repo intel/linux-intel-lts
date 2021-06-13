9e712c8b64129bf5bc272eafa6d77f8abb821f1c
Vedang Patel <vedang.patel@intel.com>
2021-06-14 00:35:20 +0800

    igc: Add BTF based metadata for XDP
    This commit adds support for BTF based metadata for XDP. Currently, the
    support has only been tested on receive side. Following is the struct
    describing the metadata:
    
    struct xdp_md_desc {
    	u64 timestamp;
    };
    
    Note that only a single member is added to the struct. More members will
    be added in the future.
    
    Signed-off-by: Vedang Patel <vedang.patel@intel.com>
    Signed-off-by: Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>

--- drivers/net/ethernet/intel/igc/igc_xdp.h
+++ drivers/net/ethernet/intel/igc/igc_xdp.h
@@ -4,6 +4,12 @@
 #ifndef _IGC_XDP_H_
 #define _IGC_XDP_H_
 
+#include <asm-generic/int-ll64.h>
+
+struct igc_md_desc {
+	u64 timestamp;
+};
+
 int igc_xdp_set_prog(struct igc_adapter *adapter, struct bpf_prog *prog,
 		     struct netlink_ext_ack *extack);
 int igc_xdp_setup_pool(struct igc_adapter *adapter, struct xsk_buff_pool *pool,
@@ -13,5 +19,10 @@ static inline bool igc_xdp_is_enabled(struct igc_adapter *adapter)
 {
 	return !!adapter->xdp_prog;
 }
+
+int igc_xdp_register_rxq_info(struct igc_ring *ring);
+void igc_xdp_unregister_rxq_info(struct igc_ring *ring);
+int igc_xdp_query_btf(struct net_device *dev, u8 *enabled);
+int igc_xdp_set_btf_md(struct net_device *dev, u8 enable);
 
 #endif /* _IGC_XDP_H_ */
