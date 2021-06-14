bb9ef3cb40b9748d9a676d8d381668f0ca3a9a9f
Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>
2021-06-14 11:16:14 +0800

    net/core: XDP metadata BTF netlink API
    Add new devlink XDP attributes to be used to query or setup XDP metadata
    BTF state.
    
    IFLA_XDP_MD_BTF_ID: type NLA_U32.
    IFLA_XDP_MD_BTF_STATE: type = NLA_U8.
    
    On XDP query driver reports current loaded BTF ID, and its state if
    active or not.
    
    On XDP setup, driver will use these attributes to activate/deactivate
    a specific BTF ID.
    
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    Signed-off-by: Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>
    Signed-off-by: Aravindhan Gunasekaran <aravindhan.gunasekaran@intel.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -932,6 +932,10 @@ enum bpf_netdev_command {
 	 */
 	XDP_SETUP_PROG,
 	XDP_SETUP_PROG_HW,
+	/* Setup/query XDP Meta Data BTF */
+	XDP_SETUP_MD_BTF,
+	XDP_QUERY_MD_BTF,
+
 	/* BPF program for offload callbacks, invoked at program load time. */
 	BPF_OFFLOAD_MAP_ALLOC,
 	BPF_OFFLOAD_MAP_FREE,
@@ -955,6 +959,7 @@ struct bpf_xdp_entity {
 	struct bpf_prog *prog;
 	struct bpf_xdp_link *link;
 };
+struct btf;
 
 struct netdev_bpf {
 	enum bpf_netdev_command command;
@@ -963,7 +968,11 @@ struct netdev_bpf {
 		struct {
 			u32 flags;
 			struct bpf_prog *prog;
-			struct netlink_ext_ack *extack;
+		};
+		/* XDP_{SETUP/QUERY}_MD_BTF */
+		struct {
+			u8 btf_enable;
+			u32 btf_id;
 		};
 		/* BPF_OFFLOAD_MAP_ALLOC, BPF_OFFLOAD_MAP_FREE */
 		struct {
@@ -974,6 +983,7 @@ struct netdev_bpf {
 			struct xsk_buff_pool *pool;
 			u16 queue_id;
 		} xsk;
+		struct netlink_ext_ack *extack;
 	};
 };
 
@@ -4095,6 +4105,9 @@ int bpf_xdp_link_attach(const union bpf_attr *attr, struct bpf_prog *prog);
 u8 dev_xdp_prog_count(struct net_device *dev);
 u32 dev_xdp_prog_id(struct net_device *dev, enum bpf_xdp_mode mode);
 
+int dev_xdp_setup_md_btf(struct net_device *dev, struct netlink_ext_ack *extack,
+			 u8 enable);
+u32 dev_xdp_query_md_btf(struct net_device *dev, u8 *enabled);
 int __dev_forward_skb(struct net_device *dev, struct sk_buff *skb);
 int dev_forward_skb(struct net_device *dev, struct sk_buff *skb);
 int dev_forward_skb_nomtu(struct net_device *dev, struct sk_buff *skb);
