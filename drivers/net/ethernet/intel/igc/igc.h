b4a28cc3f24e47f0a482451f5a78e8e73e3b37f7
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2021-06-25 17:33:09 -0700

    igc: Add support for TC_SETUP_PREEMPT
    Saves which queues are marked as preemptible, activating frame
    preemption is done when the user requests it enabled, via ethtool.
    
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    Signed-off-by: Aravindhan Gunasekaran <aravindhan.gunasekaran@intel.com>

--- drivers/net/ethernet/intel/igc/igc.h
+++ drivers/net/ethernet/intel/igc/igc.h
@@ -94,6 +94,7 @@ struct igc_ring {
 	u8 queue_index;                 /* logical index of the ring*/
 	u8 reg_idx;                     /* physical index of the ring */
 	bool launchtime_enable;         /* true if LaunchTime is enabled */
+	bool preemptible;               /* true if not express */
 	ktime_t last_tx_cycle;          /* end of the cycle with a launchtime transmission */
 	ktime_t last_ff_cycle;          /* Last cycle with an active first flag */
 
