1095c3889509175b0c4309f03eba2079d1765070
Ranjan Dutta <ranjan.dutta@intel.com>
2021-07-27 00:06:41 +0800

    Revert "drm/scheduler: Fix hang when sched_entity released"
    This reverts commit 5ed8c298b2e140c640af8813a490fb4d77165e97.

--- drivers/gpu/drm/scheduler/sched_entity.c
+++ drivers/gpu/drm/scheduler/sched_entity.c
@@ -113,8 +113,7 @@ static bool drm_sched_entity_is_idle(struct drm_sched_entity *entity)
 	rmb(); /* for list_empty to work without lock */
 
 	if (list_empty(&entity->list) ||
-	    spsc_queue_count(&entity->job_queue) == 0 ||
-	    entity->stopped)
+	    spsc_queue_count(&entity->job_queue) == 0)
 		return true;
 
 	return false;
