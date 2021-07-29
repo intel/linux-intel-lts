c445f38593e6b150d66a4c03b4c0ca91825a3a62
Matt Roper <matthew.d.roper@intel.com>
2021-07-29 08:21:58 -0700

    drm/i915: Make shadow tables range-based
    Rather than defining our shadow tables as a list of individual
    registers, provide them as a list of register ranges; we'll have some
    ranges of multiple registers being added soon (and we already have a
    couple adjacent registers that we can squash into a single range now).
    
    This change also defines the table with hex literal values rather than
    symbolic register names; since that's how the tables are defined in the
    bspec, this change will make it easier to review the tables overall.
    
    v2:
     - Force signed comparison on range overlap sanity check
    
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    Reviewed-by: Caz Yokoyama <caz.yokoyama@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210729152158.2646246-1-matthew.d.roper@intel.com

--- drivers/gpu/drm/i915/gt/intel_workarounds.c
+++ drivers/gpu/drm/i915/gt/intel_workarounds.c
@@ -2060,12 +2060,7 @@ void intel_engine_apply_workarounds(struct intel_engine_cs *engine)
 	wa_list_apply(engine->gt, &engine->wa_list);
 }
 
-struct mcr_range {
-	u32 start;
-	u32 end;
-};
-
-static const struct mcr_range mcr_ranges_gen8[] = {
+static const struct i915_range mcr_ranges_gen8[] = {
 	{ .start = 0x5500, .end = 0x55ff },
 	{ .start = 0x7000, .end = 0x7fff },
 	{ .start = 0x9400, .end = 0x97ff },
@@ -2074,7 +2069,7 @@ static const struct mcr_range mcr_ranges_gen8[] = {
 	{},
 };
 
-static const struct mcr_range mcr_ranges_gen12[] = {
+static const struct i915_range mcr_ranges_gen12[] = {
 	{ .start =  0x8150, .end =  0x815f },
 	{ .start =  0x9520, .end =  0x955f },
 	{ .start =  0xb100, .end =  0xb3ff },
@@ -2083,7 +2078,7 @@ static const struct mcr_range mcr_ranges_gen12[] = {
 	{},
 };
 
-static const struct mcr_range mcr_ranges_xehp[] = {
+static const struct i915_range mcr_ranges_xehp[] = {
 	{ .start =  0x4000, .end =  0x4aff },
 	{ .start =  0x5200, .end =  0x52ff },
 	{ .start =  0x5400, .end =  0x7fff },
@@ -2102,7 +2097,7 @@ static const struct mcr_range mcr_ranges_xehp[] = {
 
 static bool mcr_range(struct drm_i915_private *i915, u32 offset)
 {
-	const struct mcr_range *mcr_ranges;
+	const struct i915_range *mcr_ranges;
 	int i;
 
 	if (GRAPHICS_VER_FULL(i915) >= IP_VER(12, 50))
