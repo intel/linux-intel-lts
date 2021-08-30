b329ea8afde7b5d6d6d28fd26c4686340508fab0
Kris, Pan <kris.pan@intel.com>
2021-08-30 09:18:40 +0800

    Revert "xhci: add xhci_get_virt_ep() helper"
    This reverts commit ba28765d338ae849af3fc60346f9d457df0cffb3.

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -993,7 +993,6 @@ struct xhci_interval_bw_table {
 	unsigned int		ss_bw_out;
 };
 
-#define EP_CTX_PER_DEV		31
 
 struct xhci_virt_device {
 	struct usb_device		*udev;
@@ -1008,7 +1007,7 @@ struct xhci_virt_device {
 	struct xhci_container_ctx       *out_ctx;
 	/* Used for addressing devices and configuration changes */
 	struct xhci_container_ctx       *in_ctx;
-	struct xhci_virt_ep		eps[EP_CTX_PER_DEV];
+	struct xhci_virt_ep		eps[31];
 	u8				fake_port;
 	u8				real_port;
 	struct xhci_interval_bw_table	*bw_table;
