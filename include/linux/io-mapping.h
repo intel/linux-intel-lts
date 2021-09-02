e7e5faef8f3b0de02336a991404681f7ac6d6c24
Junxiao Chang <junxiao.chang@intel.com>
2021-09-02 19:09:55 +0800

    Revert "io-mapping: Provide iomap_local variant"
    This reverts commit 0f331cfac9f469831b14e1a11fc9ab89f57a7dc0.

--- include/linux/io-mapping.h
+++ include/linux/io-mapping.h
@@ -79,21 +79,6 @@ io_mapping_unmap_atomic(void __iomem *vaddr)
 }
 
 static inline void __iomem *
-io_mapping_map_local_wc(struct io_mapping *mapping, unsigned long offset)
-{
-	resource_size_t phys_addr;
-
-	BUG_ON(offset >= mapping->size);
-	phys_addr = mapping->base + offset;
-	return __iomap_local_pfn_prot(PHYS_PFN(phys_addr), mapping->prot);
-}
-
-static inline void io_mapping_unmap_local(void __iomem *vaddr)
-{
-	kunmap_local_indexed((void __force *)vaddr);
-}
-
-static inline void __iomem *
 io_mapping_map_wc(struct io_mapping *mapping,
 		  unsigned long offset,
 		  unsigned long size)
@@ -112,7 +97,7 @@ io_mapping_unmap(void __iomem *vaddr)
 	iounmap(vaddr);
 }
 
-#else  /* HAVE_ATOMIC_IOMAP */
+#else
 
 #include <linux/uaccess.h>
 
@@ -177,18 +162,7 @@ io_mapping_unmap_atomic(void __iomem *vaddr)
 	preempt_enable();
 }
 
-static inline void __iomem *
-io_mapping_map_local_wc(struct io_mapping *mapping, unsigned long offset)
-{
-	return io_mapping_map_wc(mapping, offset, PAGE_SIZE);
-}
-
-static inline void io_mapping_unmap_local(void __iomem *vaddr)
-{
-	io_mapping_unmap(vaddr);
-}
-
-#endif /* !HAVE_ATOMIC_IOMAP */
+#endif /* HAVE_ATOMIC_IOMAP */
 
 static inline struct io_mapping *
 io_mapping_create_wc(resource_size_t base,
