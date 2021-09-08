efb088d5ae86ed60a5b14b422492a450313e4bc3
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2021-09-07 19:59:35 -0700

    tools: rename bitmap_alloc() to bitmap_zalloc()
    Rename bitmap_alloc() to bitmap_zalloc() in tools to follow the bitmap API
    in the kernel.
    
    No functional changes intended.
    
    Link: https://lkml.kernel.org/r/20210814211713.180533-14-yury.norov@gmail.com
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Yury Norov <yury.norov@gmail.com>
    Suggested-by: Yury Norov <yury.norov@gmail.com>
    Acked-by: Yury Norov <yury.norov@gmail.com>
    Tested-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Acked-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Alexander Lobakin <alobakin@pm.me>
    Cc: Alexey Klimov <aklimov@redhat.com>
    Cc: Dennis Zhou <dennis@kernel.org>
    Cc: Ulf Hansson <ulf.hansson@linaro.org>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- tools/testing/selftests/kvm/dirty_log_perf_test.c
+++ tools/testing/selftests/kvm/dirty_log_perf_test.c
@@ -118,7 +118,7 @@ static void run_test(enum vm_guest_mode mode, unsigned long iterations,
 	guest_num_pages = (nr_vcpus * guest_percpu_mem_size) >> vm_get_page_shift(vm);
 	guest_num_pages = vm_adjust_num_guest_pages(mode, guest_num_pages);
 	host_num_pages = vm_num_host_pages(mode, guest_num_pages);
-	bmap = bitmap_alloc(host_num_pages);
+	bmap = bitmap_zalloc(host_num_pages);
 
 #ifdef USE_CLEAR_DIRTY_LOG
 	cap.cap = KVM_CAP_MANUAL_DIRTY_LOG_PROTECT2;
