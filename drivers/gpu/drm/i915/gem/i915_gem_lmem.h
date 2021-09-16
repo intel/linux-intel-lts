63302fd7ff592e862d93f0ef1aca1fc21b73287a
Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
2021-09-16 09:28:16 -0700

    drm/i915/guc: put all guc objects in lmem when available
    The firmware binary has to be loaded from lmem and the recommendation is
    to put all other objects in there as well. Note that we don't fall back
    to system memory if the allocation in lmem fails because all objects are
    allocated during driver load and if we have issues with lmem at that point
    something is seriously wrong with the system, so no point in trying to
    handle it.
    
    NB: Cherry-picked with conflicts due to missing patches.
    
    Cc: Matthew Auld <matthew.auld@intel.com>
    Cc: Abdiel Janulgue <abdiel.janulgue@linux.intel.com>
    Cc: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Vinay Belgaumkar <vinay.belgaumkar@intel.com>
    Cc: Radoslaw Szwichtenberg <radoslaw.szwichtenberg@intel.com>
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>
    Reviewed-by: Matthew Brost <matthew.brost@intel.com>
    Signed-off-by: John Harrison <John.C.Harrison@Intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210916162819.27848-3-matthew.brost@intel.com
    (cherry picked from commit 7acbbc7cf4851b42b723098aa5699e8fca634e4a)

--- drivers/gpu/drm/i915/gem/i915_gem_lmem.h
+++ drivers/gpu/drm/i915/gem/i915_gem_lmem.h
@@ -22,6 +22,10 @@ i915_gem_object_lmem_io_map(struct drm_i915_gem_object *obj,
 bool i915_gem_object_is_lmem(struct drm_i915_gem_object *obj);
 
 struct drm_i915_gem_object *
+i915_gem_object_create_lmem_from_data(struct drm_i915_private *i915,
+				      const void *data, size_t size);
+
+struct drm_i915_gem_object *
 i915_gem_object_create_lmem(struct drm_i915_private *i915,
 			    resource_size_t size,
 			    unsigned int flags);
