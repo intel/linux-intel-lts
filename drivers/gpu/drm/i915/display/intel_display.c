2a0363d6daed515b32290445a5019adfb7b54eeb
Fernando Ramos <greenfoo@u92.eu>
2021-09-24 08:43:19 +0200

    drm/i915: cleanup: drm_modeset_lock_all() --> DRM_MODESET_LOCK_ALL_BEGIN()
    As requested in Documentation/gpu/todo.rst, replace driver calls to
    drm_modeset_lock_all() with DRM_MODESET_LOCK_ALL_BEGIN() and
    DRM_MODESET_LOCK_ALL_END()
    
    Signed-off-by: Fernando Ramos <greenfoo@u92.eu>
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210924064324.229457-13-greenfoo@u92.eu

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -11638,6 +11638,7 @@ cleanup_bios:
 int intel_modeset_init_nogem(struct drm_i915_private *i915)
 {
 	struct drm_device *dev = &i915->drm;
+	struct drm_modeset_acquire_ctx ctx;
 	enum pipe pipe;
 	struct intel_crtc *crtc;
 	int ret;
@@ -11689,7 +11690,7 @@ int intel_modeset_init_nogem(struct drm_i915_private *i915)
 	intel_vga_disable(i915);
 	intel_setup_outputs(i915);
 
-	drm_modeset_lock_all(dev);
+	DRM_MODESET_LOCK_ALL_BEGIN(dev, ctx, 0, ret);
 	intel_modeset_setup_hw_state(dev, dev->mode_config.acquire_ctx);
 	intel_acpi_assign_connector_fwnodes(i915);
 	drm_modeset_unlock_all(dev);
