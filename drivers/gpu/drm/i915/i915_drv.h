baea0c23408ae3658ba614040bf2746f0d9156b2
Dave Airlie <airlied@redhat.com>
2021-09-29 01:57:56 +0300

    drm/i915: split irq hotplug function from display vtable
    This provide a service from irq to display, so make it separate
    
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/05f533064709764dff8bcfef6a58f9a8482dc5bb.1632869550.git.jani.nikula@intel.com
    (cherry picked from commit de1677c5e32ace87ca69861dc32115e824af3278)

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -322,6 +322,10 @@ struct intel_cdclk_funcs {
 	u8 (*calc_voltage_level)(int cdclk);
 };
 
+struct intel_hotplug_funcs {
+	void (*hpd_irq_setup)(struct drm_i915_private *dev_priv);
+};
+
 struct drm_i915_display_funcs {
 	/* Returns the active state of the crtc, and if the crtc is active,
 	 * fills out the pipe-config with the hw state. */
@@ -339,7 +343,7 @@ struct drm_i915_display_funcs {
 
 	void (*fdi_link_train)(struct intel_crtc *crtc,
 			       const struct intel_crtc_state *crtc_state);
-	void (*hpd_irq_setup)(struct drm_i915_private *dev_priv);
+
 	/* clock updates for mode set */
 	/* cursor updates */
 	/* render clock increase/decrease */
@@ -915,6 +919,9 @@ struct drm_i915_private {
 
 	/* pm display functions */
 	struct drm_i915_wm_disp_funcs wm_disp;
+
+	/* irq display functions */
+	struct intel_hotplug_funcs hotplug_funcs;
 
 	/* Display functions */
 	struct drm_i915_display_funcs display;
