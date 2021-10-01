efc3c828c394efe827eae7cadac9bf13e0f2bd85
Jaegeuk Kim <jaegeuk@kernel.org>
2021-10-01 11:20:14 -0700

    scsi: ufs: core: Retry START_STOP on UNIT_ATTENTION
    Commit 57d104c153d3 ("ufs: add UFS power management support") made the UFS
    driver submit a REQUEST SENSE command before submitting a power management
    command to a WLUN to clear the POWER ON unit attention. Instead of
    submitting a REQUEST SENSE command before submitting a power management
    command, retry the power management command until it succeeds.
    
    This is the preparation to get rid of all UNIT ATTENTION code which should
    be handled by users.
    
    Link: https://lore.kernel.org/r/20211001182015.1347587-2-jaegeuk@kernel.org
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -8565,7 +8565,7 @@ static int ufshcd_set_dev_pwr_mode(struct ufs_hba *hba,
 	 */
 	for (retries = 3; retries > 0; --retries) {
 		ret = scsi_execute(sdp, cmd, DMA_NONE, NULL, 0, NULL, &sshdr,
-				   HZ, 0, 0, RQF_PM, NULL);
+				START_STOP_TIMEOUT, 0, 0, RQF_PM, NULL);
 		if (!scsi_status_is_check_condition(ret) ||
 				!scsi_sense_valid(&sshdr) ||
 				sshdr.sense_key != UNIT_ATTENTION)
