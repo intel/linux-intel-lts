85e17d1619492a1b8067aa7b79d0933ee1c3f675
Malik_Hsu <malik_hsu@wistron.corp-partner.google.com>
2021-10-01 23:03:16 +0800

    ASoC: Intel: sof_rt5682: Add support for max98360a speaker amp
    Add a board config adl_mx98360a_rt5682 to support alc5682 headset
    codec and max98360a speaker amplifier. Follow Intel BT offload design
    by connecting alc5682 to SSP0 and max98360a to SSP1.
    
    Signed-off-by: Malik_Hsu <malik_hsu@wistron.corp-partner.google.com>
    Signed-off-by: Brent Lu <brent.lu@intel.com>
    Acked-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20211001150316.414141-1-brent.lu@intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/common/soc-acpi-intel-adl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-adl-match.c
@@ -209,6 +209,21 @@ static const struct snd_soc_acpi_link_adr adlps_rvp[] = {
 	{}
 };
 
+static const struct snd_soc_acpi_codecs adl_max98373_amp = {
+	.num_codecs = 1,
+	.codecs = {"MX98373"}
+};
+
+static const struct snd_soc_acpi_codecs adl_max98357a_amp = {
+	.num_codecs = 1,
+	.codecs = {"MX98357A"}
+};
+
+static const struct snd_soc_acpi_codecs adl_max98360a_amp = {
+	.num_codecs = 1,
+	.codecs = {"MX98360A"}
+};
+
 struct snd_soc_acpi_mach snd_soc_acpi_intel_adl_machines[] = {
 	{
 		.comp_ids = &adl_rt5682_rt5682s_hp,
@@ -225,55 +240,12 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_adl_machines[] = {
 		.sof_tplg_filename = "sof-adl-max98357a-rt5682.tplg",
 	},
 	{
-		.comp_ids = &adl_rt5682_rt5682s_hp,
+		.id = "10EC5682",
 		.drv_name = "adl_mx98360_rt5682",
 		.machine_quirk = snd_soc_acpi_codec_list,
 		.quirk_data = &adl_max98360a_amp,
+		.sof_fw_filename = "sof-adl.ri",
 		.sof_tplg_filename = "sof-adl-max98360a-rt5682.tplg",
-	},
-	{
-		.id = "10508825",
-		.drv_name = "adl_rt1019p_nau8825",
-		.machine_quirk = snd_soc_acpi_codec_list,
-		.quirk_data = &adl_rt1019p_amp,
-		.sof_tplg_filename = "sof-adl-rt1019-nau8825.tplg",
-	},
-	{
-		.id = "10508825",
-		.drv_name = "adl_max98373_nau8825",
-		.machine_quirk = snd_soc_acpi_codec_list,
-		.quirk_data = &adl_max98373_amp,
-		.sof_tplg_filename = "sof-adl-max98373-nau8825.tplg",
-	},
-	{
-		.id = "10508825",
-		.drv_name = "adl_mx98360a_nau8825",
-		.machine_quirk = snd_soc_acpi_codec_list,
-		.quirk_data = &adl_max98360a_amp,
-		.sof_tplg_filename = "sof-adl-mx98360a-nau8825.tplg",
-	},
-	{
-		.id = "10508825",
-		.drv_name = "sof_nau8825",
-		.sof_tplg_filename = "sof-adl-nau8825.tplg",
-	},
-	{
-		.comp_ids = &adl_rt5682_rt5682s_hp,
-		.drv_name = "adl_max98390_rt5682",
-		.machine_quirk = snd_soc_acpi_codec_list,
-		.quirk_data = &adl_max98390_amp,
-		.sof_tplg_filename = "sof-adl-max98390-rt5682.tplg",
-	},
-	{
-		.comp_ids = &adl_rt5682_rt5682s_hp,
-		.drv_name = "adl_rt5682",
-		.sof_tplg_filename = "sof-adl-rt5682.tplg",
-	},
-	/* place amp-only boards in the end of table */
-	{
-		.id = "CSC3541",
-		.drv_name = "adl_cs35l41",
-		.sof_tplg_filename = "sof-adl-cs35l41.tplg",
 	},
 	{},
 };
