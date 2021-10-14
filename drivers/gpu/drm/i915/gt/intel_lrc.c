e28bccce3b5cab939c005108985833391930064b
Matthew Brost <matthew.brost@intel.com>
2021-10-14 10:19:48 -0700

    drm/i915/guc: Add multi-lrc context registration
    Add multi-lrc context registration H2G. In addition a workqueue and
    process descriptor are setup during multi-lrc context registration as
    these data structures are needed for multi-lrc submission.
    
    v2:
     (John Harrison)
      - Move GuC specific fields into sub-struct
      - Clean up WQ defines
      - Add comment explaining math to derive WQ / PD address
    v3:
     (John Harrison)
      - Add PARENT_SCRATCH_SIZE define
      - Update comment explaining multi-lrc register
    v4:
     (John Harrison)
      - Move PARENT_SCRATCH_SIZE to common file
    
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>
    Reviewed-by: John Harrison <John.C.Harrison@Intel.com>
    Signed-off-by: John Harrison <John.C.Harrison@Intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211014172005.27155-9-matthew.brost@intel.com
    (cherry picked from commit c2aa552ff09daf78944f44e98d366009b27f1b63)
    
    FIXME: Patch hacked as LMEM / TTM isn't backported from upstream

--- drivers/gpu/drm/i915/gt/intel_lrc.c
+++ drivers/gpu/drm/i915/gt/intel_lrc.c
@@ -942,6 +942,11 @@ __lrc_alloc_state(struct intel_context *ce, struct intel_engine_cs *engine)
 		context_size += PAGE_SIZE;
 	}
 
+	if (intel_context_is_parent(ce) && intel_engine_uses_guc(engine)) {
+		ce->parallel.guc.parent_page = context_size / PAGE_SIZE;
+		context_size += PARENT_SCRATCH_SIZE;
+	}
+
 	obj = i915_gem_object_create_lmem(engine->i915, context_size, 0);
 	if (IS_ERR(obj))
 		obj = i915_gem_object_create_shmem(engine->i915, context_size);
