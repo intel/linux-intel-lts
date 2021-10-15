b1baf05308df696a6de57b927e81e53afb98a832
Cezary Rojewski <cezary.rojewski@intel.com>
2021-10-15 18:12:53 +0200

    ASoC: topology: Add header payload_size verification
    commit 86e2d14b6d1a68941b6c0ef39502ec1433b680cb upstream.
    
    Add sanity check to make sure the data is read within file boundary.
    Helps in situations where file is only partially copied or malformed.
    
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>
    Link: https://lore.kernel.org/r/20211015161257.27052-3-cezary.rojewski@intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/soc-topology.c
+++ sound/soc/soc-topology.c
@@ -2443,6 +2443,7 @@ static int soc_tplg_manifest_load(struct soc_tplg *tplg,
 		_manifest = manifest;
 	} else {
 		abi_match = false;
+
 		ret = manifest_new_ver(tplg, manifest, &_manifest);
 		if (ret < 0)
 			return ret;
@@ -2470,6 +2471,14 @@ static int soc_valid_header(struct soc_tplg *tplg,
 			"ASoC: invalid header size for type %d at offset 0x%lx size 0x%zx.\n",
 			le32_to_cpu(hdr->type), soc_tplg_get_hdr_offset(tplg),
 			tplg->fw->size);
+		return -EINVAL;
+	}
+
+	if (soc_tplg_get_hdr_offset(tplg) + hdr->payload_size >= tplg->fw->size) {
+		dev_err(tplg->dev,
+			"ASoC: invalid header of type %d at offset %ld payload_size %d\n",
+			le32_to_cpu(hdr->type), soc_tplg_get_hdr_offset(tplg),
+			hdr->payload_size);
 		return -EINVAL;
 	}
 
