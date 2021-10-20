7345be5866b1022940df926bd79676d987d00242
Lay Kuan Loon <kuan.loon.lay@intel.com>
2021-10-20 14:14:42 +0800

    net: networkproxy: add configfs
    Add configfs to read/write protocol handling decision and management
    info database that used in Network Proxy Agent.
    
    Signed-off-by: Lay Kuan Loon <kuan.loon.lay@intel.com>
    Signed-off-by: Ong Boon Leong <boon.leong.ong@intel.com>
    Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>
    Signed-off-by: Tan Tee Min <tee.min.tan@intel.com>
    Signed-off-by: Noor Azura Ahmad Tarmizi <noor.azura.ahmad.tarmizi@intel.com>

--- drivers/net/networkproxy/network_proxy.c
+++ drivers/net/networkproxy/network_proxy.c
@@ -1,11 +1,25 @@
 // SPDX-License-Identifier: GPL-2.0
 /* Copyright (c) 2022, Intel Corporation. */
 
+#include <linux/delay.h>
+#include <linux/inetdevice.h>
+#include <linux/ipv6.h>
+#include <linux/list.h>
 #include <linux/module.h>
+#include <linux/netdevice.h>
 #include <linux/network_proxy.h>
+#include <linux/sched.h>
 #include <linux/slab.h>
+#include <linux/wait.h>
+#include <net/addrconf.h>
+#include <net/if_inet6.h>
+
+/* timeout 1 second */
+#define NP_TIMEOUT_MS	1000
 
 static struct np_context *np_ctx;
+static struct task_struct *agent_access_task;
+static unsigned char *ipc_ptr;
 
 int netprox_send_ipc_msg(int cmd, const char *msg, int size)
 {
@@ -37,6 +51,270 @@ int netprox_send_ipc_msg(int cmd, const char *msg, int size)
 	return 0;
 }
 EXPORT_SYMBOL(netprox_send_ipc_msg);
+
+static int netprox_read_from_agent(struct np_rules *rule, void *content,
+				   int *size)
+{
+	struct np_rules *ipc_resp;
+	void *ipc_res;
+	long time;
+
+	netprox_send_ipc_msg(NP_H2A_CMD_READ_CLS_RULE, (const char *)rule,
+			     sizeof(struct np_rules));
+
+	agent_access_task = current;
+	set_current_state(TASK_INTERRUPTIBLE);
+	time = schedule_timeout(msecs_to_jiffies(NP_TIMEOUT_MS));
+	if (time > 0) {
+		/* If time > 0 mean Agent response
+		 * NP_A2H_CMD_READ_CLS_RULE_RESULT, check the response
+		 * is same as query
+		 */
+		ipc_resp = (struct np_rules *)ipc_ptr;
+		if (rule->group == ipc_resp->group &&
+		    rule->type == ipc_resp->type &&
+		    rule->offset == ipc_resp->offset &&
+		    rule->size == ipc_resp->size) {
+			/* only copy the content without struct np_rules */
+			ipc_res = ipc_resp + 1;
+			memcpy(content, ipc_res, ipc_resp->size);
+		}
+	} else {
+		pr_err("netprox read rule timeout\n");
+		return -EPERM;
+	}
+
+	return 0;
+}
+
+static int netprox_process_classifier_rule_read(struct np_rules *rule,
+						void *content, int *size)
+{
+	int ret;
+
+	switch (rule->type) {
+	case NP_RL_T_IPV4:
+	case NP_RL_T_IPV6:
+	case NP_RL_T_SNMP:
+	case NP_RL_T_TCP_WAKE_PORT:
+	case NP_RL_T_MDNS:
+		ret = netprox_read_from_agent(rule, content, size);
+		break;
+	default:
+		ret = -EINVAL;
+		break;
+	}
+
+	return ret;
+}
+
+static int netprox_process_mib_rule_read(struct np_rules *rule, void *content,
+					 int *size)
+{
+	int ret;
+
+	switch (rule->type) {
+	case NP_RL_T_MAC_ADDR:
+	case NP_RL_T_IPV4:
+	case NP_RL_T_IPV6:
+	case NP_RL_T_SNMP_COMMUNITY_STR:
+	case NP_RL_T_TCP_WAKE_PORT:
+	case NP_RL_T_IPV4_SUBNET:
+		ret = netprox_read_from_agent(rule, content, size);
+		break;
+	default:
+		ret = -EINVAL;
+		break;
+	}
+
+	return ret;
+}
+
+int netprox_read_rule(struct np_rules *rule, void *content, int *size)
+{
+	int ret;
+
+	switch (rule->group) {
+	case NP_RL_G_CLS:
+		ret = netprox_process_classifier_rule_read(rule, content, size);
+		break;
+	case NP_RL_G_MIB:
+		ret = netprox_process_mib_rule_read(rule, content, size);
+		break;
+	default:
+		ret = -EINVAL;
+		break;
+	}
+
+	return ret;
+}
+EXPORT_SYMBOL(netprox_read_rule);
+
+static int netprox_send_netdev_mib(int rule_type)
+{
+	struct inet6_ifaddr *ifp, *tmp;
+	struct in_device *indevice;
+	struct in_ifaddr *inifaddr;
+	struct net_device *ndev;
+	struct inet6_dev *idev;
+	struct np_rules *rule;
+	int size, i;
+	int ret = 0;
+	char *ptr;
+
+	if (np_ctx->np_netdev) {
+		ndev = np_ctx->np_netdev->netdev;
+	} else {
+		pr_err("netprox net device not register\n");
+		return -ENODEV;
+	}
+
+	size = 0;
+
+	switch (rule_type) {
+	case NP_RL_T_MAC_ADDR:
+		size = sizeof(struct np_rules) + NP_MAC_ADDR_BYTES;
+		break;
+	case NP_RL_T_IPV4_SUBNET:
+	case NP_RL_T_IPV4:
+		indevice = ndev->ip_ptr;
+		if (indevice) {
+			inifaddr = indevice->ifa_list;
+			if (inifaddr) {
+				/* only set size if ipv4 info is valid */
+				size = sizeof(struct np_rules)
+				       + NP_IPV4_ADDR_BYTES;
+			} else {
+				pr_err("IPV4 MIB ifa_list is null\n");
+			}
+		} else {
+			pr_err("IPV4 MIB ip_ptr is null\n");
+		}
+		break;
+	case NP_RL_T_IPV6:
+		idev = ndev->ip6_ptr;
+		if (idev) {
+			/* only set size if ipv6 info is valid */
+			size = sizeof(struct np_rules) +
+				(NP_IPV6_ADDR_ARRAY * NP_IPV6_ADDR_BYTES);
+		} else {
+			pr_err("IPV6 MIB ip6_ptr is null\n");
+		}
+		break;
+	default:
+		pr_err("unknown MIB type\n");
+		break;
+	}
+
+	if (size == 0)
+		return -EPERM;
+
+	rule = kzalloc(size, GFP_KERNEL);
+	if (!rule)
+		return -ENOMEM;
+
+	/* Set rule of MIB */
+	rule->group = NP_RL_G_MIB;
+	rule->type = rule_type;
+	rule->offset = 0;
+	ptr = (char *)rule->value;
+
+	switch (rule_type) {
+	case NP_RL_T_MAC_ADDR:
+		memcpy(ptr, ndev->dev_addr, NP_MAC_ADDR_BYTES);
+		break;
+	case NP_RL_T_IPV4:
+		indevice = ndev->ip_ptr;
+		inifaddr = indevice->ifa_list;
+		memcpy(ptr, (void *)&inifaddr->ifa_address, NP_IPV4_ADDR_BYTES);
+		break;
+	case NP_RL_T_IPV6:
+		i = 0;
+		idev = ndev->ip6_ptr;
+		size = sizeof(struct np_rules);
+		list_for_each_entry_safe(ifp, tmp, &idev->addr_list,
+					 if_list) {
+			if (i++ == NP_IPV6_ADDR_ARRAY) {
+				pr_err("more than %d IPV6 addr\n",
+				       NP_IPV6_ADDR_ARRAY);
+				break;
+			}
+			size += NP_IPV6_ADDR_BYTES;
+			memcpy(ptr, (void *)&ifp->addr, NP_IPV6_ADDR_BYTES);
+			ptr += NP_IPV6_ADDR_BYTES;
+		}
+		break;
+	case NP_RL_T_IPV4_SUBNET:
+		indevice = ndev->ip_ptr;
+		inifaddr = indevice->ifa_list;
+		memcpy(ptr, (void *)&inifaddr->ifa_mask, NP_IPV4_ADDR_BYTES);
+		break;
+	default:
+		ret = -EPERM;
+		pr_err("unknown MIB type\n");
+		break;
+	}
+
+	if (ret) {
+		kfree(rule);
+		return ret;
+	}
+
+	rule->size = size - sizeof(struct np_rules);
+	ret = netprox_send_ipc_msg(NP_H2A_CMD_WRITE_CLS_RULE,
+				   (const char *)rule,
+				   size);
+	kfree(rule);
+
+	return ret;
+}
+
+static int netprox_process_classifier_rule_write(struct np_rules *rule,
+						 int size)
+{
+	unsigned short *value;
+	int ret = 0;
+
+	switch (rule->type) {
+	case NP_RL_T_IPV4:
+	case NP_RL_T_IPV6:
+		/* Check if IPv4/IPv6 rule is enable then send IPv4/IPv6 info */
+		value = (unsigned short *)(rule->value);
+		if (*value & NP_RL_CLS_ENABLE) {
+			ret = netprox_send_netdev_mib(NP_RL_T_MAC_ADDR);
+			if (ret == 0)
+				ret = netprox_send_netdev_mib(rule->type);
+			if (ret == 0)
+				ret =
+				netprox_send_netdev_mib(NP_RL_T_IPV4_SUBNET);
+		}
+		break;
+	default:
+		break;
+	}
+
+	ret |= netprox_send_ipc_msg(NP_H2A_CMD_WRITE_CLS_RULE,
+				    (const char *)rule,
+				    size);
+	return ret;
+}
+
+int netprox_write_rule(struct np_rules *rule, int size)
+{
+	int ret;
+
+	switch (rule->group) {
+	case NP_RL_G_CLS:
+		ret = netprox_process_classifier_rule_write(rule, size);
+		break;
+	default:
+		ret = -EINVAL;
+		break;
+	}
+
+	return ret;
+}
+EXPORT_SYMBOL(netprox_write_rule);
 
 int netprox_agent_is_ready(void)
 {
@@ -80,6 +358,10 @@ int netprox_ipc_recv(int cmd, unsigned char *payload, int size)
 
 	/* Process IPC message from Network Proxy Agent */
 	switch (cmd) {
+	case NP_A2H_CMD_AGENT_INFO:
+		if (size == sizeof(struct np_agent_info))
+			memcpy(&np_ctx->np_agent_info, payload, size);
+		break;
 	case NP_A2H_CMD_AGENT_READY:
 		np_ctx->agent_state = NP_AGENT_READY;
 		np_ctx->np_netdev->proxy_wakeup_enable(netdev, 1);
@@ -92,6 +374,13 @@ int netprox_ipc_recv(int cmd, unsigned char *payload, int size)
 		break;
 	case NP_A2H_CMD_HOST_IS_EXITED:
 		np_ctx->host_state = NP_HOST_PROXY_EXIT;
+		break;
+	case NP_A2H_CMD_READ_CLS_RULE_RESULT:
+		ipc_ptr = payload;
+		if (agent_access_task)
+			wake_up_process(agent_access_task);
+		else
+			pr_err("Received cls_rule_result after timeout.\n");
 		break;
 	default:
 		pr_err("%s unknown command %d\n", __func__, cmd);
@@ -133,6 +422,22 @@ int netprox_deregister_netdev(struct np_netdev *np_netdev)
 	return 0;
 }
 EXPORT_SYMBOL(netprox_deregister_netdev);
+
+int netprox_register_configfs(struct np_configfs *np_configfs)
+{
+	np_ctx->np_configfs = np_configfs;
+
+	return np_cfs_agent_info(np_ctx, np_ctx->np_agent_info);
+}
+EXPORT_SYMBOL(netprox_register_configfs);
+
+int netprox_deregister_configfs(struct np_configfs *np_configfs)
+{
+	np_ctx->np_configfs = NULL;
+
+	return 0;
+}
+EXPORT_SYMBOL(netprox_deregister_configfs);
 
 static int __init network_proxy_init(void)
 {
