4c962bc4c72c6773bc46735b9e9e55fce6d98fc4
Lay Kuan Loon <kuan.loon.lay@intel.com>
2021-10-20 14:14:42 +0800

    net: networkproxy: add configfs
    Add configfs to read/write protocol handling decision and management
    info database that used in Network Proxy Agent.
    
    Signed-off-by: Lay Kuan Loon <kuan.loon.lay@intel.com>
    Signed-off-by: Ong Boon Leong <boon.leong.ong@intel.com>
    Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>
    Signed-off-by: Tan Tee Min <tee.min.tan@intel.com>
    Signed-off-by: Noor Azura Ahmad Tarmizi <noor.azura.ahmad.tarmizi@intel.com>

--- include/linux/network_proxy.h
+++ include/linux/network_proxy.h
@@ -34,23 +34,48 @@ struct np_netdev {
 	void (*proxy_wakeup_enable)(struct net_device *netdev, bool enable);
 };
 
+/* Network Device for Network Proxy Agent */
+struct np_configfs {
+	void (*agent_info)(struct np_agent_info *info);
+};
+
 /* Network Proxy Context */
 struct np_context {
 	enum np_host_state host_state;
 	enum np_agent_state agent_state;
 	struct np_ipcdev *np_ipcdev;
 	struct np_netdev *np_netdev;
+	struct np_configfs *np_configfs;
+	struct np_agent_info np_agent_info;
 };
 
 int netprox_agent_is_ready(void);
 int netprox_host_proxy_enter(void);
 int netprox_host_proxy_exit(void);
 int netprox_send_ipc_msg(int cmd, const char *msg, int size);
+int netprox_read_rule(struct np_rules *rule, void *ptr, int *size);
+int netprox_write_rule(struct np_rules *rule, int size);
 int netprox_ipc_recv(int cmd, unsigned char *payload, int size);
 int netprox_register_ipcdev(struct np_ipcdev *np_ipcdev);
 int netprox_deregister_ipcdev(struct np_ipcdev *np_ipcdev);
 int netprox_register_netdev(struct np_netdev *np_netdev, void *config,
 			    int size);
 int netprox_deregister_netdev(struct np_netdev *np_netdev);
+int netprox_register_configfs(struct np_configfs *np_cfs);
+int netprox_deregister_configfs(struct np_configfs *np_cfs);
+
+static inline int np_cfs_agent_info(struct np_context *ctx,
+				    struct np_agent_info np_agent_info)
+{
+	if (ctx->np_configfs && np_agent_info.major) {
+		ctx->np_configfs->agent_info(&np_agent_info);
+
+		return 0;
+	}
+
+	pr_err("Network Proxy Configfs registration fail.\n");
+
+	return -ENODEV;
+}
 
 #endif /* __NETWORK_PROXY_H__ */
