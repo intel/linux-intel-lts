3ff4c4cd8883e9ab6f7230e19a9b1b54fb6f26d4
Aravindhan Gunasekaran <aravindhan.gunasekaran@intel.com>
2021-10-22 21:48:19 +0530

    igc: Add context for empty packet
    We see an issue with the dummy(empty) packet (mis)using
    previous valid data packet context. Since context descriptors
    need to be replaced in the internal HW maintained register
    memory, we initialize a simple context for the dummy packet,
    though we really don't need one.
    
    Signed-off-by: Aravindhan Gunasekaran <aravindhan.gunasekaran@intel.com>

--- drivers/net/ethernet/intel/igc/igc_main.c
+++ drivers/net/ethernet/intel/igc/igc_main.c
@@ -2903,8 +2903,8 @@ static void igc_update_tx_stats(struct igc_q_vector *q_vector,
 	q_vector->tx.total_packets += packets;
 }
 
-static void igc_launchtm_ctxtdesc(struct igc_ring *tx_ring,
-				  ktime_t txtime)
+static int igc_launchtm_ctxtdesc(struct igc_ring *tx_ring,
+				 ktime_t txtime)
 {
 	bool first_flag = false, insert_empty = false;
 	struct igc_adv_tx_context_desc *context_desc;
@@ -2912,6 +2912,7 @@ static void igc_launchtm_ctxtdesc(struct igc_ring *tx_ring,
 	u32 mss_l4len_idx = 0;
 	u32 type_tucmd = 0;
 	__le32 launch_time;
+	int desc_used = 0;
 
 	launch_time = igc_tx_launchtime(tx_ring, txtime, &first_flag, &insert_empty);
 
@@ -2926,8 +2927,15 @@ static void igc_launchtm_ctxtdesc(struct igc_ring *tx_ring,
 		data = skb_put(empty, IGC_EMPTY_FRAME_SIZE);
 		memset(data, 0, IGC_EMPTY_FRAME_SIZE);
 
+		igc_tx_ctxtdesc(tx_ring, 0, false, 0, 0, 0);
+
 		if (igc_init_tx_empty_descriptor(tx_ring, empty) < 0)
 			dev_kfree_skb_any(empty);
+
+		/* 1 desc. for Empty packet data
+		 * 1 desc. for Empty packet context
+		 */
+		desc_used += 2;
 	}
 
 done:
@@ -2935,6 +2943,7 @@ done:
 	context_desc = IGC_TX_CTXTDESC(tx_ring, i);
 
 	i++;
+	desc_used++;
 	tx_ring->next_to_use = (i < tx_ring->count) ? i : 0;
 
 	/* set bits to identify this as an advanced context descriptor */
@@ -2951,6 +2960,8 @@ done:
 	context_desc->type_tucmd_mlhl	= cpu_to_le32(type_tucmd);
 	context_desc->mss_l4len_idx	= cpu_to_le32(mss_l4len_idx);
 	context_desc->launch_time = launch_time;
+
+	return desc_used;
 }
 
 static void igc_xdp_xmit_zc(struct igc_ring *ring)
@@ -2970,7 +2981,7 @@ static void igc_xdp_xmit_zc(struct igc_ring *ring)
 	ntu = ring->next_to_use;
 	budget = igc_desc_unused(ring);
 
-	while (xsk_tx_peek_desc(pool, &xdp_desc) && budget > 2) {
+	while (xsk_tx_peek_desc(pool, &xdp_desc) && budget > 3) {
 		u32 cmd_type, olinfo_status;
 		struct igc_tx_buffer *bi;
 		ktime_t launch_tm = 0;
@@ -2980,8 +2991,7 @@ static void igc_xdp_xmit_zc(struct igc_ring *ring)
 
 		if (ring->launchtime_enable && xdp_desc.txtime > 0) {
 			launch_tm = ns_to_ktime(xdp_desc.txtime);
-			budget--;
-			igc_launchtm_ctxtdesc(ring, launch_tm);
+			budget -= igc_launchtm_ctxtdesc(ring, launch_tm);
 		}
 
 		/* re-read ntu as igc_launchtm_ctxtdesc() updates it */
