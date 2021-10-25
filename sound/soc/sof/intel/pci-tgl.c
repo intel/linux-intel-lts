239e053411b769fd7f0bbdcc0ab5d83e88d9ecf6
Muralidhar Reddy <muralidhar.reddy@intel.com>
2021-10-25 11:49:39 +0530

    ALSA: hda: Add AlderLake-PS PCI ID
    Signed-off-by: Muralidhar Reddy <muralidhar.reddy@intel.com>

--- sound/soc/sof/intel/pci-tgl.c
+++ sound/soc/sof/intel/pci-tgl.c
@@ -115,6 +115,8 @@ static const struct pci_device_id sof_pci_ids[] = {
 		.driver_data = (unsigned long)&adls_desc},
 	{ PCI_DEVICE(0x8086, 0x51c8), /* ADL-P */
 		.driver_data = (unsigned long)&adl_desc},
+	{ PCI_DEVICE(0x8086, 0x51c9), /* ADL-PS */
+		.driver_data = (unsigned long)&adl_desc},
 	{ PCI_DEVICE(0x8086, 0x54c8), /* ADL-N */
 		.driver_data = (unsigned long)&adl_desc},
 	{ 0, }
