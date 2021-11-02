56090a245131bfbc58f8b4a0a014cff41e065861
skirank1 <kiran.kumar1.s@intel.com>
2021-11-02 13:24:21 +0530

    misc/xlink-core: Async notification
    Provide notification when TBH turn on and off. Call the user registered
    callback function for event notification.
    
    Signed-off-by: skirank1 <kiran.kumar1.s@intel.com>

--- drivers/misc/xlink-core/xlink-core.h
+++ drivers/misc/xlink-core/xlink-core.h
@@ -43,4 +43,16 @@ enum xlink_error xlink_register_device_event_user(struct xlink_handle *handle,
 enum xlink_error xlink_write_volatile_user(struct xlink_handle *handle,
 					   u16 chan, u8 const *message,
 					   u32 size);
+enum xlink_error xlink_register_device_event_user(struct xlink_handle *handle,
+						  u32 *event_list, u32 num_events,
+						  xlink_device_event_cb event_notif_fn);
+enum xlink_error do_xlink_register_device_event(struct xlink_handle *handle,
+						u32 *event_list, u32 num_events,
+						xlink_device_event_cb event_notif_fn,
+						u32 user_flag);
+enum xlink_error xlink_unregister_device_event_user(struct xlink_handle *handle,
+						    u32 *event_list, u32 num_events);
+enum xlink_error do_xlink_unregister_device_event(struct xlink_handle *handle,
+						  u32 *event_list, u32 num_events,
+						  u32 user_flag);
 #endif /* XLINK_CORE_H_ */
