b0141f3034167bba18f426d3026cb95fd64cb938
Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
2021-11-03 12:14:29 -0700

    i915: drm/i915/gsc: define gsc fw
    Add FW definition and the matching override modparam
    
    v2: Update GSC to v102.0.0.7366.
    v3: Update GSC to v102.0.0.1511
    v4: Update GSC to v102.0.0.1556
    
    v5: FIXME: Skip GSC file-vs-compatibility version check
        This is for GSC fw: in dii-client repo, we only allow explicit GSC
        file names based on full release version. However, upstream will, (once
        fully enabled) only allow GSC file names based on compatibility versions.
    
        In current code, "wanted" and "selected" is supposed to carry the
        compatibility version (i.e. the most important thing for functional ABI
        alignment across driver and firmware). However, in internal repo,
        "selected" properly extracts the compatibility version of the firmware we
        are planning to load while "wanted" is the file name expecting that to
        match the compatibility version - as per what upstream is heading towards.
    
        Thus, this check will fail for dii-client so let's skip that check for now
        until additional code is added (as upstream GSC FW support progresses) to
        separately handle how to handle different wanted file name vs wanted
        compatibility versions.
    
    Working
    
    Signed-off-by: Anusha Srivatsa <anusha.srivatsa@intel.com>
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Signed-off-by: Alan Previn <alan.previn.teres.alexis@intel.com>
    Reviewed-by: Alan Previn <alan.previn.teres.alexis@intel.com>

--- drivers/gpu/drm/i915/gt/uc/intel_uc_fw.c
+++ drivers/gpu/drm/i915/gt/uc/intel_uc_fw.c
@@ -130,6 +130,9 @@ void intel_uc_fw_change_status(struct intel_uc_fw *uc_fw,
 	fw_def(BROXTON,      0, huc_mmp(bxt,  2, 0, 0)) \
 	fw_def(SKYLAKE,      0, huc_mmp(skl,  2, 0, 0))
 
+#define INTEL_GSC_FIRMWARE_DEFS(fw_def, gsc_def) \
+	fw_def(METEORLAKE,   0, gsc_def(mtl, 102, 0, 0, 1556))
+
 /*
  * Set of macros for producing a list of filenames from the above table.
  */
@@ -149,6 +152,14 @@ void intel_uc_fw_change_status(struct intel_uc_fw *uc_fw,
 	__stringify(minor_) "." \
 	__stringify(patch_) ".bin"
 
+#define __MAKE_UC_FW_PATH_MMPB(prefix_, name_, major_, minor_, patch_, build_) \
+	"i915/" \
+	__stringify(prefix_) "_" name_ "_" \
+	__stringify(major_) "." \
+	__stringify(minor_) "." \
+	__stringify(patch_) "." \
+	__stringify(build_) ".bin"
+
 /* Minor for internal driver use, not part of file name */
 #define MAKE_GUC_FW_PATH_MAJOR(prefix_, major_, minor_, patch_) \
 	__MAKE_UC_FW_PATH_MAJOR(prefix_, "guc", major_)
@@ -165,6 +176,9 @@ void intel_uc_fw_change_status(struct intel_uc_fw *uc_fw,
 #define MAKE_HUC_FW_PATH_MMP(prefix_, major_, minor_, patch_) \
 	__MAKE_UC_FW_PATH_MMP(prefix_, "huc", major_, minor_, patch_)
 
+#define MAKE_GSC_FW_PATH(prefix_, major_, minor_, patch, build_) \
+	__MAKE_UC_FW_PATH_MMPB(prefix_, "gsc", major_, minor_, patch, build_)
+
 /*
  * All blobs need to be declared via MODULE_FIRMWARE().
  * This first expansion of the table macros is solely to provide
@@ -175,6 +189,7 @@ void intel_uc_fw_change_status(struct intel_uc_fw *uc_fw,
 
 INTEL_GUC_FIRMWARE_DEFS(INTEL_UC_MODULE_FW, MAKE_GUC_FW_PATH_MAJOR, MAKE_GUC_FW_PATH_MMP)
 INTEL_HUC_FIRMWARE_DEFS(INTEL_UC_MODULE_FW, MAKE_HUC_FW_PATH_BLANK, MAKE_HUC_FW_PATH_MMP, MAKE_HUC_FW_PATH_GSC)
+INTEL_GSC_FIRMWARE_DEFS(INTEL_UC_MODULE_FW, MAKE_GSC_FW_PATH)
 
 /*
  * The next expansion of the table macros (in __uc_fw_auto_select below) provides
@@ -224,6 +239,10 @@ struct __packed uc_fw_blob {
 #define HUC_FW_BLOB_GSC(prefix_) \
 	UC_FW_BLOB_NEW(0, 0, 0, true, MAKE_HUC_FW_PATH_GSC(prefix_))
 
+#define GSC_FW_BLOB(prefix_, major_, minor_, patch_, build_) \
+	UC_FW_BLOB_NEW(major_, minor_, patch_, true, \
+		       MAKE_GSC_FW_PATH(prefix_, major_, minor_, patch_, build_))
+
 struct __packed uc_fw_platform_requirement {
 	enum intel_platform p;
 	u8 rev; /* first platform rev using this FW */
@@ -250,9 +269,14 @@ static const struct uc_fw_platform_requirement blobs_huc[] = {
 	INTEL_HUC_FIRMWARE_DEFS(MAKE_FW_LIST, HUC_FW_BLOB, HUC_FW_BLOB_MMP, HUC_FW_BLOB_GSC)
 };
 
+static const struct uc_fw_platform_requirement blobs_gsc[] = {
+	INTEL_GSC_FIRMWARE_DEFS(MAKE_FW_LIST, GSC_FW_BLOB)
+};
+
 static const struct fw_blobs_by_type blobs_all[INTEL_UC_FW_NUM_TYPES] = {
 	[INTEL_UC_FW_TYPE_GUC] = { blobs_guc, ARRAY_SIZE(blobs_guc) },
 	[INTEL_UC_FW_TYPE_HUC] = { blobs_huc, ARRAY_SIZE(blobs_huc) },
+	[INTEL_UC_FW_TYPE_GSC] = { blobs_gsc, ARRAY_SIZE(blobs_gsc) },
 };
 
 static void
@@ -264,14 +288,6 @@ __uc_fw_auto_select(struct drm_i915_private *i915, struct intel_uc_fw *uc_fw)
 	u8 rev = INTEL_REVID(i915);
 	int i;
 	bool found;
-
-	/*
-	 * GSC FW support is still not fully in place, so we're not defining
-	 * the FW blob yet because we don't want the driver to attempt to load
-	 * it until we're ready for it.
-	 */
-	if (uc_fw->type == INTEL_UC_FW_TYPE_GSC)
-		return;
 
 	/*
 	 * The only difference between the ADL GuC FWs is the HWConfig support.
@@ -794,6 +810,26 @@ int intel_uc_check_file_version(struct intel_uc_fw *uc_fw, bool *old_ver)
 	struct intel_uc_fw_file *selected = &uc_fw->file_selected;
 
 	if (!wanted->ver.major || !selected->ver.major)
+		return 0;
+
+	/*
+	 * FIXME: This is for GSC fw: in dii-client repo, we only allow explicit GSC
+	 * file names based on full release version. However, upstream will, (once
+	 * fully enabled) only allow GSC file names based on compatibility versions.
+	 *
+	 * In current code, "wanted" and "selected" is supposed to carry the
+	 * compatibility version (i.e. the most important thing for functional ABI
+	 * alignment across driver and firmware). However, in internal repo,
+	 * "selected" properly extracts the compatibility version of the firmware we
+	 * are planning to load while "wanted" is the file name expecting that to
+	 * match the compatibility version - as per what upstream is heading towards.
+	 *
+	 * Thus, this check will fail for dii-client so let's skip that check for now
+	 * until additional code is added (as upstream GSC FW support progresses) to
+	 * separately handle how to handle different wanted file name vs wanted
+	 * compatibility versions.
+	 */
+	if (uc_fw->type == INTEL_UC_FW_TYPE_GSC)
 		return 0;
 
 	/* Check the file's major version was as it claimed */
