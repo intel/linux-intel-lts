0c9b36e3f0c92e9c6c865205424e35d2067ff237
Jani Nikula <jani.nikula@intel.com>
2021-11-11 12:13:04 +0200

    drm/i915/driver: add i915_driver_ prefix to functions
    Add the i915_driver_ prefix to the switcheroo functions in
    i915_driver.[ch].
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211111101304.13094-3-jani.nikula@intel.com
    (cherry picked from commit b8d65b8a5aea3309c14cb899149ad5ce93589f58)

--- drivers/gpu/drm/i915/i915_driver.c
+++ drivers/gpu/drm/i915/i915_driver.c
@@ -1230,7 +1230,8 @@ out:
 	return ret;
 }
 
-int i915_suspend_switcheroo(struct drm_i915_private *i915, pm_message_t state)
+int i915_driver_suspend_switcheroo(struct drm_i915_private *i915,
+				   pm_message_t state)
 {
 	int error;
 
@@ -1391,7 +1392,7 @@ static int i915_drm_resume_early(struct drm_device *dev)
 	return ret;
 }
 
-int i915_resume_switcheroo(struct drm_i915_private *i915)
+int i915_driver_resume_switcheroo(struct drm_i915_private *i915)
 {
 	int ret;
 
