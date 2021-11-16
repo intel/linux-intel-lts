55dfc305fbdf78409f29a7805ccecf5a3212e591
Matthew Brost <matthew.brost@intel.com>
2021-11-16 11:49:29 -0800

    drm/i915: Drop stealing of bits from i915_sw_fence function pointer
    Rather than stealing bits from i915_sw_fence function pointer use
    separate fields for function pointer and flags. If using two different
    fields, the 4 byte alignment for the i915_sw_fence function pointer can
    also be dropped.
    
    v2:
     (CI)
      - Set new function field rather than flags in __i915_sw_fence_init
    v3:
     (Tvrtko)
      - Remove BUG_ON(!fence->flags) in reinit as that will now blow up
      - Only define fence->flags if CONFIG_DRM_I915_SW_FENCE_CHECK_DAG is
        defined
    v4:
      - Rebase, resend for CI
    
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>
    Acked-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Alan Previn <alan.previn.teres.alexis@intel.com>
    Signed-off-by: John Harrison <John.C.Harrison@Intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211116194929.10211-1-matthew.brost@intel.com

--- drivers/gpu/drm/i915/i915_request.c
+++ drivers/gpu/drm/i915/i915_request.c
@@ -720,7 +720,7 @@ void i915_request_cancel(struct i915_request *rq, int error)
 	intel_context_cancel_request(rq->context, rq);
 }
 
-static int __i915_sw_fence_call
+static int
 submit_notify(struct i915_sw_fence *fence, enum i915_sw_fence_notify state)
 {
 	struct i915_request *request =
@@ -756,7 +756,7 @@ submit_notify(struct i915_sw_fence *fence, enum i915_sw_fence_notify state)
 	return NOTIFY_DONE;
 }
 
-static int __i915_sw_fence_call
+static int
 semaphore_notify(struct i915_sw_fence *fence, enum i915_sw_fence_notify state)
 {
 	struct i915_request *rq = container_of(fence, typeof(*rq), semaphore);
