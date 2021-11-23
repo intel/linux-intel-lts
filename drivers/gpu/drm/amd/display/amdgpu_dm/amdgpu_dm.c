ad377bf0f0f61b98e9b69771a69fc46470761ab4
Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
2021-11-23 11:56:38 -0500

    drm/amd/display: Fix DPIA outbox timeout after S3/S4/reset
    [Why]
    The HW interrupt gets disabled after S3/S4/reset so we don't receive
    notifications for HPD or AUX from DMUB - leading to timeout and
    black screen with (or without) DPIA links connected.
    
    [How]
    Re-enable the interrupt after S3/S4/reset like we do for the other
    DC interrupts.
    
    Guard both instances of the outbox interrupt enable or we'll hang
    during restore on ASIC that don't support it.
    
    Fixes: 6eff272dbee7ad ("drm/amd/display: Fix DPIA outbox timeout after GPU reset")
    
    Reviewed-by: Jude Shih <Jude.Shih@amd.com>
    Acked-by: Pavle Kotarac <Pavle.Kotarac@amd.com>
    Signed-off-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -2581,7 +2581,8 @@ static int dm_resume(void *handle)
 		 */
 		link_enc_cfg_init(dm->dc, dc_state);
 
-		amdgpu_dm_outbox_init(adev);
+		if (dc_enable_dmub_notifications(adev->dm.dc))
+			amdgpu_dm_outbox_init(adev);
 
 		r = dm_dmub_hw_init(adev);
 		if (r)
