22ba9e7fb202dbe5ac8026382245b2a812fba757
Junxiao Chang <junxiao.chang@intel.com>
2021-11-24 13:16:38 +0800

    Revert "drm/i915/guc: Insert submit fences between requests in parent-child relationship"
    This reverts commit 5dbd7cd261216fa28be6fc54b5f12e2dca27f340.

--- drivers/gpu/drm/i915/gt/uc/intel_guc_submission.c
+++ drivers/gpu/drm/i915/gt/uc/intel_guc_submission.c
@@ -674,7 +674,8 @@ static int rq_prio(const struct i915_request *rq)
 
 static bool is_multi_lrc_rq(struct i915_request *rq)
 {
-	return intel_context_is_parallel(rq->context);
+	return intel_context_is_child(rq->context) ||
+		intel_context_is_parent(rq->context);
 }
 
 static bool can_merge_rq(struct i915_request *rq,
@@ -2845,8 +2846,6 @@ static void guc_parent_context_unpin(struct intel_context *ce)
 	GEM_BUG_ON(!intel_context_is_parent(ce));
 	GEM_BUG_ON(!intel_engine_is_virtual(ce->engine));
 
-	if (ce->last_rq)
-		i915_request_put(ce->last_rq);
 	unpin_guc_id(guc, ce);
 	lrc_unpin(ce);
 }
