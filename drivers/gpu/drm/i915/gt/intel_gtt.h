86e562f9e310d57ddb58bdfe9678cc7d2a1eec2b
Michael Cheng <michael.cheng@intel.com>
2021-12-06 13:52:45 -0800

    drm/i915: Introduce new macros for i915 PTE
    Certain functions within i915 uses macros that are defined for
    specific architectures by the mmu, such as _PAGE_RW and _PAGE_PRESENT
    (Some architectures don't even have these macros defined, like ARM64).
    
    Instead of re-using bits defined for the CPU, we should use bits
    defined for i915. This patch introduces two new 64 bit macros,
    GEN8_PAGE_PRESENT and GEN8_PAGE_RW, to check for bits 0 and 1 and, to
    replace all occurrences of _PAGE_RW and _PAGE_PRESENT within i915.
    
    v2(Michael Cheng): Use GEN8_ instead of I915_
    
    Signed-off-by: Michael Cheng <michael.cheng@intel.com>
    Reviewed-by: Lucas De Marchi <lucas.demarchi@intel.com>
    [ Move defines together with other GEN8 defines ]
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211206215245.513677-2-michael.cheng@intel.com
    (cherry picked from commit 5f97816762722cb08a4e0a124fec02abe0a2208e)
    
     Conflicts:
    	drivers/gpu/drm/i915/gt/intel_gtt.h
    	- Resolved in the context of this kernel

--- drivers/gpu/drm/i915/gt/intel_gtt.h
+++ drivers/gpu/drm/i915/gt/intel_gtt.h
@@ -135,8 +135,12 @@ typedef u64 gen8_pte_t;
 #define GEN8_PPAT_ELLC_OVERRIDE		(0<<2)
 #define GEN8_PPAT(i, x)			((u64)(x) << ((i) * 8))
 
+#define GEN8_PAGE_PRESENT		BIT_ULL(0)
+#define GEN8_PAGE_RW			BIT_ULL(1)
+
 #define GEN8_PDE_IPS_64K BIT(11)
 #define GEN8_PDE_PS_2M   BIT(7)
+#define GEN8_PDPE_PS_1G  BIT_ULL(7)
 
 enum i915_cache_level;
 
