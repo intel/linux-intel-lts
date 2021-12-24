b5d386e27ea160930d3277590e2508534af55125
Zhou Furong <furong.zhou@intel.com>
2021-12-24 11:31:04 +0800

    hantro: add squashed hantro driver
    handro driver is media codec driver for KMB.

---
+++ drivers/gpu/drm/hantro/hantro_cooling.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ *    Hantro thermal cooling header file.
+ *
+ *    Copyright (c) 2021, Intel Corporation
+ */
+
+#ifndef __HANTRO_COOLING_H__
+#define __HANTRO_COOLING_H__
+
+#include "hantro_priv.h"
+
+int setup_thermal_cooling(struct device_info *pdevinfo);
+
+#endif /* __HANTRO_COOLING_H__ */
