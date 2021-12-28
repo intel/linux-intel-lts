98b37af31632ba740e106751d01bb73d84a745d4
Muralidhar Reddy <muralidhar.reddy@intel.com>
2021-12-28 16:28:30 +0530

    ASoC: Intel: common: Add entry for rt711-sdca-sdw codec in ADL match table
    RT711 sdca sdw is added with SDW0 link for ADL-PS platform.
    
    Signed-off-by: Muralidhar Reddy <muralidhar.reddy@intel.com>

--- sound/soc/intel/common/soc-acpi-intel-adl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-adl-match.c
@@ -200,6 +200,15 @@ static const struct snd_soc_acpi_link_adr adl_rvp[] = {
 	{}
 };
 
+static const struct snd_soc_acpi_link_adr adlps_rvp[] = {
+	{
+		.mask = BIT(0),
+		.num_adr = ARRAY_SIZE(rt711_sdca_0_adr),
+		.adr_d = rt711_sdca_0_adr,
+	},
+	{}
+};
+
 struct snd_soc_acpi_mach snd_soc_acpi_intel_adl_machines[] = {
 	{},
 };
@@ -232,6 +241,14 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_adl_sdw_machines[] = {
 		.sof_fw_filename = "sof-adl.ri",
 		.sof_tplg_filename = "sof-adl-rt711.tplg",
 	},
+	{
+		.link_mask = 0x1, /* link0 required */
+		.links = adlps_rvp,
+		.drv_name = "sof_sdw",
+		.sof_fw_filename = "sof-adl.ri",
+		.sof_tplg_filename = "sof-adl-rt711.tplg",
+	},
+
 	{},
 };
 EXPORT_SYMBOL_GPL(snd_soc_acpi_intel_adl_sdw_machines);
