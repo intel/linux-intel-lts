7dd3a2155353ba11993ad459cc68ec794c7c32f9
Mohammad Athari Bin Ismail <mohammad.athari.ismail@intel.com>
2022-01-19 18:03:46 +0800

    net: phy: reconfigure PHY WOL in resume if WOL option still enabled
    When the PHY wakes up from suspend through WOL event, there is a need to
    reconfigure the WOL if the WOL option still enabled. The main operation
    is to clear the WOL event status. So that, subsequent WOL event can be
    triggered properly.
    
    This fix is needed especially for the PHY that operates in PHY_POLL mode
    where there is no handler (such as interrupt handler) available to clear
    the WOL event status.
    
    Fixes: 611d779af7ca ("net: phy: fix MDIO bus PM PHY resuming")
    Signed-off-by: Mohammad Athari Bin Ismail <mohammad.athari.ismail@intel.com>
    Signed-off-by: Ling Pei Lee <pei.lee.ling@intel.com>

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -295,6 +295,7 @@ static __maybe_unused int mdio_bus_phy_suspend(struct device *dev)
 
 static __maybe_unused int mdio_bus_phy_resume(struct device *dev)
 {
+	struct ethtool_wolinfo wol = { .cmd = ETHTOOL_GWOL };
 	struct phy_device *phydev = to_phy_device(dev);
 	int ret;
 
@@ -314,6 +315,13 @@ static __maybe_unused int mdio_bus_phy_resume(struct device *dev)
 	if (ret < 0)
 		return ret;
 no_resume:
+	/* If the PHY has WOL option still enabled, reconfigure the WOL mainly
+	 * to clear the WOL event status.
+	 */
+	phy_ethtool_get_wol(phydev, &wol);
+	if (wol.wolopts)
+		phy_ethtool_set_wol(phydev, &wol);
+
 	if (phydev->attached_dev && phydev->adjust_link)
 		phy_start_machine(phydev);
 
