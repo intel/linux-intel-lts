637413c0a333ec390d7ec23cd882f1b35d9cd82a
Natanael Cintean <natanael.cintean@intel.com>
2022-02-09 15:18:47 +0200

    media: kmb-camera.h: Increase total sensor count to 16
     Extra camera devices needed for stereo support and the total
     camera device count is 15, thus we need the max sensor count to be 16.
    
    Signed-off-by: Natanael Cintean <natanael.cintean@intel.com>

--- drivers/media/platform/kmb-camera/kmb-camera.h
+++ drivers/media/platform/kmb-camera/kmb-camera.h
@@ -14,7 +14,7 @@
 #include "kmb-video.h"
 #include "kmb-isp.h"
 
-#define KMB_MAX_SENSOR_COUNT	11
+#define KMB_MAX_SENSOR_COUNT	16
 #define KMB_MAX_OUTPUT_COUNT	6
 #define KMB_MAX_OUTPUT_NAME_LEN	16
 
