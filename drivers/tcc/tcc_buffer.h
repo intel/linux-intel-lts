f9273194a098f7e5a62a2183a619cbccce5232e2
Qiang Rao <qiang.rao@intel.com>
2022-02-11 00:15:14 +0800

    Assume default hardware prefetch bitmask in measurement function.
    Most platforms supporting tcc use same hardware prefetch bitmask,
    only a few uses different bitmask. With this change, no need to
    patch for every new platform.
    
    Signed-off-by: Qiang Rao <qiang.rao@intel.com>

--- drivers/tcc/tcc_buffer.h
+++ drivers/tcc/tcc_buffer.h
@@ -59,12 +59,8 @@
 /* TCC Device Interface */
 #define TCC_BUFFER_NAME  "/tcc/tcc_buffer"
 #define UNDEFINED_DEVNODE 256
-
 #define TCC_REG_MAX_OFFSET 0x100000
-
-/* IOCTL MAGIC number */
 #define IOCTL_TCC_MAGIC   'T'
-
 enum tcc_buf_region_type {
 	RGN_UNKNOWN = 0,
 	RGN_L1,
@@ -76,9 +72,7 @@ enum tcc_buf_region_type {
 };
 
 /*
- * IN:
  * id: pseudo-SRAM region id from which user request for attribute.
- * OUT:
  * latency: delay in clockcycles
  * type: the type of the memory pSRAM region
  * size: total size in byte
@@ -95,10 +89,8 @@ struct tcc_buf_mem_config_s {
 };
 
 /*
- * IN:
  * id: pseudo-SRAM region id, from which user request for buffer
  * size: buffer size (byte).
- * OUT:
  * devnode: driver returns device node to user
  */
 struct tcc_buf_mem_req_s {
@@ -115,67 +107,67 @@ struct tcc_registers_wl_s {
 
 /* enum type for tcc_register structure */
 enum TCC_REG_PHASE {
-	TCC_PRE_MEM        = 0x00000000,
-	TCC_POST_MEM       = 0x00000001,
-	TCC_LATE_INIT      = 0x00000002,
+	TCC_PRE_MEM = 0,
+	TCC_POST_MEM,
+	TCC_LATE_INIT,
 	TCC_INVALID_PHASE  = 0xFFFFFFFF
 };
 
 enum TCC_REG_FORMAT {
-	TCC_MMIO32         = 0x00000000,
-	TCC_MMIO64         = 0x00000001,
-	TCC_MSR            = 0x00000002,
-	TCC_IOSFSB         = 0x00000003,
-	TCC_MAILBOX        = 0x00000004,
+	TCC_MMIO32 = 0,
+	TCC_MMIO64,
+	TCC_MSR,
+	TCC_IOSFSB,
+	TCC_MAILBOX,
 	TCC_INVALID_FORMAT = 0xFFFFFFFF
 };
 
 enum TCC_IOSFSB_NETWORK {
-	TCC_IOSFSB_CPU     = 0x00000000,
-	TCC_IOSFSB_PCH     = 0x00000001
+	TCC_IOSFSB_CPU = 0,
+	TCC_IOSFSB_PCH
 };
 
 enum TCC_MAILBOX_TYPE {
-	TCC_MAILBOX_SA     = 0x00000000,
-	TCC_MAILBOX_MSR    = 0x00000001
+	TCC_MAILBOX_SA = 0,
+	TCC_MAILBOX_MSR
 };
 
 /* Support mmio32 and mmio64 formats only. */
 struct tcc_register_s {
-	enum TCC_REG_PHASE e_phase;		/* IN: enum'd above */
-	enum TCC_REG_FORMAT e_format;	/* IN: enum'd above, determines which structure format to use */
+	enum TCC_REG_PHASE e_phase;
+	enum TCC_REG_FORMAT e_format;
 	union {
 		struct {
-			u32 base;		/* IN: ECAM format B:D:F:R of BAR (add this to ECAM_BASE) */
-			u32 addr;		/* IN: offset from BAR */
-			u32 mask;		/* IN: data bit-mask (1's are valid) */
-			u32 data;		/* OUT: data value */
+			u32 base;
+			u32 addr;
+			u32 mask;
+			u32 data;
 		} mmio32;
 		struct {
-			u64 base;		/* IN: ECAM format B:D:F:R of BAR (add this to ECAM_BASE) */
-			u64 addr;		/* IN: offset from BAR */
-			u64 mask;		/* IN: data bit-mask (1's are valid) */
-			u64 data;		/* OUT: data value */
+			u64 base;
+			u64 addr;
+			u64 mask;
+			u64 data;
 		} mmio64;
 		struct {
-			u32 apic_id;	/* IN: APIC ID of logical CPU corresponding to this MSR value */
-			u32 addr;		/* IN: ECX value */
-			u64 mask;		/* IN: EDX:EAX data bit-mask (1's are valid) */
-			u64 data;		/* OUT: EDX:EAX data value */
+			u32 apic_id;
+			u32 addr;
+			u64 mask;
+			u64 data;
 		} msr;
 		struct {
-			enum TCC_IOSFSB_NETWORK e_iosfsb_network; /* IN: which IOSFSB network to use */
-			u8 port;		/* IN: IOSFSB Port ID */
-			u8 type;		/* IN: IOSFSB Register Type (Command) */
-			u32 addr;		/* IN: register address */
-			u32 mask;		/* IN: data bit-mask (1's are valid) */
-			u32 data;		/* IN: data value */
+			enum TCC_IOSFSB_NETWORK e_iosfsb_network;
+			u8 port;
+			u8 type;
+			u32 addr;
+			u32 mask;
+			u32 data;
 		} iosfsb;
 		struct {
-			enum TCC_MAILBOX_TYPE e_type;	/* IN: Mailbox type */
-			u32 addr;		/* IN: register address */
-			u32 mask;		/* IN: data bit-mask (1's are valid) */
-			u32 data;		/* OUT: data value */
+			enum TCC_MAILBOX_TYPE e_type;
+			u32 addr;
+			u32 mask;
+			u32 data;
 		} mailbox;
 	} info;
 };
@@ -198,44 +190,13 @@ enum ioctl_index {
 	IOCTL_TCC_MEASURE_USER_END
 };
 
-/*
- * User to get pseudo-SRAM region counts
- */
-#define TCC_GET_REGION_COUNT _IOR(IOCTL_TCC_MAGIC, IOCTL_TCC_GET_REGION_COUNT, unsigned int *)
-
-/*
- * User to get memory config of selected region
- */
-#define TCC_GET_MEMORY_CONFIG _IOWR(IOCTL_TCC_MAGIC, IOCTL_TCC_GET_MEMORY_CONFIG, struct tcc_buf_mem_config_s *)
-
-/*
- * User to query PTCT size
- */
-#define TCC_QUERY_PTCT_SIZE _IOR(IOCTL_TCC_MAGIC, IOCTL_TCC_QUERY_PTCT_SIZE, unsigned int *)
-
-/*
- * User to get PTCT data
- */
-#define TCC_GET_PTCT _IOR(IOCTL_TCC_MAGIC, IOCTL_TCC_GET_PTCT, unsigned int *)
-
-/*
- * User to request pseudo-SRAM buffer from selected region
- */
-#define TCC_REQ_BUFFER _IOWR(IOCTL_TCC_MAGIC, IOCTL_TCC_REQ_BUFFER, struct tcc_buf_mem_req_s *)
-
-/*
- * User to get TCC Register
- */
-#define TCC_GET_REGISTER _IOWR(IOCTL_TCC_MAGIC, IOCTL_TCC_GET_REGISTER, struct tcc_register_s *)
-
-/*
- * User to get TCC Error Log Buffer
- */
-#define TCC_GET_ERRLOG _IOWR(IOCTL_TCC_MAGIC, IOCTL_TCC_GET_ERRLOG, unsigned int *)
-
-/*
- * User to trigger test case on cache
- */
+#define TCC_GET_REGION_COUNT       _IOR(IOCTL_TCC_MAGIC, IOCTL_TCC_GET_REGION_COUNT, unsigned int *)
+#define TCC_GET_MEMORY_CONFIG      _IOWR(IOCTL_TCC_MAGIC, IOCTL_TCC_GET_MEMORY_CONFIG, struct tcc_buf_mem_config_s *)
+#define TCC_QUERY_PTCT_SIZE        _IOR(IOCTL_TCC_MAGIC, IOCTL_TCC_QUERY_PTCT_SIZE, unsigned int *)
+#define TCC_GET_PTCT               _IOR(IOCTL_TCC_MAGIC, IOCTL_TCC_GET_PTCT, unsigned int *)
+#define TCC_REQ_BUFFER             _IOWR(IOCTL_TCC_MAGIC, IOCTL_TCC_REQ_BUFFER, struct tcc_buf_mem_req_s *)
+#define TCC_GET_REGISTER           _IOWR(IOCTL_TCC_MAGIC, IOCTL_TCC_GET_REGISTER, struct tcc_register_s *)
+#define TCC_GET_ERRLOG             _IOWR(IOCTL_TCC_MAGIC, IOCTL_TCC_GET_ERRLOG, unsigned int *)
 #define TCC_MEASURE_CACHE          _IOW(IOCTL_TCC_MAGIC, IOCTL_TCC_MEASURE_CACHE, unsigned int)
 #define TCC_MEASURE_USER_START     _IO(IOCTL_TCC_MAGIC, IOCTL_TCC_MEASURE_USER_START)
 #define TCC_MEASURE_USER_END       _IO(IOCTL_TCC_MAGIC, IOCTL_TCC_MEASURE_USER_END)
