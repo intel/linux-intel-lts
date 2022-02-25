4af5dbd83d0ceeeb11132c41ce85419767ea0206
Kuogee Hsieh <quic_khsieh@quicinc.com>
2022-02-25 13:23:12 -0800

    drm/msm/dp: enable widebus feature for display port
    Widebus feature will transmit two pixel data per pixel clock to interface.
    This feature now is required to be enabled to easy migrant to higher
    resolution applications in future. However since some legacy chipsets
    does not support this feature, this feature is enabled by setting
    wide_bus_en flag to true within msm_dp_desc struct.
    
    changes in v2:
    -- remove compression related code from timing
    -- remove op_info from  struct msm_drm_private
    -- remove unnecessary wide_bus_en variables
    -- pass wide_bus_en into timing configuration by struct msm_dp
    
    Changes in v3:
    -- split patch into 3 patches
    -- enable widebus feature base on chip hardware revision
    
    Changes in v5:
    -- DP_INTF_CONFIG_DATABUS_WIDEN
    
    Changes in v6:
    -- static inline bool msm_dp_wide_bus_enable() in msm_drv.h
    
    Changes in v7:
    -- add Tested-by
    
    Changes in v9:
    -- add wide_bus_en to msm_dp_desc
    
    Changes in v10:
    -- add wide_bus_en boolean to dp_catalog struc to avoid passing it as parameter
    
    Changes in v11:
    -- add const to dp_catalog_hw_revision()
    -- add const to msm_dp_wide_bus_available()
    
    Changes in v12:
    -- dp_catalog_hw_revision(const struct dp_catalog *dp_catalog)
    -- msm_dp_wide_bus_available(const struct msm_dp *dp_display)
    
    Signed-off-by: Kuogee Hsieh <quic_khsieh@quicinc.com>
    Reported-by: kernel test robot <lkp@intel.com>
    Tested-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Reviewed-by: Stephen Boyd <swboyd@chromium.org>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Patchwork: https://patchwork.freedesktop.org/patch/476283/
    Link: https://lore.kernel.org/r/1645824192-29670-5-git-send-email-quic_khsieh@quicinc.com
    [DB: fixed the compilation]
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- drivers/gpu/drm/msm/dp/dp_ctrl.c
+++ drivers/gpu/drm/msm/dp/dp_ctrl.c
@@ -1828,6 +1828,7 @@ int dp_ctrl_on_stream(struct dp_ctrl *dp_ctrl, bool force_link_train)
 	int ret = 0;
 	bool mainlink_ready = false;
 	struct dp_ctrl_private *ctrl;
+	unsigned long pixel_rate_orig;
 
 	if (!dp_ctrl)
 		return -EINVAL;
@@ -1835,6 +1836,10 @@ int dp_ctrl_on_stream(struct dp_ctrl *dp_ctrl, bool force_link_train)
 	ctrl = container_of(dp_ctrl, struct dp_ctrl_private, dp_ctrl);
 
 	ctrl->dp_ctrl.pixel_rate = ctrl->panel->dp_mode.drm_mode.clock;
+
+	pixel_rate_orig = ctrl->dp_ctrl.pixel_rate;
+	if (dp_ctrl->wide_bus_en)
+		ctrl->dp_ctrl.pixel_rate >>= 1;
 
 	DRM_DEBUG_DP("rate=%d, num_lanes=%d, pixel_rate=%d\n",
 		ctrl->link->link_params.rate,
@@ -1870,7 +1875,7 @@ int dp_ctrl_on_stream(struct dp_ctrl *dp_ctrl, bool force_link_train)
 
 	dp_catalog_ctrl_config_msa(ctrl->catalog,
 		ctrl->link->link_params.rate,
-		ctrl->dp_ctrl.pixel_rate, dp_ctrl_use_fixed_nvid(ctrl));
+		pixel_rate_orig, dp_ctrl_use_fixed_nvid(ctrl));
 
 	dp_ctrl_setup_tr_unit(ctrl);
 
