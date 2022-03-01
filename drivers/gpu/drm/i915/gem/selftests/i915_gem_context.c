3864179bd4083b5ff9eb9c4c43c0055048f81f3f
Matt Roper <matthew.d.roper@intel.com>
2022-03-01 15:15:41 -0800

    drm/i915/xehp: CCS should use RCS setup functions
    The compute engine handles the same commands the render engine can
    (except 3D pipeline), so it makes sense that CCS is more similar to RCS
    than non-render engines.
    
    The CCS context state (lrc) is also similar to the render one, so reuse
    it. Note that the compute engine has its own CTX_R_PWR_CLK_STATE
    register.
    
    In order to avoid having multiple RCS && CCS checks, add the following
    engine flag:
     - I915_ENGINE_HAS_RCS_REG_STATE - use the render (larger) reg state ctx.
    
    BSpec: 46260
    Original-author: Michel Thierry
    Cc: Tvrtko Ursulin <tvrtko.ursulin@linux.intel.com>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Signed-off-by: Aravind Iddamsetty <aravind.iddamsetty@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220301231549.1817978-6-matthew.d.roper@intel.com
    (cherry picked from commit c674c5b9342e5cb0f3d9e9bcaf37dbe2087845e5)

--- drivers/gpu/drm/i915/gem/selftests/i915_gem_context.c
+++ drivers/gpu/drm/i915/gem/selftests/i915_gem_context.c
@@ -895,7 +895,9 @@ out_file:
 	return err;
 }
 
-static int rpcs_query_batch(struct drm_i915_gem_object *rpcs, struct i915_vma *vma)
+static int rpcs_query_batch(struct drm_i915_gem_object *rpcs,
+			    struct i915_vma *vma,
+			    struct intel_engine_cs *engine)
 {
 	u32 *cmd;
 
@@ -906,7 +908,7 @@ static int rpcs_query_batch(struct drm_i915_gem_object *rpcs, struct i915_vma *v
 		return PTR_ERR(cmd);
 
 	*cmd++ = MI_STORE_REGISTER_MEM_GEN8;
-	*cmd++ = i915_mmio_reg_offset(GEN8_R_PWR_CLK_STATE(RENDER_RING_BASE));
+	*cmd++ = i915_mmio_reg_offset(GEN8_R_PWR_CLK_STATE(engine->mmio_base));
 	*cmd++ = lower_32_bits(vma->node.start);
 	*cmd++ = upper_32_bits(vma->node.start);
 	*cmd = MI_BATCH_BUFFER_END;
@@ -967,7 +969,7 @@ retry:
 	if (err)
 		goto err_vma;
 
-	err = rpcs_query_batch(rpcs, vma);
+	err = rpcs_query_batch(rpcs, vma, ce->engine);
 	if (err)
 		goto err_batch;
 
