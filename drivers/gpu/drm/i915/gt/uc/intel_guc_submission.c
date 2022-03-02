e712fb8f4b62512871956fe34cebbe1a8ab7d068
John Harrison <John.C.Harrison@Intel.com>
2022-03-01 16:33:54 -0800

    drm/i915/guc: Move lrc desc setup to where it is needed
    The LRC descriptor was being initialised early on in the context
    registration sequence. It could then be determined that the actual
    registration needs to be delayed and the descriptor would be wiped
    out. This is inefficient, so move the setup to later in the process
    after the point of no return.
    
    v2: Move some split changes into the split patch (and do them
    correctly).
    
    Signed-off-by: John Harrison <John.C.Harrison@Intel.com>
    Reviewed-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220302003357.4188363-6-John.C.Harrison@Intel.com
    (cherry picked from commit 8e2e9c435e2bdcf3cbd53a0e82877616ae9a513a)

--- drivers/gpu/drm/i915/gt/uc/intel_guc_submission.c
+++ drivers/gpu/drm/i915/gt/uc/intel_guc_submission.c
@@ -2170,6 +2170,8 @@ static int __guc_action_register_context(struct intel_guc *guc,
 					     0, loop);
 }
 
+static void prepare_context_registration_info(struct intel_context *ce);
+
 static int register_context(struct intel_context *ce, bool loop)
 {
 	struct intel_guc *guc = ce_to_guc(ce);
@@ -2179,6 +2181,8 @@ static int register_context(struct intel_context *ce, bool loop)
 
 	GEM_BUG_ON(intel_context_is_child(ce));
 	trace_intel_context_register(ce);
+
+	prepare_context_registration_info(ce);
 
 	if (intel_context_is_parent(ce))
 		ret = __guc_action_register_multi_lrc(guc, ce, ce->guc_id.id,
@@ -2333,8 +2337,6 @@ static int try_context_registration(struct intel_context *ce, bool loop)
 
 	clr_ctx_id_mapping(guc, desc_idx);
 	set_ctx_id_mapping(guc, desc_idx, ce);
-
-	prepare_context_registration_info(ce);
 
 	/*
 	 * The context_lookup xarray is used to determine if the hardware
