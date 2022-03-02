4850ead219571ffb1188bd8ac9d552c18feffbee
G, IndhuX <indhux.g@intel.com>
2022-03-02 22:12:02 +0530

    net: wwan: t7xx: Device deep sleep lock/unlock
    Introduce the mechanism to lock/unlock the device 'deep sleep' mode.
    When the PCIe link state is L1.2 or L2, the host side still can keep
    the device is in D0 state from the host side point of view. At the same
    time, if the device's 'deep sleep' mode is unlocked, the device will
    go to 'deep sleep' while it is still in D0 state on the host side.
    
    Signed-off-by: G, IndhuX <indhux.g@intel.com>
    Signed-off-by: Haijun Liu <haijun.liu@mediatek.com>
    Signed-off-by: Chandrashekar Devegowda <chandrashekar.devegowda@intel.com>
    Co-developed-by: Ricardo Martinez <ricardo.martinez@linux.intel.com>
    Signed-off-by: Ricardo Martinez <ricardo.martinez@linux.intel.com>

--- drivers/net/wwan/t7xx/t7xx_pci.h
+++ drivers/net/wwan/t7xx/t7xx_pci.h
@@ -21,6 +21,7 @@
 #include <linux/irqreturn.h>
 #include <linux/mutex.h>
 #include <linux/pci.h>
+#include <linux/spinlock.h>
 #include <linux/types.h>
 
 #include "t7xx_reg.h"
@@ -50,7 +51,10 @@ typedef irqreturn_t (*t7xx_intr_callback)(int irq, void *param);
  * @base_addr: memory base addresses of HW components
  * @md: modem interface
  * @md_pm_entities: list of pm entities
+ * @md_pm_lock: protects PCIe sleep lock
  * @md_pm_entity_mtx: protects md_pm_entities list
+ * @sleep_disable_count: PCIe L1.2 lock counter
+ * @sleep_lock_acquire: indicates that sleep has been disabled
  * @pm_sr_ack: ack from the device when went to sleep or woke up
  * @md_pm_state: state for resume/suspend
  * @ccmni_ctlb: context structure used to control the network data path
@@ -67,7 +71,10 @@ struct t7xx_pci_dev {
 
 	/* Low Power Items */
 	struct list_head	md_pm_entities;
+	spinlock_t		md_pm_lock;		/* Protects PCI resource lock */
 	struct mutex		md_pm_entity_mtx;	/* Protects MD PM entities list */
+	atomic_t		sleep_disable_count;
+	struct completion	sleep_lock_acquire;
 	struct completion	pm_sr_ack;
 	atomic_t		md_pm_state;
 
@@ -104,6 +111,9 @@ struct md_pm_entity {
 	void			*entity_param;
 };
 
+void t7xx_pci_disable_sleep(struct t7xx_pci_dev *t7xx_dev);
+void t7xx_pci_enable_sleep(struct t7xx_pci_dev *t7xx_dev);
+int t7xx_pci_sleep_disable_complete(struct t7xx_pci_dev *t7xx_dev);
 int t7xx_pci_pm_entity_register(struct t7xx_pci_dev *t7xx_dev, struct md_pm_entity *pm_entity);
 int t7xx_pci_pm_entity_unregister(struct t7xx_pci_dev *t7xx_dev, struct md_pm_entity *pm_entity);
 void t7xx_pci_pm_init_late(struct t7xx_pci_dev *t7xx_dev);
