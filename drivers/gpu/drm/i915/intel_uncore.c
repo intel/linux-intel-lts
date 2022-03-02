349f674fa4035faa7e87ad015e94eef34f12ce4b
Zawawi, Muhammad Zul Husni <muhammad.zul.husni.zawawi@intel.com>
2022-03-02 17:48:59 +0800

    Revert "drm/i915: Flush TLBs before releasing backing store"
    Seeing GPU hang on VM side with "[drm] ERROR rcs0 TLB invalidation
    did not complete in 4ms!". After bisecting the kernel patches, it
    seems this "drm/i915: Flush TLBs before releasing backing store"
    caused the issue. Also, this issue is not seen on kernel 5.10
    without this commit.
    
    Error Log:
    
    [ 134.882596] i915 0000:00:02.0: [drm] GPU HANG: ecode 12:0:00000000
    [ 134.883404] i915 0000:00:02.0: [drm] Resetting chip for stopped heartbeat on rcs0
    [ 134.885008] i915 0000:00:02.0: GuC interface version 0.1.0.0
    [ 134.886682] i915 0000:00:02.0: GuC firmware PRELOADED version 1.0 submission:SR-IOV VF
    [ 134.887319] i915 0000:00:02.0: HuC firmware PRELOADED
    [ 135.176718] i915 0000:00:02.0: [drm] ERROR rcs0 TLB invalidation did not complete in 4ms!
    [ 135.182675] i915 0000:00:02.0: [drm] ERROR bcs0 TLB invalidation did not complete in 4ms!
    [ 135.188662] i915 0000:00:02.0: [drm] ERROR vcs0 TLB invalidation did not complete in 4ms!
    [ 135.194650] i915 0000:00:02.0: [drm] ERROR vcs1 TLB invalidation did not complete in 4ms!
    [ 135.200666] i915 0000:00:02.0: [drm] ERROR vecs0 TLB invalidation did not complete in 4ms!
    [ 135.206474] i915 0000:00:02.0: [drm] ERROR rcs0 TLB invalidation did not complete in 4ms!
    
    This reverts commit 8a17a077e7e9ecce25c95dbdb27843d2d6c2f0f7
    
    Signed-off-by: Zawawi, Muhammad Zul Husni <muhammad.zul.husni.zawawi@intel.com>

--- drivers/gpu/drm/i915/intel_uncore.c
+++ drivers/gpu/drm/i915/intel_uncore.c
@@ -724,8 +724,7 @@ void intel_uncore_forcewake_get__locked(struct intel_uncore *uncore,
 }
 
 static void __intel_uncore_forcewake_put(struct intel_uncore *uncore,
-					 enum forcewake_domains fw_domains,
-					 bool delayed)
+					 enum forcewake_domains fw_domains)
 {
 	struct intel_uncore_forcewake_domain *domain;
 	unsigned int tmp;
@@ -740,11 +739,7 @@ static void __intel_uncore_forcewake_put(struct intel_uncore *uncore,
 			continue;
 		}
 
-		if (delayed &&
-		    !(domain->uncore->fw_domains_timer & domain->mask))
-			fw_domain_arm_timer(domain);
-		else
-			fw_domains_put(uncore, domain->mask);
+		fw_domains_put(uncore, domain->mask);
 	}
 }
 
@@ -765,20 +760,7 @@ void intel_uncore_forcewake_put(struct intel_uncore *uncore,
 		return;
 
 	spin_lock_irqsave(&uncore->lock, irqflags);
-	__intel_uncore_forcewake_put(uncore, fw_domains, false);
-	spin_unlock_irqrestore(&uncore->lock, irqflags);
-}
-
-void intel_uncore_forcewake_put_delayed(struct intel_uncore *uncore,
-					enum forcewake_domains fw_domains)
-{
-	unsigned long irqflags;
-
-	if (!uncore->fw_get_funcs)
-		return;
-
-	spin_lock_irqsave(&uncore->lock, irqflags);
-	__intel_uncore_forcewake_put(uncore, fw_domains, true);
+	__intel_uncore_forcewake_put(uncore, fw_domains);
 	spin_unlock_irqrestore(&uncore->lock, irqflags);
 }
 
@@ -820,7 +802,7 @@ void intel_uncore_forcewake_put__locked(struct intel_uncore *uncore,
 	if (!uncore->fw_get_funcs)
 		return;
 
-	__intel_uncore_forcewake_put(uncore, fw_domains, false);
+	__intel_uncore_forcewake_put(uncore, fw_domains);
 }
 
 void assert_forcewakes_inactive(struct intel_uncore *uncore)
