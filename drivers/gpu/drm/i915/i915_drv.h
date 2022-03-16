ced57b683c40111dc81d8881ad7aaca08645b4e7
Jani Nikula <jani.nikula@intel.com>
2022-03-16 11:50:18 +0200

    drm/i915: move i915_gem_object_needs_bit17_swizzle() to i915_gem_tiling.[ch]
    Move i915_gem_object_needs_bit17_swizzle() to i915_gem_tiling.[ch] as a
    i915_gem_object function related to tiling. Also un-inline while at it;
    does not seem like this is a function needed in hot paths.
    
    v2: i915_gem_tiling.[ch] instead of intel_ggtt_fencing.[ch] (Chris)
    
    Cc: Tvrtko Ursulin <tvrtko.ursulin@linux.intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220316095018.137998-1-jani.nikula@intel.com
    (cherry picked from commit 0438fd1aa69cb97dffa4c4285b1a330e46ef8832)

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1533,15 +1533,6 @@ i915_gem_context_lookup(struct drm_i915_file_private *file_priv, u32 id)
 	return ctx;
 }
 
-/* i915_gem_tiling.c */
-static inline bool i915_gem_object_needs_bit17_swizzle(struct drm_i915_gem_object *obj)
-{
-	struct drm_i915_private *i915 = to_i915(obj->base.dev);
-
-	return to_gt(i915)->ggtt->bit_6_swizzle_x == I915_BIT_6_SWIZZLE_9_10_17 &&
-		i915_gem_object_is_tiled(obj);
-}
-
 /* intel_device_info.c */
 static inline struct intel_device_info *
 mkwrite_device_info(struct drm_i915_private *dev_priv)
