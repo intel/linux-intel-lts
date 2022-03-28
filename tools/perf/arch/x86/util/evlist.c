9b6b95f2c6759adfcb1285b24881bd560506513d
Zhengjun Xing <zhengjun.xing@linux.intel.com>
2022-03-29 00:20:05 +0800

    perf stat: Add topdown metrics in default ‘perf stat’ on the hybrid machine
    Topdwon metrics is missed in default ‘perf stat’ on the hybrid machine,
    add topdown metrics in default ‘perf stat’for hybrid systems.
    
    In the default perf stat only support topdown events on the cpu_core.
    For the cpu_atom, we should not enable all topdown events as default
    perf stat because of the multiplexing for now. Widen the size of the
    event name column by 7 chars, so that all metrics after the # become
    aligned again.
    
    Perf should not trigger any warning or multiplexing for the default
    of perf stat. So the warning message will not print for the default
    of the perf stat.
    
    Before:
     # perf stat     /bin/true
    
     Performance counter stats for '/bin/true':
    
                  0.51 msec task-clock                #    0.455 CPUs utilized
                     0      context-switches          #    0.000 /sec
                     0      cpu-migrations            #    0.000 /sec
                    43      page-faults               #   83.680 K/sec
               691,740      cpu_core/cycles/          #    1.346 G/sec
         <not counted>      cpu_atom/cycles/                                              (0.00%)
               723,832      cpu_core/instructions/    #    1.409 G/sec
         <not counted>      cpu_atom/instructions/                                        (0.00%)
               144,017      cpu_core/branches/        #  280.262 M/sec
         <not counted>      cpu_atom/branches/                                            (0.00%)
                 4,617      cpu_core/branch-misses/   #    8.985 M/sec
         <not counted>      cpu_atom/branch-misses/                                       (0.00%)
    
           0.001129270 seconds time elapsed
    
           0.001190000 seconds user
           0.000000000 seconds sys
    
    After:
     # ./perf stat     /bin/true
    
     Performance counter stats for 'system wide':
    
                 31.77 msec cpu-clock                        #   24.396 CPUs utilized
                    72      context-switches                 #    2.266 K/sec
                    24      cpu-migrations                   #  755.340 /sec
                     1      page-faults                      #   31.472 /sec
             1,567,769      cpu_core/cycles/                 #   49.342 M/sec
               859,364      cpu_core/instructions/           #   27.046 M/sec
               170,712      cpu_core/branches/               #    5.373 M/sec
                 6,424      cpu_core/branch-misses/          #  202.179 K/sec
             7,305,978      cpu_core/slots/                  #  229.937 M/sec
             1,075,531      cpu_core/topdown-retiring/       #     14.7% retiring
               513,565      cpu_core/topdown-bad-spec/       #      7.0% bad speculation
             3,231,017      cpu_core/topdown-fe-bound/       #     44.2% frontend bound
             2,486,414      cpu_core/topdown-be-bound/       #     34.0% backend bound
               294,774      cpu_core/topdown-heavy-ops/      #      4.0% heavy operations       #     10.7% light operations
               497,022      cpu_core/topdown-br-mispredict/  #      6.8% branch mispredict      #      0.2% machine clears
             2,429,919      cpu_core/topdown-fetch-lat/      #     33.3% fetch latency          #     11.0% fetch bandwidth
             1,180,796      cpu_core/topdown-mem-bound/      #     16.2% memory bound           #     17.9% Core bound
    
           0.001302422 seconds time elapsed
    
           0.000000000 seconds user
           0.000000000 seconds sys
    
    Signed-off-by: Zhengjun Xing <zhengjun.xing@linux.intel.com>

--- tools/perf/arch/x86/util/evlist.c
+++ tools/perf/arch/x86/util/evlist.c
@@ -5,15 +5,35 @@
 #include "util/parse-events.h"
 
 #define TOPDOWN_L1_EVENTS	"{slots,topdown-retiring,topdown-bad-spec,topdown-fe-bound,topdown-be-bound}"
+#define TOPDOWN_L1_EVENTS_ATOM  "{cpu_atom/topdown-retiring/,cpu_atom/topdown-bad-spec/,cpu_atom/topdown-fe-bound/,cpu_atom/topdown-be-bound/}"
+#define TOPDOWN_L1_EVENTS_CORE  "{slots,cpu_core/topdown-retiring/,cpu_core/topdown-bad-spec/,cpu_core/topdown-fe-bound/,cpu_core/topdown-be-bound/}"
 #define TOPDOWN_L2_EVENTS	"{slots,topdown-retiring,topdown-bad-spec,topdown-fe-bound,topdown-be-bound,topdown-heavy-ops,topdown-br-mispredict,topdown-fetch-lat,topdown-mem-bound}"
+#define TOPDOWN_L2_EVENTS_ATOM  "{cpu_atom/topdown-retiring/,cpu_atom/topdown-bad-spec/,cpu_atom/topdown-fe-bound/,cpu_atom/topdown-be-bound/,cpu_atom/topdown-heavy-ops/,cpu_atom/topdown-br-mispredict/,cpu_atom/topdown-fetch-lat/,cpu_atom/topdown-mem-bound/}"
+#define TOPDOWN_L2_EVENTS_CORE  "{slots,cpu_core/topdown-retiring/,cpu_core/topdown-bad-spec/,cpu_core/topdown-fe-bound/,cpu_core/topdown-be-bound/,cpu_core/topdown-heavy-ops/,cpu_core/topdown-br-mispredict/,cpu_core/topdown-fetch-lat/,cpu_core/topdown-mem-bound/}"
 
-int arch_evlist__add_default_attrs(struct evlist *evlist)
+int arch_evlist__add_default_attrs(struct evlist *evlist, const char *pmu_name)
 {
-	if (!pmu_have_event("cpu", "slots"))
-		return 0;
+	const char *topdown_events;
 
-	if (pmu_have_event("cpu", "topdown-heavy-ops"))
-		return parse_events(evlist, TOPDOWN_L2_EVENTS, NULL);
-	else
-		return parse_events(evlist, TOPDOWN_L1_EVENTS, NULL);
+	if (!pmu_name)
+		return 0;
+	if (strcmp(pmu_name, "cpu_atom") && !pmu_have_event(pmu_name, "slots"))
+		return 0;
+	if (pmu_have_event(pmu_name, "topdown-heavy-ops")) {
+		if (!strcmp(pmu_name, "cpu_core"))
+			topdown_events = TOPDOWN_L2_EVENTS_CORE;
+		else if (!strcmp(pmu_name, "cpu_atom"))
+			topdown_events = TOPDOWN_L2_EVENTS_ATOM;
+		else
+			topdown_events = TOPDOWN_L2_EVENTS;
+	} else {
+		if (!strcmp(pmu_name, "cpu_core"))
+			topdown_events = TOPDOWN_L1_EVENTS_CORE;
+		else if (!strcmp(pmu_name, "cpu_atom"))
+			topdown_events = TOPDOWN_L1_EVENTS_ATOM;
+		else
+			topdown_events = TOPDOWN_L1_EVENTS;
+	}
+
+	return parse_events(evlist, topdown_events, NULL);
 }
