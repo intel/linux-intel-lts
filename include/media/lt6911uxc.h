f182645bc890ff3103b9231c5b8b51a2c38231f4
zouxiaoh <xiaohong.zou@intel.com>
2022-04-12 14:33:37 +0800

    media: lt6911uxc: define device name
    Change Description:
    define sensor name
    
    Signed-off-by: Ng, Khai Wen <khai.wen.ng@intel.com>
    Signed-off-by: Chen Meng J <meng.j.chen@intel.com>
    Signed-off-by: zouxiaoh <xiaohong.zou@intel.com>

--- include/media/lt6911uxc.h
+++ include/media/lt6911uxc.h
@@ -1,10 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-/* Copyright (C) 2014 - 2021 Intel Corporation */
+/* Copyright (C) 2014 - 2022 Intel Corporation */
 
 #ifndef __LT6911UXC_H
 #define __LT6911UXC_H
 
 #include <linux/types.h>
+
+#define LT6911UXC_NAME             "lt6911uxc"
 
 struct lt6911uxc_platform_data {
 	unsigned int port;
