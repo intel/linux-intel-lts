12431117ac4a0e6f0ff4039316d1d2fdfc17521e
Tomas Winkler <tomas.winkler@intel.com>
2022-04-19 12:33:13 -0700

    drm/i915/dg2: add gsc with special gsc bar offsets
    DG2 uses different GSC offsets on memory bar
    and uses PXP head (HECI1).
    
    v2 (Daniele): Rebased to before the ATS patches
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Reviewed-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com> #v1
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Reviewed-by: John Harrison <John.C.Harrison@Intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220419193314.526966-7-daniele.ceraolospurio@intel.com
    (cherry picked from commit f15856d7de914595d0daa2c706f53a693b48e228)
    [ Fix conflict due to missing 'needs_compact_pt']

--- drivers/gpu/drm/i915/i915_pci.c
+++ drivers/gpu/drm/i915/i915_pci.c
@@ -1049,6 +1049,7 @@ static const struct intel_device_info xehpsdv_info = {
 	.has_4tile = 1, \
 	.has_64k_pages = 1, \
 	.has_guc_deprivilege = 1, \
+	.has_heci_pxp = 1, \
 	.platform_engine_mask = \
 		BIT(RCS0) | BIT(BCS0) | \
 		BIT(VECS0) | BIT(VECS1) | \
