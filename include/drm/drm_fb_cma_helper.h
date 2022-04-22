bf520773575b2af5ba757229b8aa365e32be1888
Lucas De Marchi <lucas.demarchi@intel.com>
2022-04-21 21:40:52 -0700

    Revert "drm: Add and export function drm_fb_cma_sync_non_coherent"
    This reverts commit b09069046d6b7df1deea02cc14dc893e5e96630a.
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- include/drm/drm_fb_cma_helper.h
+++ include/drm/drm_fb_cma_helper.h
@@ -4,7 +4,6 @@
 
 #include <linux/types.h>
 
-struct drm_device;
 struct drm_framebuffer;
 struct drm_plane_state;
 
@@ -14,10 +13,6 @@ struct drm_gem_cma_object *drm_fb_cma_get_gem_obj(struct drm_framebuffer *fb,
 dma_addr_t drm_fb_cma_get_gem_addr(struct drm_framebuffer *fb,
 				   struct drm_plane_state *state,
 				   unsigned int plane);
-
-void drm_fb_cma_sync_non_coherent(struct drm_device *drm,
-				  struct drm_plane_state *old_state,
-				  struct drm_plane_state *state);
 
 #endif
 
