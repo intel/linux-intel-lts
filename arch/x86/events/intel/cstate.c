851ba5676265ac9bd632d73d2bbb1d933d2020d7
Kan Liang <kan.liang@linux.intel.com>
2022-04-22 08:25:14 -0700

    perf/x86/cstate: Add Alder Lake N CPU support
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>

--- arch/x86/events/intel/cstate.c
+++ arch/x86/events/intel/cstate.c
@@ -680,6 +680,7 @@ static const struct x86_cpu_id intel_cstates_match[] __initconst = {
 	X86_MATCH_INTEL_FAM6_MODEL(ROCKETLAKE,		&icl_cstates),
 	X86_MATCH_INTEL_FAM6_MODEL(ALDERLAKE,		&adl_cstates),
 	X86_MATCH_INTEL_FAM6_MODEL(ALDERLAKE_L,		&adl_cstates),
+	X86_MATCH_INTEL_FAM6_MODEL(ALDERLAKE_N,         &adl_cstates),
 	{ },
 };
 MODULE_DEVICE_TABLE(x86cpu, intel_cstates_match);
