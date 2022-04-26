37d7bdb2ae1b44c35b4b4f2f60baf60dffdaa05a
Lucas De Marchi <lucas.demarchi@intel.com>
2022-04-25 23:53:31 -0700

    Revert "drm/vram-helper: Unexport drm_vram_helper_{alloc,release}_mm()"
    This reverts commit 9aa0267476eda3c77890dd939fb2579bffceb3ae.
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- drivers/gpu/drm/drm_gem_vram_helper.c
+++ drivers/gpu/drm/drm_gem_vram_helper.c
@@ -1012,8 +1012,9 @@ static void drm_vram_mm_cleanup(struct drm_vram_mm *vmm)
  * Helpers for integration with struct drm_device
  */
 
-static struct drm_vram_mm *drm_vram_helper_alloc_mm(struct drm_device *dev, uint64_t vram_base,
-						    size_t vram_size)
+/* deprecated; use drmm_vram_mm_init() */
+struct drm_vram_mm *drm_vram_helper_alloc_mm(
+	struct drm_device *dev, uint64_t vram_base, size_t vram_size)
 {
 	int ret;
 
@@ -1035,8 +1036,9 @@ err_kfree:
 	dev->vram_mm = NULL;
 	return ERR_PTR(ret);
 }
+EXPORT_SYMBOL(drm_vram_helper_alloc_mm);
 
-static void drm_vram_helper_release_mm(struct drm_device *dev)
+void drm_vram_helper_release_mm(struct drm_device *dev)
 {
 	if (!dev->vram_mm)
 		return;
@@ -1045,6 +1047,7 @@ static void drm_vram_helper_release_mm(struct drm_device *dev)
 	kfree(dev->vram_mm);
 	dev->vram_mm = NULL;
 }
+EXPORT_SYMBOL(drm_vram_helper_release_mm);
 
 static void drm_vram_mm_release(struct drm_device *dev, void *ptr)
 {
