925c760e0ad999f23ce90ea78f01a9b9ab5056c0
Lucas De Marchi <lucas.demarchi@intel.com>
2022-04-26 10:35:39 -0700

    Revert "drm/simple-helper: drm_gem_simple_display_pipe_prepare_fb as default"
    This reverts commit 40cfc7fce4d00f7c2152b28a1f6e7fcbb1a47ca6.
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- include/drm/drm_simple_kms_helper.h
+++ include/drm/drm_simple_kms_helper.h
@@ -116,11 +116,8 @@ struct drm_simple_display_pipe_funcs {
 	 * the documentation for the &drm_plane_helper_funcs.prepare_fb hook for
 	 * more details.
 	 *
-	 * For GEM drivers who neither have a @prepare_fb nor @cleanup_fb hook
-	 * set drm_gem_simple_display_pipe_prepare_fb() is called automatically
-	 * to implement this. Other drivers which need additional plane
-	 * processing can call drm_gem_simple_display_pipe_prepare_fb() from
-	 * their @prepare_fb hook.
+	 * Drivers which always have their buffers pinned should use
+	 * drm_gem_simple_display_pipe_prepare_fb() for this hook.
 	 */
 	int (*prepare_fb)(struct drm_simple_display_pipe *pipe,
 			  struct drm_plane_state *plane_state);
