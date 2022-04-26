9cd8164e0b8e5b7cfe2cd43cdbe37fb0b5d23764
Lucas De Marchi <lucas.demarchi@intel.com>
2022-04-26 11:44:21 -0700

    Revert "drm/i915/gt: Pipelined clear"
    This reverts commit 563baae1875cbcac332086cca325cf55a0532b9e.
    
    [ Conflicts:
    	drivers/gpu/drm/i915/gt/intel_migrate.c
    
    	Fix conflict due to non-reverted commit:
    	7c517f83fa8c ("drm/i915/gt: finish INTEL_GEN and friends conversion")
    
    	And build breakage due to removing linux/types.h include ]
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- drivers/gpu/drm/i915/gt/intel_migrate.h
+++ drivers/gpu/drm/i915/gt/intel_migrate.h
@@ -42,24 +42,6 @@ int intel_context_migrate_copy(struct intel_context *ce,
 			       bool dst_is_lmem,
 			       struct i915_request **out);
 
-int
-intel_migrate_clear(struct intel_migrate *m,
-		    struct i915_gem_ww_ctx *ww,
-		    struct dma_fence *await,
-		    struct scatterlist *sg,
-		    enum i915_cache_level cache_level,
-		    bool is_lmem,
-		    u32 value,
-		    struct i915_request **out);
-int
-intel_context_migrate_clear(struct intel_context *ce,
-			    struct dma_fence *await,
-			    struct scatterlist *sg,
-			    enum i915_cache_level cache_level,
-			    bool is_lmem,
-			    u32 value,
-			    struct i915_request **out);
-
 void intel_migrate_fini(struct intel_migrate *m);
 
 #endif /* __INTEL_MIGRATE__ */
