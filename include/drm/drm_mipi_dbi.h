69f7c18c7273e166cdaea61727c9558bb331a39f
Lucas De Marchi <lucas.demarchi@intel.com>
2022-04-25 23:53:35 -0700

    Revert "drm/dbi: Print errors for mipi_dbi_command()"
    This reverts commit 3f5aa5ac0b0f9704f0c60f5fbbbcdc8c043d6eb6.
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- include/drm/drm_mipi_dbi.h
+++ include/drm/drm_mipi_dbi.h
@@ -183,12 +183,7 @@ int mipi_dbi_buf_copy(void *dst, struct drm_framebuffer *fb,
 #define mipi_dbi_command(dbi, cmd, seq...) \
 ({ \
 	const u8 d[] = { seq }; \
-	struct device *dev = &(dbi)->spi->dev;	\
-	int ret; \
-	ret = mipi_dbi_command_stackbuf(dbi, cmd, d, ARRAY_SIZE(d)); \
-	if (ret) \
-		dev_err_ratelimited(dev, "error %d when sending command %#02x\n", ret, cmd); \
-	ret; \
+	mipi_dbi_command_stackbuf(dbi, cmd, d, ARRAY_SIZE(d)); \
 })
 
 #ifdef CONFIG_DEBUG_FS
