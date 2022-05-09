46f0eac9e45ffdebd85174077b6243a69aa1e1ec
Takashi Iwai <tiwai@suse.de>
2022-05-09 09:14:25 +0800

    drm: drm_file struct kABI compatibility workaround
    The recent fix for DRM core
      patches.suse/drm-serialize-drm_file.master-with-a-new-spinlock.patch
    introduced a new field in struct drm_file.
    Apply the standard workaround to move it at the tail with __GENKSYMS__
    for keeping the kABI compatibility; drm_file isn't embedded, so it
    should be safe to do that.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/drm/drm_file.h
+++ include/drm/drm_file.h
@@ -244,9 +244,6 @@ struct drm_file {
 	 */
 	struct drm_master *master;
 
-	/** @master_lock: Serializes @master. */
-	spinlock_t master_lookup_lock;
-
 	/** @pid: Process that opened this file. */
 	struct pid *pid;
 
@@ -361,6 +358,11 @@ struct drm_file {
 	/* private: */
 #if IS_ENABLED(CONFIG_DRM_LEGACY)
 	unsigned long lock_count; /* DRI1 legacy lock count */
+#endif
+
+#ifndef __GENKSYMS__
+	/** @master_lock: Serializes @master. */
+	spinlock_t master_lookup_lock;
 #endif
 };
 
