b87b3cde32a4f45fa7ff33c587e2767481da1c89
Philippe Gerum <rpm@xenomai.org>
2022-07-22 15:45:31 +0200

    net: dovetail: move oob flags out of skb header
    The 'oob' bits should not be part of the header because we do not want
    them to be copied from the source skb during a clone operation. Manage
    these bits outside of the skb header section, making sure to (re)set
    them appropriately when required.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -790,6 +790,12 @@ struct sk_buff {
 #ifdef CONFIG_SKB_EXTENSIONS
 	__u8			active_extensions;
 #endif
+#ifdef CONFIG_NET_OOB
+	__u8			oob:1;
+	__u8			oob_clone:1;
+	__u8			oob_cloned:1;
+#endif
+
 	/* fields enclosed in headers_start/headers_end are copied
 	 * using a single memcpy() in __copy_skb_header()
 	 */
@@ -861,11 +867,6 @@ struct sk_buff {
 	__u8			decrypted:1;
 #endif
 	__u8			scm_io_uring:1;
-#ifdef CONFIG_NET_OOB
-	__u8			oob:1;
-	__u8			oob_clone:1;
-	__u8			oob_cloned:1;
-#endif
 
 #ifdef CONFIG_NET_SCHED
 	__u16			tc_index;	/* traffic control index */
