a1bfa464bebf45ea9d93104dac01304ce7b922d3
Hao Li <hao3.li@intel.com>
2022-08-05 12:46:05 +0800

    Revert "f2fs: fix to do sanity check on inode type during garbage collection"
    This reverts commit f00305815472c4d15b17191b95a4b677665c2c22.

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -1434,8 +1434,7 @@ next_step:
 
 		if (phase == 3) {
 			inode = f2fs_iget(sb, dni.ino);
-			if (IS_ERR(inode) || is_bad_inode(inode)
-			  || special_file(inode->i_mode)) {
+			if (IS_ERR(inode) || is_bad_inode(inode)) {
 				set_sbi_flag(sbi, SBI_NEED_FSCK);
 				continue;
 			}
