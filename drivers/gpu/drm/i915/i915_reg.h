52c87beeaaaff3bcea2367affee41036d070f011
Ranjan Dutta <ranjan.dutta@intel.com>
2022-09-06 07:43:13 +0800

    Revert "drm/i915/dg1: Update DMC_DEBUG3 register"
    This reverts commit a3a85c045aa014004f62057f22e558c29938f766.

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -7546,8 +7546,7 @@ enum {
 #define TGL_DMC_DEBUG_DC5_COUNT	_MMIO(0x101084)
 #define TGL_DMC_DEBUG_DC6_COUNT	_MMIO(0x101088)
 
-#define TGL_DMC_DEBUG3		_MMIO(0x101090)
-#define DG1_DMC_DEBUG3		_MMIO(0x13415c)
+#define DMC_DEBUG3		_MMIO(0x101090)
 
 /* Display Internal Timeout Register */
 #define RM_TIMEOUT		_MMIO(0x42060)
