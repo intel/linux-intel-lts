9eef094cfc9e7446527b283de3606bd8929aab80
Ranjan Dutta <ranjan.dutta@intel.com>
2022-09-06 07:43:31 +0800

    Revert "drm/bridge: tc358767: Move (e)DP bridge endpoint parsing into dedicated function"
    This reverts commit c038b9b7338924e5eb8d4b34d1a76691529feef8.

--- drivers/gpu/drm/bridge/tc358767.c
+++ drivers/gpu/drm/bridge/tc358767.c
@@ -1535,11 +1535,18 @@ static irqreturn_t tc_irq_handler(int irq, void *arg)
 	return IRQ_HANDLED;
 }
 
-static int tc_probe_edp_bridge_endpoint(struct tc_data *tc)
+static int tc_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
-	struct device *dev = tc->dev;
+	struct device *dev = &client->dev;
 	struct drm_panel *panel;
+	struct tc_data *tc;
 	int ret;
+
+	tc = devm_kzalloc(dev, sizeof(*tc), GFP_KERNEL);
+	if (!tc)
+		return -ENOMEM;
+
+	tc->dev = dev;
 
 	/* port@2 is the output port */
 	ret = drm_of_find_panel_or_bridge(dev->of_node, 2, 0, &panel, NULL);
@@ -1558,25 +1565,6 @@ static int tc_probe_edp_bridge_endpoint(struct tc_data *tc)
 	} else {
 		tc->bridge.type = DRM_MODE_CONNECTOR_DisplayPort;
 	}
-
-	return ret;
-}
-
-static int tc_probe(struct i2c_client *client, const struct i2c_device_id *id)
-{
-	struct device *dev = &client->dev;
-	struct tc_data *tc;
-	int ret;
-
-	tc = devm_kzalloc(dev, sizeof(*tc), GFP_KERNEL);
-	if (!tc)
-		return -ENOMEM;
-
-	tc->dev = dev;
-
-	ret = tc_probe_edp_bridge_endpoint(tc);
-	if (ret)
-		return ret;
 
 	/* Shut down GPIO is optional */
 	tc->sd_gpio = devm_gpiod_get_optional(dev, "shutdown", GPIOD_OUT_HIGH);
