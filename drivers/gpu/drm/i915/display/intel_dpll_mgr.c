3ae5f52c956bdc0dec4ab0ffaa3f882c93e56db3
Ville Syrjälä <ville.syrjala@linux.intel.com>
2022-09-21 15:23:40 +0300

    drm/i915: Always initialize dpll.lock
    Initialize the dll.lock mutex whether or not we manage to
    initialize the rest of the dpll mgr.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220921122343.13061-2-ville.syrjala@linux.intel.com
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/display/intel_dpll_mgr.c
+++ drivers/gpu/drm/i915/display/intel_dpll_mgr.c
@@ -4188,6 +4188,8 @@ void intel_shared_dpll_init(struct drm_i915_private *dev_priv)
 	const struct dpll_info *dpll_info;
 	int i;
 
+	mutex_init(&dev_priv->display.dpll.lock);
+
 	if (IS_DG2(dev_priv))
 		/* No shared DPLLs on DG2; port PLLs are part of the PHY */
 		dpll_mgr = NULL;
@@ -4232,7 +4234,6 @@ void intel_shared_dpll_init(struct drm_i915_private *dev_priv)
 
 	dev_priv->display.dpll.mgr = dpll_mgr;
 	dev_priv->display.dpll.num_shared_dpll = i;
-	mutex_init(&dev_priv->display.dpll.lock);
 }
 
 /**
