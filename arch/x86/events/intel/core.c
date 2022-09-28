8544f1970dd38a9e330d80030575e79801919003
Kan Liang <kan.liang@linux.intel.com>
2022-09-28 08:33:28 -0700

    perf/x86: Add new Raptor Lake S support
    From PMU's perspective, the new Raptor Lake S is the same as the other
    of hybrid {ALDER,RAPTOP}LAKE.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20220928153331.3757388-1-kan.liang@linux.intel.com

--- arch/x86/events/intel/core.c
+++ arch/x86/events/intel/core.c
@@ -6189,6 +6189,7 @@ __init int intel_pmu_init(void)
 	case INTEL_FAM6_ALDERLAKE_L:
 	case INTEL_FAM6_RAPTORLAKE:
 	case INTEL_FAM6_RAPTORLAKE_P:
+	case INTEL_FAM6_RAPTORLAKE_S:
 		/*
 		 * Alder Lake has 2 types of CPU, core and atom.
 		 *
