a9d4ced492cc8e7cdf45d9a1f1821b825eec183b
Christian König <christian.koenig@amd.com>
2022-09-28 13:21:05 +0200

    drm/amdgpu: drop the fence argument from amdgpu_vmid_grab
    This is always the job anyway.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Luben Tuikov <luben.tuikov@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20221014084641.128280-5-christian.koenig@amd.com

--- drivers/gpu/drm/amd/amdgpu/amdgpu_job.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_job.c
@@ -268,9 +268,7 @@ static struct dma_fence *amdgpu_job_dependency(struct drm_sched_job *sched_job,
 		fence = amdgpu_device_switch_gang(ring->adev, job->gang_submit);
 
 	while (fence == NULL && vm && !job->vmid) {
-		r = amdgpu_vmid_grab(vm, ring, &job->sync,
-				     &job->base.s_fence->finished,
-				     job);
+		r = amdgpu_vmid_grab(vm, ring, &job->sync, job);
 		if (r)
 			DRM_ERROR("Error getting VM ID (%d)\n", r);
 
