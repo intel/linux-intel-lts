aeb296d1f0a804d72b384b5c8a78bb4b26a6e47a
Gan Yi Fang <yi.fang.gan@intel.com>
2022-10-26 20:42:01 -0400

    net: stmmac: Add check for supported EEE advertisement
    Current implementation checks the supported linkmode from
    PHY when modifying EEE settings. In fact, the supported
    linkmode in EEE is different from the supported linkmode
    in PHY. This patch checks the supported linkmode in EEE
    instead of the supported linkmode from PHY.
    
    Signed-off-by: Gan Yi Fang <yi.fang.gan@intel.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
@@ -839,19 +839,17 @@ static int stmmac_ethtool_op_set_eee(struct net_device *dev,
 		stmmac_disable_eee_mode(priv);
 	} else {
 		__ETHTOOL_DECLARE_LINK_MODE_MASK(supported);
-
-		struct ethtool_link_ksettings link_ks = {};
+		__ETHTOOL_DECLARE_LINK_MODE_MASK(advertised);
 
 		ethtool_convert_legacy_u32_to_link_mode(supported,
 							edata->supported);
+		ethtool_convert_legacy_u32_to_link_mode(advertised,
+							edata->advertised);
 
-		/* Get the current phy link settings */
-		stmmac_ethtool_get_link_ksettings(dev, &link_ks);
-
-		/* Check if the request is supported */
-		if (!bitmap_subset(supported,
-				   link_ks.link_modes.supported,
-				   __ETHTOOL_LINK_MODE_MASK_NBITS)) {
+		/*Check if the advertise speed is supported.*/
+		if (!bitmap_subset(advertised,
+				   supported,
+				   __ETHTOOL_LINK_MODE_MASK_NBITS)){
 			return -EOPNOTSUPP;
 		}
 	}
