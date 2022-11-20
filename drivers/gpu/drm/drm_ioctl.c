3139f1c16011215c9c271544643b1f048f300c09
Ranjan Dutta <ranjan.dutta@intel.com>
2022-11-20 20:20:42 +0800

    Revert "drm: Use size_t type for len variable in drm_copy_field()"
    This reverts commit e7d701800365d2dd3ef2252497dcaed9a2d2fada.

--- drivers/gpu/drm/drm_ioctl.c
+++ drivers/gpu/drm/drm_ioctl.c
@@ -473,7 +473,7 @@ EXPORT_SYMBOL(drm_invalid_op);
  */
 static int drm_copy_field(char __user *buf, size_t *buf_len, const char *value)
 {
-	size_t len;
+	int len;
 
 	/* don't overflow userbuf */
 	len = strlen(value);
