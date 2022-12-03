4d81ec2c7fd7dc0b388080558f8be1e9e49370f6
Philippe Gerum <rpm@xenomai.org>
2022-12-03 11:34:35 +0100

    irq_pipeline: lockdep: fix preemption_disabled/enabled predicates
    We need to account for interrupt pipelining in these assertions.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/linux/lockdep.h
+++ include/linux/lockdep.h
@@ -646,7 +646,7 @@ do {									\
 	WARN_ON_ONCE(IS_ENABLED(CONFIG_PREEMPT_COUNT)	&&		\
 		     __lockdep_enabled			&&		\
 		     (preempt_count() != 0		||		\
-		      !this_cpu_read(hardirqs_enabled)));		\
+		     __lockdep_check_irqs_disabled()));			\
 } while (0)
 
 #define lockdep_assert_preemption_disabled()				\
@@ -654,7 +654,7 @@ do {									\
 	WARN_ON_ONCE(IS_ENABLED(CONFIG_PREEMPT_COUNT)	&&		\
 		     __lockdep_enabled			&&		\
 		     (preempt_count() == 0		&&		\
-		      this_cpu_read(hardirqs_enabled)));		\
+		     __lockdep_check_irqs_enabled()));			\
 } while (0)
 
 /*
