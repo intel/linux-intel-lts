fb75e2871baf721d281f902dfa5a751f2240e518
Song Yoong Siang <yoong.siang.song@intel.com>
2022-12-08 22:13:31 +0800

    net: stmmac: Fix 'no previous prototype' build warning
    This commit fixes following build warning:
    
    drivers/net/ethernet/stmicro/stmmac/dwmac4_core.c:79:6: warning: no
    previous prototype for ‘dwmac4_mac_start_tx’ [-Wmissing-prototypes]
       79 | void dwmac4_mac_start_tx(void __iomem *ioaddr)
          |      ^~~~~~~~~~~~~~~~~~~
    drivers/net/ethernet/stmicro/stmmac/dwmac4_core.c:88:6: warning: no
    previous prototype for ‘dwmac4_mac_stop_tx’ [-Wmissing-prototypes]
       88 | void dwmac4_mac_stop_tx(void __iomem *ioaddr)
          |      ^~~~~~~~~~~~~~~~~~
    drivers/net/ethernet/stmicro/stmmac/dwmac4_core.c:97:6: warning: no
    previous prototype for ‘dwmac4_mac_start_rx’ [-Wmissing-prototypes]
       97 | void dwmac4_mac_start_rx(void __iomem *ioaddr)
          |      ^~~~~~~~~~~~~~~~~~~
    drivers/net/ethernet/stmicro/stmmac/dwmac4_core.c:106:6: warning: no
    previous prototype for ‘dwmac4_mac_stop_rx’ [-Wmissing-prototypes]
      106 | void dwmac4_mac_stop_rx(void __iomem *ioaddr)
          |      ^~~~~~~~~~~~~~~~~~
    
    Fixes: e211a5a7b594 ("net: stmmac: move TX/RX MAC state machine enablement to struct stmmac_ops")
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac4_core.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac4_core.c
@@ -81,7 +81,7 @@ static void dwmac4_core_init(struct mac_device_info *hw,
 		init_waitqueue_head(&priv->tstamp_busy_wait);
 }
 
-void dwmac4_mac_start_tx(void __iomem *ioaddr)
+static void dwmac4_mac_start_tx(void __iomem *ioaddr)
 {
 	u32 value;
 
@@ -90,7 +90,7 @@ void dwmac4_mac_start_tx(void __iomem *ioaddr)
 	writel(value, ioaddr + GMAC_CONFIG);
 }
 
-void dwmac4_mac_stop_tx(void __iomem *ioaddr)
+static void dwmac4_mac_stop_tx(void __iomem *ioaddr)
 {
 	u32 value;
 
@@ -99,7 +99,7 @@ void dwmac4_mac_stop_tx(void __iomem *ioaddr)
 	writel(value, ioaddr + GMAC_CONFIG);
 }
 
-void dwmac4_mac_start_rx(void __iomem *ioaddr)
+static void dwmac4_mac_start_rx(void __iomem *ioaddr)
 {
 	u32 value;
 
@@ -108,7 +108,7 @@ void dwmac4_mac_start_rx(void __iomem *ioaddr)
 	writel(value, ioaddr + GMAC_CONFIG);
 }
 
-void dwmac4_mac_stop_rx(void __iomem *ioaddr)
+static void dwmac4_mac_stop_rx(void __iomem *ioaddr)
 {
 	u32 value;
 
