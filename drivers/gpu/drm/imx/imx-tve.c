a7f700f96f97b5a58986893eefa3a5e09790b6f5
Ranjan Dutta <ranjan.dutta@intel.com>
2022-12-28 01:01:39 +0800

    Revert "drm/imx: imx-tve: Fix return type of imx_tve_connector_mode_valid"
    This reverts commit 16743c4bf3ef433cdd9babcd4d627b3387ad947d.

--- drivers/gpu/drm/imx/imx-tve.c
+++ drivers/gpu/drm/imx/imx-tve.c
@@ -212,9 +212,8 @@ static int imx_tve_connector_get_modes(struct drm_connector *connector)
 	return ret;
 }
 
-static enum drm_mode_status
-imx_tve_connector_mode_valid(struct drm_connector *connector,
-			     struct drm_display_mode *mode)
+static int imx_tve_connector_mode_valid(struct drm_connector *connector,
+					struct drm_display_mode *mode)
 {
 	struct imx_tve *tve = con_to_tve(connector);
 	unsigned long rate;
