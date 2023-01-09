bb35a46590627c3970175f4e9c87dbf686192522
Neil Armstrong <neil.armstrong@linaro.org>
2023-01-09 11:15:20 +0100

    drm/msm/dpu: add support for SM8550
    Add definitions for the display hardware used on Qualcomm SM8550
    platform.
    
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>
    Patchwork: https://patchwork.freedesktop.org/patch/517512/
    Link: https://lore.kernel.org/r/20230103-topic-sm8550-upstream-mdss-dsi-v3-4-660c3bcb127f@linaro.org
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
@@ -1310,6 +1310,7 @@ static const struct of_device_id dpu_dt_match[] = {
 	{ .compatible = "qcom,sm8250-dpu", },
 	{ .compatible = "qcom,sm8350-dpu", },
 	{ .compatible = "qcom,sm8450-dpu", },
+	{ .compatible = "qcom,sm8550-dpu", },
 	{}
 };
 MODULE_DEVICE_TABLE(of, dpu_dt_match);
