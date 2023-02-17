e39fe205f468d5b39baae17b3952eba0d7abb3fd
John Ogness <john.ogness@linutronix.de>
2023-02-17 09:53:44 +0106

    printk: ignore consoles without write() callback
    The ttynull driver does not provide an implementation for the write()
    callback. This leads to a NULL pointer dereference in the related
    printing kthread, which assumes it can call that callback.
    
    Do not create kthreads for consoles that do not implement the write()
    callback. Also, for pr_flush(), ignore consoles that do not implement
    write() or write_atomic(), since there is no way those consoles can
    flush their output.
    
    Link: https://lore.kernel.org/lkml/1831554214.546921.1676479103702.JavaMail.zimbra@hale.at
    Reported-by: Michael Thalmeier <michael.thalmeier@hale.at>
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: Luis Claudio R. Goncalves <lgoncalv@redhat.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -2267,6 +2267,10 @@ out:
 /* Must be called within console_lock(). */
 static void start_printk_kthread(struct console *con)
 {
+	/* No need to start a printing thread if the console cannot print. */
+	if (!con->write)
+		return;
+
 	con->thread = kthread_run(printk_kthread_func, con,
 				  "pr/%s%d", con->name, con->index);
 	if (IS_ERR(con->thread)) {
@@ -3565,6 +3569,8 @@ bool pr_flush(int timeout_ms, bool reset_on_progress)
 
 		for_each_console(con) {
 			if (!(con->flags & CON_ENABLED))
+				continue;
+			if (!con->write && !con->write_atomic)
 				continue;
 			printk_seq = atomic64_read(&con->printk_seq);
 			if (printk_seq < seq)
