a79264b3a14d5a8b9f81334feebbbd179a294f5d
Matt Roper <matthew.d.roper@intel.com>
2023-02-23 16:23:00 -0800

    drm/i915: Stop whitelisting CS_CTX_TIMESTAMP on Xe_HP platforms
    Xe_HP architecture already makes the CS_CTX_TIMESTAMP readable by
    userspace on all engines; there's no longer a need to add it to the
    software-managed whitelist for the non-RCS engines.
    
    Bspec: 45545
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    Acked-by: Gustavo Sousa <gustavo.sousa@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230224002300.3578985-2-matthew.d.roper@intel.com

--- drivers/gpu/drm/i915/gt/intel_workarounds.c
+++ drivers/gpu/drm/i915/gt/intel_workarounds.c
@@ -2204,16 +2204,9 @@ static void tgl_whitelist_build(struct intel_engine_cs *engine)
 	}
 }
 
-static void xehpsdv_whitelist_build(struct intel_engine_cs *engine)
-{
-	allow_read_ctx_timestamp(engine);
-}
-
 static void dg2_whitelist_build(struct intel_engine_cs *engine)
 {
 	struct i915_wa_list *w = &engine->whitelist;
-
-	allow_read_ctx_timestamp(engine);
 
 	switch (engine->class) {
 	case RENDER_CLASS:
@@ -2265,8 +2258,6 @@ static void blacklist_trtt(struct intel_engine_cs *engine)
 
 static void pvc_whitelist_build(struct intel_engine_cs *engine)
 {
-	allow_read_ctx_timestamp(engine);
-
 	/* Wa_16014440446:pvc */
 	blacklist_trtt(engine);
 }
@@ -2300,7 +2291,7 @@ void intel_engine_init_whitelist(struct intel_engine_cs *engine)
 	else if (IS_DG2(i915))
 		dg2_whitelist_build(engine);
 	else if (IS_XEHPSDV(i915))
-		xehpsdv_whitelist_build(engine);
+		; /* none needed */
 	else if (GRAPHICS_VER(i915) == 12)
 		tgl_whitelist_build(engine);
 	else if (GRAPHICS_VER(i915) == 11)
