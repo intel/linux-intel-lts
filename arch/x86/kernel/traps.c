a71c32138db7ef7055c361840d8e824215e238a0
Philippe Gerum <rpm@xenomai.org>
2023-03-04 18:55:26 +0100

    x86: dovetail: fix early exit path for traps handled oob
    Every call to oob_trap_notify() must be paired with a call to
    oob_trap_unwind() to close the trap handling section, even when the
    in-band side decides to leave it unhandled because the companion core
    did not switch in-band.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- arch/x86/kernel/traps.c
+++ arch/x86/kernel/traps.c
@@ -170,6 +170,13 @@ bool mark_trap_entry(int trapnr, struct pt_regs *regs)
 		return true;
 	}
 
+	/*
+	 * If the oob core did not switch us inband, our caller is
+	 * expected to leave the trap handler immediately, so we may
+	 * notify the core about this right now.
+	 */
+	oob_trap_unwind(trapnr, regs);
+
 	return false;
 }
 
@@ -184,7 +191,13 @@ static __always_inline
 bool mark_trap_entry_raw(int trapnr, struct pt_regs *regs)
 {
 	oob_trap_notify(trapnr, regs);
-	return running_inband();
+
+	if (running_oob()) {
+		oob_trap_unwind(trapnr, regs);
+		return false;
+	}
+
+	return true;
 }
 
 static __always_inline
