7becae7cdff87cebf08a3a07fa30ca209f6ce16b
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2023-03-15 14:31:33 +0300

    mm, treewide: redefine MAX_ORDER sanely
    MAX_ORDER currently defined as number of orders page allocator supports:
    user can ask buddy allocator for page order between 0 and MAX_ORDER-1.
    
    This definition is counter-intuitive and lead to number of bugs all over
    the kernel.
    
    Change the definition of MAX_ORDER to be inclusive: the range of orders
    user can ask from buddy allocator is 0..MAX_ORDER now.
    
    [kirill@shutemov.name: fix min() warning]
      Link: https://lkml.kernel.org/r/20230315153800.32wib3n5rickolvh@box
    [akpm@linux-foundation.org: fix another min_t warning]
    [kirill@shutemov.name: fixups per Zi Yan]
      Link: https://lkml.kernel.org/r/20230316232144.b7ic4cif4kjiabws@box.shutemov.name
    [akpm@linux-foundation.org: fix underlining in docs]
      Link: https://lore.kernel.org/oe-kbuild-all/202303191025.VRCTk6mP-lkp@intel.com/
    Link: https://lkml.kernel.org/r/20230315113133.11326-11-kirill.shutemov@linux.intel.com
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Reviewed-by: Michael Ellerman <mpe@ellerman.id.au>	[powerpc]
    Cc: "Kirill A. Shutemov" <kirill@shutemov.name>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/slab.c
+++ mm/slab.c
@@ -466,7 +466,7 @@ static int __init slab_max_order_setup(char *str)
 {
 	get_option(&str, &slab_max_order);
 	slab_max_order = slab_max_order < 0 ? 0 :
-				min(slab_max_order, MAX_ORDER - 1);
+				min(slab_max_order, MAX_ORDER);
 	slab_max_order_set = true;
 
 	return 1;
