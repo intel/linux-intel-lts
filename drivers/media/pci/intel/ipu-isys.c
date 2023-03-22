e5e4def4bfd3c95dc117c308adab9c9449421ee6
zouxiaoh <xiaohong.zou@intel.com>
2023-03-22 15:43:12 +0800

    media: intel: isys: Add debug log for get i2c adapter.
    Change Description:
    Add debug log for get i2c adapter.
    
    Signed-off-by: zouxiaoh <xiaohong.zou@intel.com>

--- drivers/media/pci/intel/ipu-isys.c
+++ drivers/media/pci/intel/ipu-isys.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
-// Copyright (C) 2013 - 2021 Intel Corporation
+// Copyright (C) 2013 - 2022 Intel Corporation
 
 #include <linux/debugfs.h>
 #include <linux/delay.h>
@@ -140,9 +140,16 @@ static int isys_register_ext_subdev(struct ipu_isys *isys,
 			sd_info->i2c.i2c_adapter_bdf,
 			sizeof(sd_info->i2c.i2c_adapter_bdf));
 	if (bus < 0) {
-		dev_err(&isys->adev->dev, "Failed to find adapter!");
+		dev_err(&isys->adev->dev,
+			"getting i2c bus id for adapter %d (bdf %s) failed",
+			sd_info->i2c.i2c_adapter_id,
+			sd_info->i2c.i2c_adapter_bdf);
 		return -ENOENT;
 	}
+	dev_info(&isys->adev->dev,
+		 "got i2c bus id %d for adapter %d (bdf %s)", bus,
+		 sd_info->i2c.i2c_adapter_id,
+		 sd_info->i2c.i2c_adapter_bdf);
 	adapter = i2c_get_adapter(bus);
 	if (!adapter) {
 		dev_warn(&isys->adev->dev, "can't find adapter\n");
