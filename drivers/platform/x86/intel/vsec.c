6c2e4475ff67a0a1758c7104e3b229a7579aaa78
Qiao, Wei <wei.qiao@intel.com>
2023-03-24 16:46:29 +0800

    Revert "platform/x86/intel/vsec: Enhance and Export intel_vsec_add_aux()"
    This reverts commit 9b1dce1dbbba1f4a10440d2c5064f876c6b1e033.

--- drivers/platform/x86/intel/vsec.c
+++ drivers/platform/x86/intel/vsec.c
@@ -137,9 +137,8 @@ static void intel_vsec_dev_release(struct device *dev)
 	kfree(intel_vsec_dev);
 }
 
-int intel_vsec_add_aux(struct pci_dev *pdev, struct device *parent,
-		       struct intel_vsec_device *intel_vsec_dev,
-		       const char *name)
+static int intel_vsec_add_aux(struct pci_dev *pdev, struct intel_vsec_device *intel_vsec_dev,
+			      const char *name)
 {
 	struct auxiliary_device *auxdev = &intel_vsec_dev->auxdev;
 	int ret, id;
@@ -150,12 +149,9 @@ int intel_vsec_add_aux(struct pci_dev *pdev, struct device *parent,
 		return ret;
 	}
 
-	if (!parent)
-		parent = &pdev->dev;
-
 	auxdev->id = ret;
 	auxdev->name = name;
-	auxdev->dev.parent = parent;
+	auxdev->dev.parent = &pdev->dev;
 	auxdev->dev.release = intel_vsec_dev_release;
 
 	ret = auxiliary_device_init(auxdev);
@@ -172,7 +168,7 @@ int intel_vsec_add_aux(struct pci_dev *pdev, struct device *parent,
 		return ret;
 	}
 
-	ret = devm_add_action_or_reset(parent, intel_vsec_remove_aux,
+	ret = devm_add_action_or_reset(&pdev->dev, intel_vsec_remove_aux,
 				       auxdev);
 	if (ret < 0)
 		return ret;
@@ -185,7 +181,6 @@ int intel_vsec_add_aux(struct pci_dev *pdev, struct device *parent,
 
 	return 0;
 }
-EXPORT_SYMBOL_NS_GPL(intel_vsec_add_aux, INTEL_VSEC);
 
 static int intel_vsec_add_dev(struct pci_dev *pdev, struct intel_vsec_header *header,
 			      struct intel_vsec_platform_info *info)
@@ -243,8 +238,7 @@ static int intel_vsec_add_dev(struct pci_dev *pdev, struct intel_vsec_header *he
 	else
 		intel_vsec_dev->ida = &intel_vsec_ida;
 
-	return intel_vsec_add_aux(pdev, NULL, intel_vsec_dev,
-				  intel_vsec_name(header->id));
+	return intel_vsec_add_aux(pdev, intel_vsec_dev, intel_vsec_name(header->id));
 }
 
 static bool intel_vsec_walk_header(struct pci_dev *pdev,
