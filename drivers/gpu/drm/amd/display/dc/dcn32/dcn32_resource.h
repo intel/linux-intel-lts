cedde5db57f2e95986cb28deac77a4fb02b07f5b
Pan, Kris <kris.pan@intel.com>
2023-03-28 00:54:03 +0000

    Revert "drm/amd/display: adjust MALL size available for DCN32 and DCN321"
    This reverts commit 38327b6cb1fa2bb098bca0db7dd25ee80a6d64b1.

--- drivers/gpu/drm/amd/display/dc/dcn32/dcn32_resource.h
+++ drivers/gpu/drm/amd/display/dc/dcn32/dcn32_resource.h
@@ -144,8 +144,6 @@ void dcn32_restore_mall_state(struct dc *dc,
 
 bool dcn32_allow_subvp_with_active_margin(struct pipe_ctx *pipe);
 
-unsigned int dcn32_calc_num_avail_chans_for_mall(struct dc *dc, int num_chans);
-
 /* definitions for run time init of reg offsets */
 
 /* CLK SRC */
