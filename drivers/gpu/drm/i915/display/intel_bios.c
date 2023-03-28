751f782cef4382eaf349f3ebcaa055f940859be9
Pan, Kris <kris.pan@intel.com>
2023-03-28 00:54:16 +0000

    Revert "drm/i915: Do panel VBT init early if the VBT declares an explicit panel type"
    This reverts commit bd61a84b5eee4eaf3daf1c69499a4eca1f972ca4.

--- drivers/gpu/drm/i915/display/intel_bios.c
+++ drivers/gpu/drm/i915/display/intel_bios.c
@@ -620,14 +620,14 @@ static void dump_pnp_id(struct drm_i915_private *i915,
 
 static int opregion_get_panel_type(struct drm_i915_private *i915,
 				   const struct intel_bios_encoder_data *devdata,
-				   const struct edid *edid, bool use_fallback)
+				   const struct edid *edid)
 {
 	return intel_opregion_get_panel_type(i915);
 }
 
 static int vbt_get_panel_type(struct drm_i915_private *i915,
 			      const struct intel_bios_encoder_data *devdata,
-			      const struct edid *edid, bool use_fallback)
+			      const struct edid *edid)
 {
 	const struct bdb_lvds_options *lvds_options;
 
@@ -652,7 +652,7 @@ static int vbt_get_panel_type(struct drm_i915_private *i915,
 
 static int pnpid_get_panel_type(struct drm_i915_private *i915,
 				const struct intel_bios_encoder_data *devdata,
-				const struct edid *edid, bool use_fallback)
+				const struct edid *edid)
 {
 	const struct bdb_lvds_lfp_data *data;
 	const struct bdb_lvds_lfp_data_ptrs *ptrs;
@@ -701,9 +701,9 @@ static int pnpid_get_panel_type(struct drm_i915_private *i915,
 
 static int fallback_get_panel_type(struct drm_i915_private *i915,
 				   const struct intel_bios_encoder_data *devdata,
-				   const struct edid *edid, bool use_fallback)
+				   const struct edid *edid)
 {
-	return use_fallback ? 0 : -1;
+	return 0;
 }
 
 enum panel_type {
@@ -715,13 +715,13 @@ enum panel_type {
 
 static int get_panel_type(struct drm_i915_private *i915,
 			  const struct intel_bios_encoder_data *devdata,
-			  const struct edid *edid, bool use_fallback)
+			  const struct edid *edid)
 {
 	struct {
 		const char *name;
 		int (*get_panel_type)(struct drm_i915_private *i915,
 				      const struct intel_bios_encoder_data *devdata,
-				      const struct edid *edid, bool use_fallback);
+				      const struct edid *edid);
 		int panel_type;
 	} panel_types[] = {
 		[PANEL_TYPE_OPREGION] = {
@@ -744,8 +744,7 @@ static int get_panel_type(struct drm_i915_private *i915,
 	int i;
 
 	for (i = 0; i < ARRAY_SIZE(panel_types); i++) {
-		panel_types[i].panel_type = panel_types[i].get_panel_type(i915, devdata,
-									  edid, use_fallback);
+		panel_types[i].panel_type = panel_types[i].get_panel_type(i915, devdata, edid);
 
 		drm_WARN_ON(&i915->drm, panel_types[i].panel_type > 0xf &&
 			    panel_types[i].panel_type != 0xff);
@@ -3192,26 +3191,14 @@ out:
 	kfree(oprom_vbt);
 }
 
-static void intel_bios_init_panel(struct drm_i915_private *i915,
-				  struct intel_panel *panel,
-				  const struct intel_bios_encoder_data *devdata,
-				  const struct edid *edid,
-				  bool use_fallback)
+void intel_bios_init_panel(struct drm_i915_private *i915,
+			   struct intel_panel *panel,
+			   const struct intel_bios_encoder_data *devdata,
+			   const struct edid *edid)
 {
-	/* already have it? */
-	if (panel->vbt.panel_type >= 0) {
-		drm_WARN_ON(&i915->drm, !use_fallback);
-		return;
-	}
-
-	panel->vbt.panel_type = get_panel_type(i915, devdata,
-					       edid, use_fallback);
-	if (panel->vbt.panel_type < 0) {
-		drm_WARN_ON(&i915->drm, use_fallback);
-		return;
-	}
-
 	init_vbt_panel_defaults(panel);
+
+	panel->vbt.panel_type = get_panel_type(i915, devdata, edid);
 
 	parse_panel_options(i915, panel);
 	parse_generic_dtd(i915, panel);
@@ -3224,21 +3211,6 @@ static void intel_bios_init_panel(struct drm_i915_private *i915,
 	parse_psr(i915, panel);
 	parse_mipi_config(i915, panel);
 	parse_mipi_sequence(i915, panel);
-}
-
-void intel_bios_init_panel_early(struct drm_i915_private *i915,
-				 struct intel_panel *panel,
-				 const struct intel_bios_encoder_data *devdata)
-{
-	intel_bios_init_panel(i915, panel, devdata, NULL, false);
-}
-
-void intel_bios_init_panel_late(struct drm_i915_private *i915,
-				struct intel_panel *panel,
-				const struct intel_bios_encoder_data *devdata,
-				const struct edid *edid)
-{
-	intel_bios_init_panel(i915, panel, devdata, edid, true);
 }
 
 /**
