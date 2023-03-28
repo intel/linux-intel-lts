af294bc3e9b845f003e746f961ebbe81477dc433
Pan, Kris <kris.pan@intel.com>
2023-03-28 00:55:14 +0000

    Revert "drm/vmwgfx: Do not drop the reference to the handle too soon"
    This reverts commit 0a127ac972404600c99eb141c8d5b5348e53ee4f.

--- drivers/gpu/drm/vmwgfx/vmwgfx_overlay.c
+++ drivers/gpu/drm/vmwgfx/vmwgfx_overlay.c
@@ -458,7 +458,6 @@ int vmw_overlay_ioctl(struct drm_device *dev, void *data,
 	ret = vmw_overlay_update_stream(dev_priv, buf, arg, true);
 
 	vmw_bo_unreference(&buf);
-	drm_gem_object_put(&buf->base.base);
 
 out_unlock:
 	mutex_unlock(&overlay->mutex);
