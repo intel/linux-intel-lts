3fec332c27689373c02b78fbc383acb5bfaced77
Pan, Kris <kris.pan@intel.com>
2023-03-28 00:55:06 +0000

    Revert "drm/vkms: Fix memory leak in vkms_init()"
    This reverts commit bebd60ec3bf21062f103e32e6203c6daabdbd51b.

--- drivers/gpu/drm/vkms/vkms_drv.c
+++ drivers/gpu/drm/vkms/vkms_drv.c
@@ -218,7 +218,6 @@ out_unregister:
 
 static int __init vkms_init(void)
 {
-	int ret;
 	struct vkms_config *config;
 
 	config = kmalloc(sizeof(*config), GFP_KERNEL);
@@ -231,11 +230,7 @@ static int __init vkms_init(void)
 	config->writeback = enable_writeback;
 	config->overlay = enable_overlay;
 
-	ret = vkms_create(config);
-	if (ret)
-		kfree(config);
-
-	return ret;
+	return vkms_create(config);
 }
 
 static void vkms_destroy(struct vkms_config *config)
