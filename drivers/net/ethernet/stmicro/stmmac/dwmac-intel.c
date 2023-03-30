080c773b88f705d1ccda4406deac03b38ceb5a44
Michael Sit Wei Hong <michael.wei.hong.sit@intel.com>
2023-03-30 17:14:04 +0800

    net: stmmac: remove redundant fixup to support fixed-link mode
    Currently, intel_speed_mode_2500() will fix-up xpcs_an_inband
    to 1 if the underlying controller has a max speed of 1000Mbps.
    The value has been initialized and modified if it is
    a fixed-linked setup earlier.
    
    This patch removes the fix-up to allow for fixed-linked setup
    support. In stmmac_phy_setup(), ovr_an_inband is set based on
    the value of xpcs_an_inband. Which in turn will return an
    error in phylink_parse_mode() where MLO_AN_FIXED and
    ovr_an_inband are both set.
    
    Fixes: c82386310d95 ("stmmac: intel: prepare to support 1000BASE-X phy interface setting")
    Signed-off-by: Michael Sit Wei Hong <michael.wei.hong.sit@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-intel.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-intel.c
@@ -253,7 +253,6 @@ static void intel_speed_mode_2500(struct net_device *ndev, void *intel_data)
 		priv->plat->fixed_2G5_clock_rate = true;
 	} else {
 		priv->plat->max_speed = 1000;
-		priv->plat->mdio_bus_data->xpcs_an_inband = true;
 		priv->plat->fixed_2G5_clock_rate = false;
 	}
 }
