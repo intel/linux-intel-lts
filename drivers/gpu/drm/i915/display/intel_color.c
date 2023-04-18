2e5622438f8c87a5e3dd58f5374d8f5b79320a00
Uma Shankar <uma.shankar@intel.com>
2023-04-18 18:38:22 +0530

    drm/i915/xelpd: Attach gamma mode property
    Attach the gamma mode property to allow userspace set the gamma mode
    and provide the luts for the same.
    
    v2: Rebase on XE_LPD
    
    v3: Updated the enum count.
    
    v4: Create the prop before adding the gamma_mode range
    
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>
    Signed-off-by: Bhanuprakash Modem <bhanuprakash.modem@intel.com>

--- drivers/gpu/drm/i915/display/intel_color.c
+++ drivers/gpu/drm/i915/display/intel_color.c
@@ -4146,6 +4146,7 @@ void intel_color_crtc_init(struct intel_crtc *crtc)
 					       "logarithmic gamma",
 					       xelpd_logarithmic_gamma,
 					       sizeof(xelpd_logarithmic_gamma));
+		drm_crtc_attach_gamma_mode_property(&crtc->base);
 	}
 }
 
