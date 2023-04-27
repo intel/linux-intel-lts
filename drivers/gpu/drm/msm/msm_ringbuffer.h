bc23c50aaec8235e8419d0e6006093c6aa096379
Ranjan Dutta <ranjan.dutta@intel.com>
2023-04-27 13:17:50 +0800

    Revert "drm/msm: Document and rename preempt_lock"
    This reverts commit 8dde1d9d6f065ea4c03c1d77182ba9e122d516d3.

--- drivers/gpu/drm/msm/msm_ringbuffer.h
+++ drivers/gpu/drm/msm/msm_ringbuffer.h
@@ -46,12 +46,7 @@ struct msm_ringbuffer {
 	struct msm_rbmemptrs *memptrs;
 	uint64_t memptrs_iova;
 	struct msm_fence_context *fctx;
-
-	/*
-	 * preempt_lock protects preemption and serializes wptr updates against
-	 * preemption.  Can be aquired from irq context.
-	 */
-	spinlock_t preempt_lock;
+	spinlock_t lock;
 };
 
 struct msm_ringbuffer *msm_ringbuffer_new(struct msm_gpu *gpu, int id,
