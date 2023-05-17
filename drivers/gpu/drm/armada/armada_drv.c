395d37f3eef45cddea7606196930b5726e071c40
Ranjan Dutta <ranjan.dutta@intel.com>
2023-05-17 10:32:08 +0800

    Revert "drm/armada: Fix a potential double free in an error handling path"
    This reverts commit 4a519731ece83d0884fa12595206f1d12f7fe4e5.

--- drivers/gpu/drm/armada/armada_drv.c
+++ drivers/gpu/drm/armada/armada_drv.c
@@ -102,6 +102,7 @@ static int armada_drm_bind(struct device *dev)
 	if (ret) {
 		dev_err(dev, "[" DRM_NAME ":%s] can't kick out simple-fb: %d\n",
 			__func__, ret);
+		kfree(priv);
 		return ret;
 	}
 
