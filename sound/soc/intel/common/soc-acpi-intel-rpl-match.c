41f11efc6053c863be025e4172c57ab27a179bb5
Li, Yifan2 <yifan2.li@intel.com>
2023-05-29 04:42:27 -0400

    ASoC: Intel: soc-acpi: Add entry for rt711-sdca-sdw at link 2 in RPL match table
    RT711 sdca sdw is added with SDW2 link for RPL-P CRB platform.
    This patch is a backport for upstream commit f5460a1 which can
    not easily apply to 6.1 tree.
    
    Signed-off-by: Li, Yifan2 <yifan2.li@intel.com>

--- sound/soc/intel/common/soc-acpi-intel-rpl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-rpl-match.c
@@ -284,6 +284,15 @@ static const struct snd_soc_acpi_link_adr rpl_sdw_rt1316_link12_rt714_link0[] = 
 	{}
 };
 
+static const struct snd_soc_acpi_link_adr rplp_crb[] = {
+	{
+		.mask = BIT(2),
+		.num_adr = ARRAY_SIZE(rt711_sdca_2_adr),
+		.adr_d = rt711_sdca_2_adr,
+	},
+	{}
+};
+
 struct snd_soc_acpi_mach snd_soc_acpi_intel_rpl_machines[] = {
 	{},
 };
@@ -332,6 +341,12 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_rpl_sdw_machines[] = {
 		.links = rpl_rvp,
 		.drv_name = "sof_sdw",
 		.sof_tplg_filename = "sof-rpl-rt711.tplg",
+	},
+	{
+		.link_mask = 0x4, /* link2 required */
+		.links = rplp_crb,
+		.drv_name = "sof_sdw",
+		.sof_tplg_filename = "sof-rpl-rt711-l2.tplg",
 	},
 	{},
 };
