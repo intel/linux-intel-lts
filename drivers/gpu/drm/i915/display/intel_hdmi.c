b537af458050a0fcb54b947a323206aff623f0f7
Junxiao Chang <junxiao.chang@intel.com>
2023-07-06 15:06:00 +0800

    Revert "drm/i915/mtl: Add C10 phy programming for HDMI"
    This reverts commit ccbcd8f70f846c86aa0bda629f0aa6f60a100e01.

--- drivers/gpu/drm/i915/display/intel_hdmi.c
+++ drivers/gpu/drm/i915/display/intel_hdmi.c
@@ -46,7 +46,6 @@
 #include "intel_atomic.h"
 #include "intel_audio.h"
 #include "intel_connector.h"
-#include "intel_cx0_phy.h"
 #include "intel_ddi.h"
 #include "intel_de.h"
 #include "intel_display_types.h"
@@ -1882,9 +1881,7 @@ hdmi_port_clock_valid(struct intel_hdmi *hdmi,
 	 * FIXME: We will hopefully get an algorithmic way of programming
 	 * the MPLLB for HDMI in the future.
 	 */
-	if (IS_METEORLAKE(dev_priv))
-		return intel_c10_phy_check_hdmi_link_rate(clock);
-	else if (IS_DG2(dev_priv))
+	if (IS_DG2(dev_priv))
 		return intel_snps_phy_check_hdmi_link_rate(clock);
 
 	return MODE_OK;
