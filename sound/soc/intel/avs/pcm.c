e334e0be38968192c7a4c240b3b0233f231d5529
Cezary Rojewski <cezary.rojewski@intel.com>
2023-08-18 09:59:57 +0200

    ASoC: Intel: avs: Allow for 16 channels configuration
    AudioDSP firmware and HDAudio controller support and facilitate audio
    formats with up to 16 channels. Reflect that with BE DAIs.
    
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>

--- sound/soc/intel/avs/pcm.c
+++ sound/soc/intel/avs/pcm.c
@@ -1300,7 +1300,7 @@ static const struct snd_soc_dai_driver i2s_dai_template = {
 	.ops = &avs_dai_nonhda_be_ops,
 	.playback = {
 		.channels_min	= 1,
-		.channels_max	= 8,
+		.channels_max	= 16,
 		.rates		= SNDRV_PCM_RATE_8000_192000 |
 				  SNDRV_PCM_RATE_KNOT,
 		.formats	= SNDRV_PCM_FMTBIT_S16_LE |
@@ -1311,7 +1311,7 @@ static const struct snd_soc_dai_driver i2s_dai_template = {
 	},
 	.capture = {
 		.channels_min	= 1,
-		.channels_max	= 8,
+		.channels_max	= 16,
 		.rates		= SNDRV_PCM_RATE_8000_192000 |
 				  SNDRV_PCM_RATE_KNOT,
 		.formats	= SNDRV_PCM_FMTBIT_S16_LE |
@@ -1390,7 +1390,7 @@ static const struct snd_soc_dai_driver hda_cpu_dai = {
 	.ops = &avs_dai_hda_be_ops,
 	.playback = {
 		.channels_min	= 1,
-		.channels_max	= 8,
+		.channels_max	= 16,
 		.rates		= SNDRV_PCM_RATE_8000_192000,
 		.formats	= SNDRV_PCM_FMTBIT_S16_LE |
 				  SNDRV_PCM_FMTBIT_S32_LE,
@@ -1400,7 +1400,7 @@ static const struct snd_soc_dai_driver hda_cpu_dai = {
 	},
 	.capture = {
 		.channels_min	= 1,
-		.channels_max	= 8,
+		.channels_max	= 16,
 		.rates		= SNDRV_PCM_RATE_8000_192000,
 		.formats	= SNDRV_PCM_FMTBIT_S16_LE |
 				  SNDRV_PCM_FMTBIT_S32_LE,
