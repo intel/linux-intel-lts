db4fa9455d86fb51716bae4cd839e8a58ddeb5d2
Philippe Gerum <rpm@xenomai.org>
2023-09-14 17:54:44 +0200

    genirq: irq_pipeline: fix irq flow handling with stacked irqchips
    When irqchips are stacked, a cascaded event decoded by a parent irq
    which is not delivered oob won't receive any ack/eoi, which is
    wrong. Failing to ack any unmasked edge event could quickly yield an
    interrupt storm. E.g. on x86, with a gpio irq controller stacked on
    top of the IO/APIC:
    
    handle_irq_pipelined_prepare()
    [on_pipeline_entry: true]
    arch_handle_irq()
            handle_fasteoi_irq()
                    handle_oob_irq()
                            mask_cond_eoi_irq()
    handle_irq_pipelined_finish()
    [on_pipeline_entry: false]
            synchronize_pipeline_on_irq()
                    handle_fasteoi_irq() [!on_pipeline_entry]
                          intel_gpio_irq()
                               handle_edge_irq() <<< failed to ack
    
    To solve this, we need to track the deferral status of every irq
    event, running the latter through the pipeline injection code when
    required, regardless of the current stage.
    
    See https://lore.kernel.org/xenomai/MN2PR11MB42855A234CF8BF7D17E9E2ECF2E6A@MN2PR11MB4285.namprd11.prod.outlook.com/.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>
    Signed-off-by: Hongzhan Chen <hongzhan.chen@intel.com>

--- include/linux/hardirq.h
+++ include/linux/hardirq.h
@@ -143,19 +143,9 @@ void irq_exit_rcu(void);
 		irq_pipeline_nmi_exit();			\
 	} while (0)
 
-static inline bool start_irq_flow(void)
-{
-	return !irqs_pipelined() || in_pipeline();
-}
-
 static inline bool on_pipeline_entry(void)
 {
 	return irqs_pipelined() && in_pipeline();
-}
-
-static inline bool in_hard_irq(void)
-{
-	return irqs_pipelined() ? in_pipeline() : in_hardirq();
 }
 
 static inline bool in_nonmaskable(void)
