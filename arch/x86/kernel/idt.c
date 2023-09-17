0a6e1382d474c5a8444ec01fb543fe3d7821ceda
Philippe Gerum <rpm@xenomai.org>
2023-09-17 11:36:26 +0200

    x86: irq_pipeline: export CALL_FUNCTION_OOB_VECTOR
    An interrupt vector to trigger function calls on remote CPUs via IPIs.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- arch/x86/kernel/idt.c
+++ arch/x86/kernel/idt.c
@@ -133,8 +133,9 @@ static const __initconst struct idt_data apic_idts[] = {
 	INTG(CALL_FUNCTION_SINGLE_VECTOR,	asm_sysvec_call_function_single),
 	INTG(REBOOT_VECTOR,			asm_sysvec_reboot),
 #ifdef CONFIG_IRQ_PIPELINE
-	INTG(RESCHEDULE_OOB_VECTOR,		asm_sysvec_reschedule_oob_ipi),
 	INTG(TIMER_OOB_VECTOR,			asm_sysvec_timer_oob_ipi),
+	INTG(RESCHEDULE_OOB_VECTOR,		asm_sysvec_reschedule_oob_ipi),
+	INTG(CALL_FUNCTION_OOB_VECTOR,		asm_sysvec_call_function_oob_ipi),
 #endif
 #endif
 
