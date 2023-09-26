82a6d84a261d63d954c34cbd0af99b84fa587740
Cezary Rojewski <cezary.rojewski@intel.com>
2023-09-26 10:06:20 +0200

    ALSA: hda: Poll SDxFIFOS after programming SDxFMT
    commit 2ee2c75c589acff83e987abfa74b6d81d237d92f upstream.
    
    Software shall read SDxFIFOS calculated by the hardware and notify if
    invalid value is programmed before continuing the stream preparation.
    
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>
    Link: https://lore.kernel.org/r/20230926080623.43927-2-cezary.rojewski@intel.com
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/sound/hdaudio.h
+++ include/sound/hdaudio.h
@@ -622,6 +622,9 @@ int snd_hdac_stream_set_lpib(struct hdac_stream *azx_dev, u32 value);
 #define snd_hdac_stream_readb_poll(dev, reg, val, cond, delay_us, timeout_us) \
 	readb_poll_timeout((dev)->sd_addr + AZX_REG_ ## reg, val, cond, \
 			   delay_us, timeout_us)
+#define snd_hdac_stream_readw_poll(dev, reg, val, cond, delay_us, timeout_us) \
+	read_poll_timeout_atomic(snd_hdac_reg_readw, val, cond, delay_us, timeout_us, \
+				 false, (dev)->bus, (dev)->sd_addr + AZX_REG_ ## reg)
 #define snd_hdac_stream_readl_poll(dev, reg, val, cond, delay_us, timeout_us) \
 	readl_poll_timeout((dev)->sd_addr + AZX_REG_ ## reg, val, cond, \
 			   delay_us, timeout_us)
