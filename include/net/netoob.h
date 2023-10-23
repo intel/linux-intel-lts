79690d2fac50b17d5b3d04c6b8221d19392f974f
Philippe Gerum <philippe.gerum@exail.com>
2023-10-23 11:48:14 +0200

    net: dovetail: introduce net struct extension
    This is needed to store the per-net information related to oob
    activity.
    
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

--- include/net/netoob.h
+++ include/net/netoob.h
@@ -2,6 +2,7 @@
 #ifndef _NET_OOBNET_H
 #define _NET_OOBNET_H
 
+#include <dovetail/net.h>
 #include <dovetail/netdevice.h>
 
 /* Device supports direct out-of-band operations (RX & TX) */
@@ -9,6 +10,7 @@
 /* Device is an out-of-band port */
 #define IFF_OOB_PORT		BIT(1)
 
+/* Netdevice context extension for oob handling. */
 struct oob_netdev_context {
 	int flags;
 	struct oob_netdev_state dev_state;
