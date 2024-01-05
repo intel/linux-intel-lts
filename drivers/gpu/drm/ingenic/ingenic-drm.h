3c74ca755021d04a4c5c56aa566c0eea6ea361c3
Junxiao Chang <junxiao.chang@intel.com>
2024-01-05 15:54:20 +0800

    Revert "drm/ingenic: Add option to alloc cached GEM buffers"
    This reverts commit 4a791cb6d34f42ef9aa7a65f2ba5aa19644c53a7.

--- drivers/gpu/drm/ingenic/ingenic-drm.h
+++ drivers/gpu/drm/ingenic/ingenic-drm.h
@@ -184,7 +184,6 @@ struct platform_driver;
 void ingenic_drm_plane_config(struct device *dev,
 			      struct drm_plane *plane, u32 fourcc);
 void ingenic_drm_plane_disable(struct device *dev, struct drm_plane *plane);
-bool ingenic_drm_map_noncoherent(const struct device *dev);
 
 extern struct platform_driver *ingenic_ipu_driver_ptr;
 
