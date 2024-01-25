3a62ca485b657f6cd4a087cf63d11a1cfa9cc0f7
Junxiao Chang <junxiao.chang@intel.com>
2024-01-25 19:21:10 +0800

    Revert "drm/i915/selftest: fix intel_selftest_modify_policy argument types"
    This reverts commit e481654426b6bb1dc80a745b9ca72b75cc2f49f8.
    
    Signed-off-by: Junxiao Chang <junxiao.chang@intel.com>

--- drivers/gpu/drm/i915/selftests/intel_scheduler_helpers.c
+++ drivers/gpu/drm/i915/selftests/intel_scheduler_helpers.c
@@ -16,7 +16,8 @@
 
 int intel_selftest_modify_policy(struct intel_engine_cs *engine,
 				 struct intel_selftest_saved_policy *saved,
-				 enum selftest_scheduler_modify modify_type)
+				 u32 modify_type)
+
 {
 	int err;
 
