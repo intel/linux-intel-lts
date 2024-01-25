f51ef44c4fda89945b3a215eddb1b01c8a2c1b53
Junxiao Chang <junxiao.chang@intel.com>
2024-01-25 19:21:13 +0800

    Revert "drm/i915/gvt: fix vgpu debugfs clean in remove"
    This reverts commit f5a9bbf962e2c4b1d9addbfaf16d7ffcc2f63bde.
    
    Signed-off-by: Junxiao Chang <junxiao.chang@intel.com>

--- drivers/gpu/drm/i915/gvt/debugfs.c
+++ drivers/gpu/drm/i915/gvt/debugfs.c
@@ -175,13 +175,8 @@ void intel_gvt_debugfs_add_vgpu(struct intel_vgpu *vgpu)
  */
 void intel_gvt_debugfs_remove_vgpu(struct intel_vgpu *vgpu)
 {
-	struct intel_gvt *gvt = vgpu->gvt;
-	struct drm_minor *minor = gvt->gt->i915->drm.primary;
-
-	if (minor->debugfs_root && gvt->debugfs_root) {
-		debugfs_remove_recursive(vgpu->debugfs);
-		vgpu->debugfs = NULL;
-	}
+	debugfs_remove_recursive(vgpu->debugfs);
+	vgpu->debugfs = NULL;
 }
 
 /**
