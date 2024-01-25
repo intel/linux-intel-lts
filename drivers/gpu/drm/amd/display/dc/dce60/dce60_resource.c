21f8d3103413243e56a9cf35f43af68dcc611a0b
Junxiao Chang <junxiao.chang@intel.com>
2024-01-25 19:21:17 +0800

    Revert "drm/amd/display: prevent memory leak"
    This reverts commit cc8deb82cca522275963b9f1a4739b58fa2a1506.
    
    Signed-off-by: Junxiao Chang <junxiao.chang@intel.com>

--- drivers/gpu/drm/amd/display/dc/dce60/dce60_resource.c
+++ drivers/gpu/drm/amd/display/dc/dce60/dce60_resource.c
@@ -1127,7 +1127,6 @@ struct resource_pool *dce60_create_resource_pool(
 	if (dce60_construct(num_virtual_links, dc, pool))
 		return &pool->base;
 
-	kfree(pool);
 	BREAK_TO_DEBUGGER();
 	return NULL;
 }
@@ -1325,7 +1324,6 @@ struct resource_pool *dce61_create_resource_pool(
 	if (dce61_construct(num_virtual_links, dc, pool))
 		return &pool->base;
 
-	kfree(pool);
 	BREAK_TO_DEBUGGER();
 	return NULL;
 }
@@ -1519,7 +1517,6 @@ struct resource_pool *dce64_create_resource_pool(
 	if (dce64_construct(num_virtual_links, dc, pool))
 		return &pool->base;
 
-	kfree(pool);
 	BREAK_TO_DEBUGGER();
 	return NULL;
 }
