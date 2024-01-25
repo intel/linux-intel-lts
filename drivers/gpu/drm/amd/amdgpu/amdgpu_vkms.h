49b8e8dd3dc37c671789cc9180219b79985313de
Junxiao Chang <junxiao.chang@intel.com>
2024-01-25 19:20:35 +0800

    Revert "drm/amdgpu: fix vkms crtc settings"
    This reverts commit fc399b0fdf2db754b6a50126498d91c525589eb0.
    
    Signed-off-by: Junxiao Chang <junxiao.chang@intel.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_vkms.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_vkms.h
@@ -10,14 +10,15 @@
 #define YRES_MAX  16384
 
 #define drm_crtc_to_amdgpu_vkms_output(target) \
-	container_of(target, struct amdgpu_vkms_output, crtc.base)
+	container_of(target, struct amdgpu_vkms_output, crtc)
 
 extern const struct amdgpu_ip_block_version amdgpu_vkms_ip_block;
 
 struct amdgpu_vkms_output {
-	struct amdgpu_crtc crtc;
+	struct drm_crtc crtc;
 	struct drm_encoder encoder;
 	struct drm_connector connector;
+	struct hrtimer vblank_hrtimer;
 	ktime_t period_ns;
 	struct drm_pending_vblank_event *event;
 };
