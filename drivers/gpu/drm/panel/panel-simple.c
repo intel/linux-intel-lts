b922e02659a7abaeedc94da17e669b78c00e97ea
Junxiao Chang <junxiao.chang@intel.com>
2024-01-25 19:20:39 +0800

    Revert "drm/panel: simple: fix active size for Ampire AM-480272H3TMQW-T01H"
    This reverts commit 314850a4d0c6c9b305c585866d82dbbc48880688.
    
    Signed-off-by: Junxiao Chang <junxiao.chang@intel.com>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -890,8 +890,8 @@ static const struct panel_desc ampire_am_480272h3tmqw_t01h = {
 	.num_modes = 1,
 	.bpc = 8,
 	.size = {
-		.width = 99,
-		.height = 58,
+		.width = 105,
+		.height = 67,
 	},
 	.bus_format = MEDIA_BUS_FMT_RGB888_1X24,
 };
