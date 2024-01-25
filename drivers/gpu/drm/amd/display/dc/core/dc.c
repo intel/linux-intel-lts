3129aa8a7e3cc544f49eb9c9cff99047470f4e8d
Junxiao Chang <junxiao.chang@intel.com>
2024-01-25 19:20:26 +0800

    Revert "drm/amd/display: Don't set dpms_off for seamless boot"
    This reverts commit 510d4a01d84cc096db1799c8a5fd93dd7dc41182.
    
    Signed-off-by: Junxiao Chang <junxiao.chang@intel.com>

--- drivers/gpu/drm/amd/display/dc/core/dc.c
+++ drivers/gpu/drm/amd/display/dc/core/dc.c
@@ -1023,9 +1023,6 @@ static void disable_vbios_mode_if_required(
 		if (stream == NULL)
 			continue;
 
-		if (stream->apply_seamless_boot_optimization)
-			continue;
-
 		// only looking for first odm pipe
 		if (pipe->prev_odm_pipe)
 			continue;
