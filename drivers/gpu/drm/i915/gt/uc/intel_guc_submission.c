ekuiter/torte-linux

drm/i915/guc: Improve debug message on context reset notification
Just recently we switched over to new GuC oriented log macros but in
the meantime yet another message was added that we missed to update.

While around improve that new message by adding engine name and use
existing helpers to check for context state.

Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
Cc: John Harrison <John.C.Harrison@Intel.com>
Reviewed-by: John Harrison <John.C.Harrison@Intel.com>
Signed-off-by: John Harrison <John.C.Harrison@Intel.com>
Link: https://patchwork.freedesktop.org/patch/msgid/20230131214413.1879-1-michal.wajdeczko@intel.com

drivers/gpu/drm/i915/gt/uc/intel_guc_submission.c
@@ -4660,9 +4660,10 @@ static void guc_handle_context_reset(struct intel_guc *guc,
 {
 	trace_intel_context_reset(ce);
 
-	drm_dbg(&guc_to_gt(guc)->i915->drm, "Got GuC reset of 0x%04X, exiting = %d, banned = %d\n",
-		ce->guc_id.id, test_bit(CONTEXT_EXITING, &ce->flags),
-		test_bit(CONTEXT_BANNED, &ce->flags));
+	guc_dbg(guc, "Got context reset notification: 0x%04X on %s, exiting = %s, banned = %s\n",
+		ce->guc_id.id, ce->engine->name,
+		str_yes_no(intel_context_is_exiting(ce)),
+		str_yes_no(intel_context_is_banned(ce)));
 
 	if (likely(intel_context_is_schedulable(ce))) {
 		capture_error_state(guc, ce);

intel/linux-intel-lts

(cherry picked from commit 5dfb29d444daf94120762e9d7b9fe6aa4e0f9929)
[ Hacked due to missing 45c64ecf97ee3 drm/i915: Improve user
experience and driver robustness under SIGINT or similar ]

drivers/gpu/drm/i915/gt/uc/intel_guc_submission.c
@@ -4686,8 +4686,10 @@ static void guc_handle_context_reset(struct intel_guc *guc,
 {
 	trace_intel_context_reset(ce);
 
-	drm_dbg(&guc_to_gt(guc)->i915->drm, "Got GuC reset of 0x%04X, blocked = %d, banned = %d\n",
-		ce->guc_id.id, context_blocked(ce), intel_context_is_banned(ce));
+	guc_dbg(guc, "Got context reset notification: 0x%04X on %s, blocked = %s, banned = %s\n",
+		ce->guc_id.id, ce->engine->name,
+		str_yes_no(context_blocked(ce)),
+		str_yes_no(intel_context_is_banned(ce)));
 
 	/*
 	 * XXX: Racey if request cancellation has occurred, see comment in