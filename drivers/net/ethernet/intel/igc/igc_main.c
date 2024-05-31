9351cab0c7134d3bbd6ed47424b146042b5e7c8c
Kishor408 <kishorx.buchi@intel.com>
2024-05-31 12:46:17 +0500

    Revert "igc: Add support for PTP .getcyclesx64()"
    This reverts commit 069b142f58196bd9f47b35e493255741e2c663c7.

--- drivers/net/ethernet/intel/igc/igc_main.c
+++ drivers/net/ethernet/intel/igc/igc_main.c
@@ -1312,10 +1312,6 @@ static void igc_tx_olinfo_status(struct igc_ring *tx_ring,
 	olinfo_status |= IGC_SET_FLAG(tx_flags, IGC_TX_FLAGS_IPV4,
 				      (IGC_TXD_POPTS_IXSM << 8));
 
-	/* Use the second timer (free running, in general) for the timestamp */
-	olinfo_status |= IGC_SET_FLAG(tx_flags, IGC_TX_FLAGS_TSTAMP_TIMER_1,
-				      IGC_TXD_PTP2_TIMER_1);
-
 	tx_desc->read.olinfo_status = cpu_to_le32(olinfo_status);
 }
 
@@ -1665,8 +1661,6 @@ done:
 		if (is_ptp && igc_request_tx_tstamp(adapter, skb, &tstamp_flags)) {
 			skb_shinfo(skb)->tx_flags |= SKBTX_IN_PROGRESS;
 			tx_flags |= IGC_TX_FLAGS_TSTAMP | tstamp_flags;
-			if (skb_shinfo(skb)->tx_flags & SKBTX_HW_TSTAMP_USE_CYCLES)
-				tx_flags |= IGC_TX_FLAGS_TSTAMP_TIMER_1;
 		} else if (!is_ptp) {
 			skb_shinfo(skb)->tx_flags |= SKBTX_IN_PROGRESS;
 			tx_flags |= IGC_TX_FLAGS_DMA_TSTAMP;
@@ -1982,9 +1976,9 @@ static struct sk_buff *igc_build_skb(struct igc_ring *rx_ring,
 
 static struct sk_buff *igc_construct_skb(struct igc_ring *rx_ring,
 					 struct igc_rx_buffer *rx_buffer,
-					 struct igc_xdp_buff *ctx)
+					 struct xdp_buff *xdp,
+					 ktime_t timestamp)
 {
-	struct xdp_buff *xdp = &ctx->xdp;
 	unsigned int metasize = xdp->data - xdp->data_meta;
 	unsigned int size = xdp->data_end - xdp->data;
 	unsigned int truesize = igc_get_rx_frame_truesize(rx_ring, size);
@@ -2001,10 +1995,8 @@ static struct sk_buff *igc_construct_skb(struct igc_ring *rx_ring,
 	if (unlikely(!skb))
 		return NULL;
 
-	if (ctx->rx_ts) {
-		skb_shinfo(skb)->tx_flags |= SKBTX_HW_TSTAMP_NETDEV;
-		skb_hwtstamps(skb)->netdev_data = ctx->rx_ts;
-	}
+	if (timestamp)
+		skb_hwtstamps(skb)->hwtstamp = timestamp;
 
 	/* Determine available headroom for copy */
 	headlen = size;
@@ -2692,10 +2684,11 @@ static int igc_clean_rx_irq(struct igc_q_vector *q_vector, const int budget)
 	int xdp_status = 0, rx_buffer_pgcnt;
 
 	while (likely(total_packets < budget)) {
-		struct igc_xdp_buff ctx = { .rx_ts = NULL };
-		struct igc_rx_buffer *rx_buffer;
 		union igc_adv_rx_desc *rx_desc;
+		struct igc_rx_buffer *rx_buffer;
 		unsigned int size, truesize;
+		struct igc_xdp_buff ctx;
+		ktime_t timestamp = 0;
 		int pkt_offset = 0;
 		int smd_type;
 		void *pktbuf;
@@ -2723,7 +2716,9 @@ static int igc_clean_rx_irq(struct igc_q_vector *q_vector, const int budget)
 		pktbuf = page_address(rx_buffer->page) + rx_buffer->page_offset;
 
 		if (igc_test_staterr(rx_desc, IGC_RXDADV_STAT_TSIP)) {
-			ctx.rx_ts = pktbuf;
+			timestamp = igc_ptp_rx_pktstamp(q_vector->adapter,
+							pktbuf);
+			ctx.rx_ts = timestamp;
 			pkt_offset = IGC_TS_HDR_LEN;
 			size -= IGC_TS_HDR_LEN;
 		}
@@ -2776,7 +2771,8 @@ static int igc_clean_rx_irq(struct igc_q_vector *q_vector, const int budget)
 		else if (ring_uses_build_skb(rx_ring))
 			skb = igc_build_skb(rx_ring, rx_buffer, &ctx.xdp);
 		else
-			skb = igc_construct_skb(rx_ring, rx_buffer, &ctx);
+			skb = igc_construct_skb(rx_ring, rx_buffer, &ctx.xdp,
+						timestamp);
 
 		/* exit if we failed to retrieve a buffer */
 		if (!skb) {
@@ -2927,7 +2923,9 @@ static int igc_clean_rx_irq_zc(struct igc_q_vector *q_vector, const int budget)
 		ctx->rx_desc = desc;
 
 		if (igc_test_staterr(desc, IGC_RXDADV_STAT_TSIP)) {
-			ctx->rx_ts = bi->xdp->data;
+			timestamp = igc_ptp_rx_pktstamp(q_vector->adapter,
+							bi->xdp->data);
+			ctx->rx_ts = timestamp;
 
 			bi->xdp->data += IGC_TS_HDR_LEN;
 
@@ -7057,24 +7055,6 @@ int igc_xsk_wakeup(struct net_device *dev, u32 queue_id, u32 flags)
 	return 0;
 }
 
-static ktime_t igc_get_tstamp(struct net_device *dev,
-			      const struct skb_shared_hwtstamps *hwtstamps,
-			      bool cycles)
-{
-	struct igc_adapter *adapter = netdev_priv(dev);
-	struct igc_inline_rx_tstamps *tstamp;
-	ktime_t timestamp;
-
-	tstamp = hwtstamps->netdev_data;
-
-	if (cycles)
-		timestamp = igc_ptp_rx_pktstamp(adapter, tstamp->timer1);
-	else
-		timestamp = igc_ptp_rx_pktstamp(adapter, tstamp->timer0);
-
-	return timestamp;
-}
-
 static const struct net_device_ops igc_netdev_ops = {
 	.ndo_open		= igc_open,
 	.ndo_stop		= igc_close,
@@ -7092,7 +7072,6 @@ static const struct net_device_ops igc_netdev_ops = {
 	.ndo_bpf		= igc_bpf,
 	.ndo_xdp_xmit		= igc_xdp_xmit,
 	.ndo_xsk_wakeup		= igc_xsk_wakeup,
-	.ndo_get_tstamp		= igc_get_tstamp,
 };
 
 /* PCIe configuration access */
@@ -7196,11 +7175,9 @@ static int igc_xdp_rx_hash(const struct xdp_md *_ctx, u32 *hash,
 static int igc_xdp_rx_timestamp(const struct xdp_md *_ctx, u64 *timestamp)
 {
 	const struct igc_xdp_buff *ctx = (void *)_ctx;
-	struct igc_adapter *adapter = netdev_priv(ctx->xdp.rxq->dev);
-	struct igc_inline_rx_tstamps *tstamp = ctx->rx_ts;
 
 	if (igc_test_staterr(ctx->rx_desc, IGC_RXDADV_STAT_TSIP)) {
-		*timestamp = igc_ptp_rx_pktstamp(adapter, tstamp->timer0);
+		*timestamp = ctx->rx_ts;
 
 		return 0;
 	}
