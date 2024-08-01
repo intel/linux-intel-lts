1767e01d75fbf31d812271d5cf4badbb63627995
Kooran Paul, Princy <princy.kooran.paul@intel.com>
2024-08-01 11:19:37 +0800

    drm/i915/mtl: Provide user the option to disable ccs
        There are RCS & CSS Sync out Sync issues on MTL caused the context timeout
        and reset. This is temporary WA to disable CCS to allow only RCS
        workloads.
    
    Signed-off-by: Chew, Tong Liang <tong.liang.chew@intel.com>
    Signed-off-by: Kooran Paul, Princy <princy.kooran.paul@intel.com>

--- drivers/gpu/drm/i915/i915_params.h
+++ drivers/gpu/drm/i915/i915_params.h
@@ -61,6 +61,7 @@ struct drm_printer;
 	param(unsigned int, lmem_size, 0, 0400) \
 	param(unsigned int, lmem_bar_size, 0, 0400) \
 	param(unsigned int, max_vfs, 0, 0400) \
+	param(int, force_disable_ccs, 0, 0400) \
 	/* leave bools at the end to not create holes */ \
 	param(bool, enable_mtl_rcs_ccs_wa, false, 0x400) \
 	param(bool, enable_hangcheck, true, 0600) \
