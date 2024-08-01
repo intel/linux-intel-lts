e4d9121df46b454c6e6fae31c67ef6de81572aa5
Philippe Gerum <rpm@xenomai.org>
2024-08-01 09:52:03 +0200

    dovetail: fix syscall detection conflict in prctl() form
    PR_SET_VMA has bit 0x01000000 set, which breaks the detection logic
    for oob syscalls.
    
    Fixes: dovetail: add core support.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/uapi/asm-generic/dovetail.h
+++ include/uapi/asm-generic/dovetail.h
@@ -2,6 +2,12 @@
 #ifndef __ASM_GENERIC_DOVETAIL_H
 #define __ASM_GENERIC_DOVETAIL_H
 
+/*
+ * Legacy out-of-band syscall marker. Will be dropped at some point.
+ */
 #define __OOB_SYSCALL_BIT	0x10000000
+
+/* Dovetail out-of-band syscall via prctl(2) */
+#define PR_OOB_SYSCALL		0x4f4f4243
 
 #endif /* !__ASM_GENERIC_DOVETAIL_H */
