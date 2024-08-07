c084c36888a4a848fbe45e2ca0025a21da46e29e
zouxiaoh <xiaohong.zou@intel.com>
2024-08-07 17:09:39 +0800

    media: pci: intel: change psys_freq to readable and writable
    Change Description:
    Change psys_freq to readable and writable, and delete psys_force_freq.
    
    Signed-off-by: linya14x <linx.yang@intel.com>
    Signed-off-by: zouxiaoh <xiaohong.zou@intel.com>

--- drivers/media/pci/intel/ipu6/ipu6.c
+++ drivers/media/pci/intel/ipu6/ipu6.c
@@ -293,29 +293,6 @@ void ipu_configure_spc(struct ipu_device *isp,
 }
 EXPORT_SYMBOL(ipu_configure_spc);
 
-int ipu_buttress_psys_freq_get(void *data, u64 *val)
-{
-	struct ipu_device *isp = data;
-	u32 reg_val;
-	int rval;
-
-	rval = pm_runtime_get_sync(&isp->psys->dev);
-	if (rval < 0) {
-		pm_runtime_put(&isp->psys->dev);
-		dev_err(&isp->pdev->dev, "Runtime PM failed (%d)\n", rval);
-		return rval;
-	}
-
-	reg_val = readl(isp->base + BUTTRESS_REG_PS_FREQ_CTL);
-
-	pm_runtime_put(&isp->psys->dev);
-
-	*val = IPU_PS_FREQ_RATIO_BASE *
-	    (reg_val & IPU_BUTTRESS_PS_FREQ_CTL_DIVISOR_MASK);
-
-	return 0;
-}
-
 void ipu_internal_pdata_init(void)
 {
 	if (ipu_ver == IPU_VER_6 || ipu_ver == IPU_VER_6EP ||
