23ea46ca3e910d6bb1f349db06aa3e6cb6f72458
Rui Chen <chenrui9@honor.com>
2024-10-25 11:15:57 +0800

    ANDROID: fs: add vendor hook to collect IO statistics
    Add vendor hook to get metainfo of direct/buffered read and write.
    Determine hot files in each performance-sensitive user scenario.
    
    Bug: 362663044
    Change-Id: Ic7ac7430890851d1b69ea29f33f65722c1b644f1
    Signed-off-by: Rui Chen <chenrui9@honor.com>

--- mm/filemap.c
+++ mm/filemap.c
@@ -3868,6 +3868,7 @@ again:
 			if (unlikely(status < 0))
 				break;
 		}
+		trace_android_vh_io_statistics(mapping, page->index, 1, false, false);
 		cond_resched();
 
 		if (unlikely(status == 0)) {
