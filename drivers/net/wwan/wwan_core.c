3d3595f5edba4281f577588debf0eb496b2ad089
Qiao, Wei <wei.qiao@intel.com>
2025-01-06 16:55:00 +0800

    Revert "wwan: add SAHARA device"
    This reverts commit 0cf6d20bf37608a778f0f72928761b4105ffdac3.

--- drivers/net/wwan/wwan_core.c
+++ drivers/net/wwan/wwan_core.c
@@ -334,10 +334,6 @@ static const struct {
 		.name = "FASTBOOT",
 		.devsuf = "fastboot",
 	},
-	[WWAN_PORT_SAHARA] = {
-		.name = "SAHARA",
-		.devsuf = "sahara",
-	},
 };
 
 static ssize_t type_show(struct device *dev, struct device_attribute *attr,
