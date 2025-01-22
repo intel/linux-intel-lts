f90d04942b56016c8b10b96b7ee22e548ecf24cf
Choong Yong Liang <yong.liang.choong@intel.com>
2025-01-22 17:43:08 +0800

    Revert "net: stmmac: set initial EEE policy configuration"
    This reverts commit 93d89d8a7e3615d77f6091ff44e942317fc12cfd.

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -1224,9 +1224,6 @@ static int stmmac_init_phy(struct net_device *dev)
 		if (priv->dma_cap.eee)
 			phy_support_eee(phydev);
 
-		if (priv->dma_cap.eee)
-			phy_support_eee(phydev);
-
 		ret = phylink_connect_phy(priv->phylink, phydev);
 	} else {
 		fwnode_handle_put(phy_fwnode);
